2ddb4ccf915894b19eb5960a0f5de4a6d009c37c
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-06 22:52:23 +0900

    Definition API and namespace, copy models from Covid19Radar project.

---
+++ Covid19RadarAPI/Models/BeaconDataModel.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Covid19Radar.Models
+{
+    public sealed class BeaconDataModel
+
+    {
+        public string UUID { get; set; }
+
+        public string Major { get; set; }
+
+        public string Minor { get; set; }
+
+        public double Distance { get; set; }
+
+        public int Rssi { get; set; }
+        public int TXPower { get; set; }
+
+        public TimeSpan ElaspedTime { get; set; }
+        public DateTime LastDetectTime { get; set; }
+    }
+}
