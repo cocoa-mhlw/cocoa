9adac30c0fe8872852c6567643791ce68b86d850
fumiya-kume <fumiya.kume@hotmail.com>
2020-04-07 06:52:52 +0900

    add cell

--- Covid19Radar/Covid19Radar/ViewModels/BeaconPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/BeaconPageViewModel.cs
@@ -1,9 +1,12 @@
 ﻿using Covid19Radar.Common;
 using Covid19Radar.Model;
 using Covid19Radar.Services;
+using Covid19Radar.Views.cell;
 using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +17,8 @@ namespace Covid19Radar.ViewModels
 {
     public class BeaconPageViewModel : ViewModelBase
     {
+        public ReactiveCollection<BeaconViewCell> beaconList = new ReactiveCollection<BeaconViewCell>();
+
         private INavigationService _navigationService;
         public BeaconPageViewModel(INavigationService navigationService)
             : base(navigationService)
@@ -21,6 +26,25 @@ namespace Covid19Radar.ViewModels
             _navigationService = navigationService;
             Title = "Beacon Page";
             AppUtils.CheckPermission();
+
+            try
+            {
+                var beaconService = DependencyService.Get<IBeaconService>();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+            //beaconService.StartBeacon();
+            //beaconService.StopAdvertising();
+            //var beaconList = beaconService.GetBeaconData();
+            //var list = beaconService.GetBeaconData();
+
+            var dummyCell = new BeaconViewCell();
+            dummyCell.UUID.Value = "AAAAAAAAAA";
+
+            beaconList.Add(dummyCell);
+            beaconList.Add(dummyCell);
 
             // Create User
             // TODO Check Register User (UUID.Major.Minor) or New
---
+++ Covid19Radar/Covid19Radar/ViewModels/cell/BeaconViewCell.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using Reactive.Bindings;
+using Xamarin.Forms;
+
+namespace Covid19Radar.Views.cell
+{
+    public class BeaconViewCell
+    {
+        public ReactiveProperty<String> UUID { get; set; } = new ReactiveProperty<String>();
+    }
+}
