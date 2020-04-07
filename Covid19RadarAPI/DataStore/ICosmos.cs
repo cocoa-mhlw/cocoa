ee2e4596f1df7308fb45651517d7aadb90d5c469
Yasuaki Matsuda <dark_crash@hotmail.com>
2020-04-08 08:47:42 +0900

    Change Introduce DI and  add mock services (#42)
    * Change Introduce DI and  add mock services
    
    * Change Introduce DI and  add mock services
    
    * exclude source control for local.settings.json
    
    * delete unnecessary file

---
+++ Covid19RadarAPI/DataStore/ICosmos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Radar.DataStore
+{
+    public interface ICosmos
+    {
+        Task CreateDatabaseAsync(string databaseId);
+    }
+}
