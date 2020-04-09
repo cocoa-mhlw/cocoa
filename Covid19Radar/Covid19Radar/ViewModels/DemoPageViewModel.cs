739b2b0f97b58835ed3cd57b0bc0efe581857d77
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-10 02:25:30 +0900

    Demo Page

---
+++ Covid19Radar/Covid19Radar/ViewModels/DemoPageViewModel.cs
@@ -0,0 +1,23 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Covid19Radar.ViewModels
+{
+    public class DemoPageViewModel : ViewModelBase
+    {
+        private INavigationService _navigationService;
+        public DemoPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            _navigationService = navigationService;
+            Title = "検出したビーコン一覧";
+        }
+
+    }
+}
