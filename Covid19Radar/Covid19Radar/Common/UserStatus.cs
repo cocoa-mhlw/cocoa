781d0112e7978a98835b9ac03030f65ef7342141
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-10 21:31:24 +0900

    Fix model's user story and workitem.

---
+++ Covid19Radar/Covid19Radar/Common/UserStatus.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Common
+{
+    public enum UserStatus
+    {
+        None,
+        Contactd,
+        OnSet,
+        Suspected,
+        Inspection,
+        Infection
+    }
+}
