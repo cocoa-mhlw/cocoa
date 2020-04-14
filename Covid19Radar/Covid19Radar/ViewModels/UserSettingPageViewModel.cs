266d9d55acc6d225efdc8b2bdf637ff86f853c21
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-15 02:49:57 +0900

    It's very crude. Displays a list of detected beacons on the page.

--- Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
@@ -1,14 +1,11 @@
 ﻿using Covid19Radar.Common;
 using Covid19Radar.Model;
 using Covid19Radar.Services;
-using Covid19Radar.Views.cell;
 using Prism.Commands;
 using Prism.DryIoc;
 using Prism.Mvvm;
 using Prism.Navigation;
 using Prism.Services;
-using Reactive.Bindings;
-using Reactive.Bindings.Extensions;
 using System;
 using System.Collections.Generic;
 using System.Linq;
