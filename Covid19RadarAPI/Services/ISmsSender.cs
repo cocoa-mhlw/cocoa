7b5f73f7e103bae2c1c5d48387a80272b8b03b9e
lohithgn <lohith@xamariners.com>
2020-04-15 13:01:59 +0800

    [chore] OTP send implementation. Created OTP container, Use OTP.NET to generate OTP, store otp generated timestamp for a user, integrate twilio

---
+++ Covid19RadarAPI/Services/ISmsSender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Radar.Services
+{
+    public interface ISmsSender
+    {
+        Task<bool> SendAsync(string body, string toPhone);
+
+    }
+}
---
+++ Covid19RadarAPI/Services/OtpGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using Microsoft.Extensions.Configuration;
+using OtpNet;
+
+namespace Covid19Radar.Services
+{
+    public class OtpGenerator : IOtpGenerator
+    {
+        private const string TotpSecretKeySetting = "TOTP_SECRET";
+        private readonly string _otpSecretKey;
+
+        public OtpGenerator(IConfiguration configuration)
+        {
+            _otpSecretKey = configuration[TotpSecretKeySetting];
+        }
+        public string Generate(DateTime timeStamp)
+        {
+            var generator = new Totp(Encoding.UTF8.GetBytes(_otpSecretKey));
+            return generator.ComputeTotp(timeStamp);
+        }
+
+        public bool Validate(string code, DateTime timeStamp)
+        {
+            var generator = new Totp(Encoding.UTF8.GetBytes(_otpSecretKey));
+            var valid = generator.VerifyTotp(timeStamp, code, out long timeStepMatched);
+            return valid;
+        }
+    }
+}
