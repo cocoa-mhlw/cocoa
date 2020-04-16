8723b2b99eb21a8234eb236500f0c5f89ada41d1
Colin Bacon <iambacon@users.noreply.github.com>
2020-04-16 11:29:53 +0800

    Add steps on list creation

--- Covid19Radar/Covid19Radar/ViewModels/DescriptionPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/DescriptionPageViewModel.cs
@@ -19,35 +19,37 @@ namespace Covid19Radar.ViewModels
 
         private void SetData()
         {
-            Steps = new List<StepModel>();
+            Steps = new List<StepModel>
+            {
+                new StepModel
+                {
+                    Title = Resx.AppResources.TextStep1Title,
+                    Image = ImageSource.FromResource("Covid19Radar.Images.description-step1.png"),
+                    Description = Resx.AppResources.TextStep1Description,
+                    StepNumber = 1
+                },
+                new StepModel
+                {
+                    Title = Resx.AppResources.TextStep2Title,
+                    Image = ImageSource.FromResource("Covid19Radar.Images.description-step2.png"),
+                    Description = Resx.AppResources.TextStep2Description,
+                    StepNumber = 2
+                },
+                new StepModel
+                {
+                    Title = Resx.AppResources.TextStep3Title,
+                    Image = ImageSource.FromResource("Covid19Radar.Images.description-step3.png"),
+                    Description = Resx.AppResources.TextStep3Description,
+                    StepNumber = 3
+                },
+                new StepModel
+                {
+                    Title = Resx.AppResources.TextStep4Title,
+                    Image = ImageSource.FromResource("Covid19Radar.Images.description-step4.png"),
+                    Description = Resx.AppResources.TextStep4Description
+                }
+            };
 
-            Steps.Add(new StepModel
-            {
-                Title = Resx.AppResources.TextStep1Title,
-                Image = ImageSource.FromResource("Covid19Radar.Images.description-step1.png"),
-                Description = Resx.AppResources.TextStep1Description,
-                StepNumber = 1
-            });
-            Steps.Add(new StepModel
-            {
-                Title = Resx.AppResources.TextStep2Title,
-                Image = ImageSource.FromResource("Covid19Radar.Images.description-step2.png"),
-                Description = Resx.AppResources.TextStep2Description,
-                StepNumber = 2
-            });
-            Steps.Add(new StepModel
-            {
-                Title = Resx.AppResources.TextStep3Title,
-                Image = ImageSource.FromResource("Covid19Radar.Images.description-step3.png"),
-                Description = Resx.AppResources.TextStep3Description,
-                StepNumber = 3
-            });
-            Steps.Add(new StepModel
-            {
-                Title = Resx.AppResources.TextStep4Title,
-                Image = ImageSource.FromResource("Covid19Radar.Images.description-step4.png"),
-                Description = Resx.AppResources.TextStep4Description
-            });
         }
     }
 }
