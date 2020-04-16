e36cbb8b3859ea7afd905a93613cd0f4cd361dde
Colin Bacon <iambacon@users.noreply.github.com>
2020-04-16 09:47:14 +0800

    Update how it works page with new design

---
+++ Covid19Radar/Covid19Radar/Model/StepModel.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Covid19Radar.Model
+{
+    public class StepModel
+    {
+        public string Description { get; set; }
+        public bool HasStepNumber => StepNumber != null;
+        public ImageSource Image { get; set; }
+        public int? StepNumber { get; set; }
+        public string Title { get; set; }
+    }
+}
