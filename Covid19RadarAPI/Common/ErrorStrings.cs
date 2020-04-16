eae4e821bec0eb57fb71985397f5cca5b4d2c232
Lohith <lohith@xamariners.com>
2020-04-16 07:03:42 +0000

    Merged PR 34: [chore] PBI:107 - Otp Validate Api Endpoint implementation
    [chore] PBI:107 - Otp Validate Api Endpoint implementation
    
    Related work items: #107

---
+++ Covid19RadarAPI/Common/ErrorStrings.cs
@@ -0,0 +1,16 @@
+﻿namespace Covid19Radar.Common
+{
+    public class ErrorStrings
+    {
+
+        //1000: User error strings
+        public const string UserNotFound = "1001: User not found.";
+
+        //2000: OTP error strings
+        public const string OtpInvalidValidateRequest = "2001: Invalid Otp validate request.";
+        public const string OtpSmsSendFailure = "2001: Failed to send SMS.";
+
+        //9000: System error strings
+        public const string PayloadInvalid = "9001: Invalid payload.";
+    }
+}
