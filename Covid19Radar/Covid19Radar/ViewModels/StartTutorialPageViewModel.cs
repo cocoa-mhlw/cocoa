c6299687a393f4812d80760def2ceb576d3ae0ba
Pedro Costa <pedrofcj@gmail.com>
2020-04-16 15:26:55 +0800

    Added logo image for first page Added style on Android project to disable AllCaps Changed TopMainImg for a bigger one with more quality Added extension to use image from resource from XAML Added 3 new entries on text resource files

--- Covid19Radar/Covid19Radar/ViewModels/StartTutorialPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/StartTutorialPageViewModel.cs
@@ -1,34 +1,16 @@
-﻿using Covid19Radar.Model;
-using Prism.Commands;
-using Prism.Mvvm;
-using Prism.Navigation;
-using Prism.Services;
-using Prism.Services.Dialogs;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
+﻿using Prism.Navigation;
 using Xamarin.Forms;
 
 namespace Covid19Radar.ViewModels
 {
     public class StartTutorialPageViewModel : ViewModelBase
     {
-        private INavigationService _navigationService;
-        public string TextStopTheSpreadOfCOVID19 { get; set; }
-        public string TextWeWillHelpAgenciesContact { get; set; }
-        public string TextToProtectThoseAroundYou { get; set; }
-        public string ButtonIWantToHelp { get; set; }
-
+        private readonly INavigationService _navigationService;
+        
         public StartTutorialPageViewModel(INavigationService navigationService)
             : base(navigationService)
         {
             _navigationService = navigationService;
-            TextStopTheSpreadOfCOVID19 = Resx.AppResources.TextStopTheSpreadOfCOVID19;
-            TextWeWillHelpAgenciesContact = Resx.AppResources.TextWeWillHelpAgenciesContact;
-            TextToProtectThoseAroundYou = Resx.AppResources.TextToProtectThoseAroundYou;
-            ButtonIWantToHelp = Resx.AppResources.ButtonIWantToHelp;
         }
 
         public Command OnClickNext => (new Command(() =>
