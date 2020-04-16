9e279df9663cec60c2ce9074dad383d66a63930d
Udara Alwis <udara@xamariners.com>
2020-04-16 17:15:51 +0800

    remove commented bits

--- Covid19Radar/Covid19Radar/Common/AppUtils.cs
+++ Covid19Radar/Covid19Radar/Common/AppUtils.cs
@@ -10,11 +10,6 @@ namespace Covid19Radar.Common
             var status = await Permissions.CheckStatusAsync<Permissions.LocationAlways>();
             if (status != PermissionStatus.Granted)
             {
-                //if (await CrossPermissions.Current.ShouldShowRequestPermissionRationaleAsync(Permission.LocationAlways))
-                //{
-                //    await Application.Current.MainPage.DisplayAlert("Need location", "This Application need BLE location", "OK");
-                //}
-
                 status = await Permissions.RequestAsync<Permissions.LocationAlways>();
             }
         }
