7bd6c1f7a715074c95e7dc758260c8940899d8e6
Albilaga <albilaga.lp@gmail.com>
2020-04-17 10:25:57 +0700

    Add OTP Entry.

---
+++ Covid19Radar/Covid19Radar/Controls/OtpEntry.xaml.cs
@@ -0,0 +1,34 @@
+﻿using Xamarin.Forms;
+
+namespace Covid19Radar.Controls
+{
+    public partial class OtpEntry : ContentView
+    {
+        public OtpEntry()
+        {
+            InitializeComponent();
+        }
+
+        public static readonly BindableProperty TitleProperty =
+            BindableProperty.Create(nameof(Text),
+                typeof(string),
+                typeof(OtpEntry),
+                null,
+                defaultBindingMode: BindingMode.TwoWay,
+                propertyChanged: TextChanged);
+
+        private static void TextChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            ((OtpEntry)bindable).Entry.Text = (string)newValue;
+        }
+
+        /// <summary>
+        /// Text for the OTP.
+        /// </summary>
+        public string Text
+        {
+            get => (string)GetValue(TitleProperty);
+            set => SetValue(TitleProperty, value);
+        }
+    }
+}
