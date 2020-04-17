eb50690d8fe111aad0c65073e428e73450d092b6
Pedro Costa <pedrofcj@gmail.com>
2020-04-17 11:45:44 +0800

    Fixed setup first page allignment Added setup completed page Added more styles for labels Changed font sizes on styles to named sizes

---
+++ Covid19Radar/Covid19Radar/Views/SetupCompletedPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Covid19Radar.Views
+{
+    public partial class SetupCompletedPage : ContentPage
+    {
+        public SetupCompletedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
