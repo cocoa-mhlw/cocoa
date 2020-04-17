ee2ba384e3e0639510f2b6db42cafdf444b0e06c
Albilaga <albilaga.lp@gmail.com>
2020-04-17 10:18:48 +0700

    Add Borderless Entry.

---
+++ Covid19Radar/Covid19Radar.Android/Renderers/BorderlessEntryRenderer.cs
@@ -0,0 +1,27 @@
+﻿using Android.Content;
+using Covid19Radar.Droid.Renderers;
+using Covid19Radar.Renderers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(BorderlessEntry), typeof(BorderlessEntryRenderer))]
+namespace Covid19Radar.Droid.Renderers
+{
+    public class BorderlessEntryRenderer : EntryRenderer
+    {
+        public BorderlessEntryRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement == null)
+            {
+                Control.Background = null;
+                Control.SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
