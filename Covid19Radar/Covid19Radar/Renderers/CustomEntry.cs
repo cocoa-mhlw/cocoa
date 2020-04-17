8992f7143e1bfe93afa8f63f9e0b486819ca925c
Albilaga <albilaga.lp@gmail.com>
2020-04-17 14:50:43 +0700

    Detect Delete Click On Android.

--- Covid19Radar/Covid19Radar/Renderers/CustomEntry.cs
+++ Covid19Radar/Covid19Radar/Renderers/CustomEntry.cs
@@ -1,9 +1,12 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Xamarin.Forms;
 
 namespace Covid19Radar.Renderers
 {
     public class CustomEntry : Entry
     {
+        public event EventHandler DeleteClicked;
+
         public static readonly BindableProperty BorderColorProperty =
            BindableProperty.Create(nameof(BorderColor),
                typeof(Color),
@@ -17,6 +20,11 @@ namespace Covid19Radar.Renderers
         {
             get => (Color)GetValue(BorderColorProperty);
             set => SetValue(BorderColorProperty, value);
+        }
+
+        public void TriggerDeleteClicked()
+        {
+            DeleteClicked?.Invoke(this, null);
         }
     }
 }
