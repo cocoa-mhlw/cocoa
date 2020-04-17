8b53c0c507b3747c840759718b7dd8ff3f3bcfbb
Colin Bacon <iambacon@users.noreply.github.com>
2020-04-17 15:52:45 +0800

    Add phonenumber property

--- Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
@@ -5,14 +5,32 @@ namespace Covid19Radar.ViewModels
 {
     public class UserSettingPageViewModel : ViewModelBase
     {
+        private string _phoneNumber;
+
+        public const int PhoneNumberMaxLength = 10;
+
+        public string PhoneNumber
+        {
+            get => _phoneNumber;
+            set
+            {
+                SetProperty(ref _phoneNumber, value);
+                RaisePropertyChanged(nameof(IsPhoneNumberValid));
+            }
+        }
+
+        public bool IsPhoneNumberValid => !string.IsNullOrWhiteSpace(PhoneNumber);
+
         public UserSettingPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = Resx.AppResources.TitleStatusSettings;
         }
 
-        public Command OnChangeStatusOverInfection => (new Command(() =>
+        public override void OnNavigatedTo(INavigationParameters parameters)
         {
-            NavigationService.NavigateAsync("SmsVerificationPage");
-        }));
+            PhoneNumber = string.Empty;
+        }
+
+        public Command OnClickNext => new Command(() => NavigationService.NavigateAsync("SmsVerificationPage"), () => IsPhoneNumberValid);
     }
 }
