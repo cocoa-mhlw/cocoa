4513482b02f5b9c70e65926784339f2c6a9003f0
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-18 17:27:52 +0900

    Enable AppCenter Enable FFCachedImage Replace FFCachedImage each PCL Resources

--- Covid19Radar/Covid19Radar/App.xaml.cs
+++ Covid19Radar/Covid19Radar/App.xaml.cs
@@ -24,6 +24,10 @@ using Prism.Logging.AppCenter;
 using Prism.Logging;
 using System.Collections.Generic;
 using System.Text;
+using Microsoft.AppCenter.Push;
+using Prism.Plugin.Popups;
+using FFImageLoading.Helpers;
+using FFImageLoading;
 
 /* 
  * Our mission...is 
@@ -37,21 +41,12 @@ namespace Covid19Radar
     public partial class App : PrismApplication
     {
         private IBeaconService _beaconService;
-        const string LogTag = "Covid19Radar";
-        /* 
-         * The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
-         * This imposes a limitation in which the App class must have a default constructor. 
-         * App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
-         */
-        static App()
-        {
-            /*
-            Crashes.SendingErrorReport += SendingErrorReportHandler;
-            Crashes.SentErrorReport += SentErrorReportHandler;
-            Crashes.FailedToSendErrorReport += FailedToSendErrorReportHandler;
-            */
-        }
 
+        /* 
+* The Xamarin Forms XAML Previewer in Visual Studio uses System.Activator.CreateInstance.
+* This imposes a limitation in which the App class must have a default constructor. 
+* App(IPlatformInitializer initializer = null) cannot be handled by the Activator.
+*/
         public App() : this(null) { }
 
         public App(IPlatformInitializer initializer) : base(initializer, setFormsDependencyResolver: true) { }
@@ -59,34 +54,16 @@ namespace Covid19Radar
         protected override async void OnInitialized()
         {
             InitializeComponent();
-            INavigationResult result;
+            LogUnobservedTaskExceptions();
 
-            // AppCenter
-            /*
-            AppCenter.LogLevel = LogLevel.Debug;
-            Crashes.ShouldProcessErrorReport = ShouldProcess;
-            Crashes.ShouldAwaitUserConfirmation = ConfirmationHandler;
-            Crashes.GetErrorAttachments = GetErrorAttachments;
             Distribute.ReleaseAvailable = OnReleaseAvailable;
-            AppCenter.Start($"android={AppConstants.AppCenterTokensAndroid};ios={AppConstants.AppCenterTokensIOS};", typeof(Analytics), typeof(Crashes), typeof(Distribute));
-            await AppCenter.GetInstallIdAsync().ContinueWith(installId =>
-            {
-                AppCenterLog.Info(LogTag, "AppCenter.InstallId=" + installId.Result);
-            });
-            await Crashes.HasCrashedInLastSessionAsync().ContinueWith(hasCrashed =>
-            {
-                AppCenterLog.Info(LogTag, "Crashes.HasCrashedInLastSession=" + hasCrashed.Result);
-            });
-            await Crashes.GetLastSessionCrashReportAsync().ContinueWith(report =>
-            {
-                AppCenterLog.Info(LogTag, "Crashes.LastSessionCrashReport.Exception=" + report.Result?.StackTrace);
-            });
+            Push.PushNotificationReceived += OnPushNotificationReceived;
+            AppCenter.Start($"android={AppConstants.AppCenterTokensAndroid};ios={AppConstants.AppCenterTokensIOS};", typeof(Analytics), typeof(Crashes), typeof(Distribute), typeof(Push));
             Container.Resolve<ILogger>().Log("Started App Center");
-            */
 
+            INavigationResult result;
             // Check user data and skip tutorial
             UserDataService userDataService = Xamarin.Forms.DependencyService.Resolve<UserDataService>();
-            //UserDataModel userData =await userDataService.Register();
 
             if (userDataService.IsExistUserData)
             {
@@ -118,11 +95,11 @@ namespace Covid19Radar
         protected override void RegisterTypes(IContainerRegistry containerRegistry)
         {
             // logger
-            /*
+            containerRegistry.RegisterPopupNavigationService();
             var logger = new AppCenterLogger();
             containerRegistry.RegisterInstance<ILogger>(logger);
             containerRegistry.RegisterInstance<ILoggerFacade>(logger);
-            */
+            containerRegistry.RegisterSingleton<IMiniLogger, FFImageLoadingLogger>();
 
             // Viewmodel
             containerRegistry.RegisterForNavigation<NavigationPage>();
@@ -143,11 +120,10 @@ namespace Covid19Radar
 
         protected override void OnStart()
         {
-            base.OnStart();
+            ImageService.Instance.Config.Logger = Container.Resolve<IMiniLogger>();
         }
 
 
-        // LogUnobservedTaskExceptions
         private void LogUnobservedTaskExceptions()
         {
             TaskScheduler.UnobservedTaskException += (sender, e) =>
@@ -156,147 +132,68 @@ namespace Covid19Radar
             };
         }
 
-        /*
-
-        static void SendingErrorReportHandler(object sender, SendingErrorReportEventArgs e)
+        private void OnPushNotificationReceived(object sender, PushNotificationReceivedEventArgs e)
         {
-            AppCenterLog.Info(LogTag, "Sending error report");
+            // Add the notification message and title to the message
+            var summary = $"Push notification received:" +
+                $"\n\tNotification title: {e.Title}" +
+                $"\n\tMessage: {e.Message}";
 
-            var args = e as SendingErrorReportEventArgs;
-            ErrorReport report = args.Report;
+            // If there is custom data associated with the notification,
+            // print the entries
+            if (e.CustomData != null)
+            {
+                summary += "\n\tCustom data:\n";
+                foreach (var key in e.CustomData.Keys)
+                {
+                    summary += $"\t\t{key} : {e.CustomData[key]}\n";
+                }
+            }
 
-            //test some values
-            if (report.StackTrace != null)
-            {
-                AppCenterLog.Info(LogTag, report.StackTrace.ToString());
-            }
-            else if (report.AndroidDetails != null)
-            {
-                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
-            }
+            // Send the notification summary to debug output
+            System.Diagnostics.Debug.WriteLine(summary);
+            Container.Resolve<ILoggerFacade>().Log(summary, Category.Debug, Priority.None);
         }
 
-        static void SentErrorReportHandler(object sender, SentErrorReportEventArgs e)
+        private bool OnReleaseAvailable(ReleaseDetails releaseDetails)
         {
-            AppCenterLog.Info(LogTag, "Sent error report");
+            // Look at releaseDetails public properties to get version information, release notes text or release notes URL
+            string versionName = releaseDetails.ShortVersion;
+            string versionCodeOrBuildNumber = releaseDetails.Version;
+            string releaseNotes = releaseDetails.ReleaseNotes;
+            Uri releaseNotesUrl = releaseDetails.ReleaseNotesUrl;
 
-            var args = e as SentErrorReportEventArgs;
-            ErrorReport report = args.Report;
+            // custom dialog
+            var title = "Version " + versionName + " available!";
+            Task answer;
 
-            //test some values
-            if (report.StackTrace != null)
+            // On mandatory update, user cannot postpone
+            if (releaseDetails.MandatoryUpdate)
             {
-                AppCenterLog.Info(LogTag, report.StackTrace.ToString());
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install");
             }
             else
             {
-                AppCenterLog.Info(LogTag, "No system exception was found");
+                answer = Current.MainPage.DisplayAlert(title, releaseNotes, "Download and Install", "Maybe tomorrow...");
             }
-
-            if (report.AndroidDetails != null)
+            answer.ContinueWith((task) =>
             {
-                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
-            }
-        }
-
-        static void FailedToSendErrorReportHandler(object sender, FailedToSendErrorReportEventArgs e)
-        {
-            AppCenterLog.Info(LogTag, "Failed to send error report");
-
-            var args = e as FailedToSendErrorReportEventArgs;
-            ErrorReport report = args.Report;
-
-            //test some values
-            if (report.StackTrace  != null)
-            {
-                AppCenterLog.Info(LogTag, report.StackTrace.ToString());
-            }
-            else if (report.AndroidDetails != null)
-            {
-                AppCenterLog.Info(LogTag, report.AndroidDetails.ThreadName);
-            }
-
-            if (e.Exception != null)
-            {
-                AppCenterLog.Info(LogTag, "There is an exception associated with the failure");
-            }
-        }
-
-        bool ShouldProcess(ErrorReport report)
-        {
-            AppCenterLog.Info(LogTag, "Determining whether to process error report");
-            return true;
-        }
-
-        bool ConfirmationHandler()
-        {
-            Xamarin.Forms.Device.BeginInvokeOnMainThread(() =>
-            {
-                Current.MainPage.DisplayActionSheet("Crash detected. Send anonymous crash report?", null, null, "Send", "Always Send", "Don't Send").ContinueWith((arg) =>
+                // If mandatory or if answer was positive
+                if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
                 {
-                    var answer = arg.Result;
-                    UserConfirmation userConfirmationSelection;
-                    if (answer == "Send")
-                    {
-                        userConfirmationSelection = UserConfirmation.Send;
-                    }
-                    else if (answer == "Always Send")
-                    {
-                        userConfirmationSelection = UserConfirmation.AlwaysSend;
-                    }
-                    else
-                    {
-                        userConfirmationSelection = UserConfirmation.DontSend;
-                    }
-                    AppCenterLog.Debug(LogTag, "User selected confirmation option: \"" + answer + "\"");
-                    Crashes.NotifyUserConfirmation(userConfirmationSelection);
-                });
-            });
-
-            return true;
-        }
-
-        IEnumerable<ErrorAttachmentLog> GetErrorAttachments(ErrorReport report)
-        {
-            return new ErrorAttachmentLog[]
-            {
-//                ErrorAttachmentLog.AttachmentWithText("Hello world!", "hello.txt"),
-//                ErrorAttachmentLog.AttachmentWithBinary(Encoding.UTF8.GetBytes("Fake image"), "fake_image.jpeg", "image/jpeg")
-            };
-        }
-
-        bool OnReleaseAvailable(ReleaseDetails releaseDetails)
-        {
-            AppCenterLog.Info(LogTag, "OnReleaseAvailable id=" + releaseDetails.Id
-                                            + " version=" + releaseDetails.Version
-                                            + " releaseNotesUrl=" + releaseDetails.ReleaseNotesUrl);
-            var custom = releaseDetails.ReleaseNotes?.ToLowerInvariant().Contains("custom") ?? false;
-            if (custom)
-            {
-                var title = "Version " + releaseDetails.ShortVersion + " available!";
-                Task answer;
-                if (releaseDetails.MandatoryUpdate)
-                {
-                    answer = Current.MainPage.DisplayAlert(title, releaseDetails.ReleaseNotes, "Update now!");
+                    // Notify SDK that user selected update
+                    Distribute.NotifyUpdateAction(UpdateAction.Update);
                 }
                 else
                 {
-                    answer = Current.MainPage.DisplayAlert(title, releaseDetails.ReleaseNotes, "Update now!", "Maybe tomorrow...");
+                    // Notify SDK that user selected postpone (for 1 day)
+                    // Note that this method call is ignored by the SDK if the update is mandatory
+                    Distribute.NotifyUpdateAction(UpdateAction.Postpone);
                 }
-                answer.ContinueWith((task) =>
-                {
-                    if (releaseDetails.MandatoryUpdate || (task as Task<bool>).Result)
-                    {
-                        Distribute.NotifyUpdateAction(UpdateAction.Update);
-                    }
-                    else
-                    {
-                        Distribute.NotifyUpdateAction(UpdateAction.Postpone);
-                    }
-                });
-            }
-            return custom;
+            });
+
+            // Return true if you are using your own dialog, false otherwise
+            return true;
         }
-*/
     }
 }
