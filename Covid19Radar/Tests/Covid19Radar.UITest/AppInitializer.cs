8f4b4e8db71b8849b563a6d8be2fb25c2f774fcd
Pedro Costa <pedrofcj@gmail.com>
2020-04-20 11:25:31 +0800

    Added UI test project and a sample on first page

---
+++ Covid19Radar/Tests/Covid19Radar.UITest/AppInitializer.cs
@@ -0,0 +1,52 @@
+﻿using Xamarin.UITest;
+using Xamariners.EndToEnd.Xamarin.Models;
+
+namespace Covid19Radar.UITest
+{
+    public class AppInitializer
+    {
+
+        public static IApp StartApp(Platform platform, RunnerConfiguration runnerConfiguration)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp
+                    .Android
+                    .ApkFile(runnerConfiguration.AndroidConfiguration.AppPath)
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            if (runnerConfiguration.IosConfiguration.Simulator)
+            {
+                return ConfigureApp
+                    .iOS
+                    .DeviceIdentifier(runnerConfiguration.IosConfiguration.SimulatorIdentifier)
+                    .AppBundle(runnerConfiguration.IosConfiguration.AppPath)
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            var iosDeviceIdentifier = runnerConfiguration.IosConfiguration.DeviceIdentifier;
+            var iosBundleName = runnerConfiguration.IosConfiguration.BundleName;
+
+            return ConfigureApp
+                .iOS
+                .DeviceIdentifier(iosDeviceIdentifier)
+                .InstalledApp(iosBundleName)
+                .PreferIdeSettings()
+                .EnableLocalScreenshots()
+                .StartApp();
+        }
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                return ConfigureApp.Android.StartApp();
+            }
+
+            return ConfigureApp.iOS.StartApp();
+        }
+
+    }
+}
