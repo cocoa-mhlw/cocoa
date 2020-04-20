b1bc0b7ff3433f150f215e3fb8a4dfa6192a734a
Udara Alwis <udara@xamariners.com>
2020-04-20 13:13:16 +0800

    Implementing Beacon data list page

--- Covid19Radar/Covid19Radar/ViewModels/DetectedBeaconPageViewmodel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/DetectedBeaconPageViewmodel.cs
@@ -1,8 +1,11 @@
-﻿using Prism.Commands;
+﻿using Covid19Radar.Model;
+using Covid19Radar.Services;
+using Prism.Commands;
 using Prism.Mvvm;
 using Prism.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using Xamarin.Forms;
@@ -10,20 +13,38 @@ using Xamarin.Forms;
 namespace Covid19Radar.ViewModels
 {
     public class DetectedBeaconPageViewmodel : ViewModelBase
-    {
+    { 
         private INavigationService _navigationService;
-        public DetectedBeaconPageViewmodel(INavigationService navigationService)
+        private IBeaconService _beaconService;
+
+
+        public ObservableCollection<BeaconDataModel> BeaconDataList { get; set; }
+
+
+        public DetectedBeaconPageViewmodel(INavigationService navigationService, IBeaconService beaconService)
             : base(navigationService)
         {
             _navigationService = navigationService;
+            _beaconService = beaconService;
+
             Title = "Detected Beacon List";
+            BeaconDataList = new ObservableCollection<BeaconDataModel>();
         }
 
-        public Command OnClickPrev => (new Command(() =>
+        public override async void OnNavigatedFrom(INavigationParameters parameters)
         {
-            _navigationService.NavigateAsync("HomePage");
+            base.OnNavigatedFrom(parameters);
+
+            if (parameters.GetNavigationMode() == NavigationMode.Forward)
+            {
+                var beaconList = _beaconService.GetBeaconData();
+                BeaconDataList = new ObservableCollection<BeaconDataModel>(beaconList);
+            }
+        }
+
+        public Command OnClickPrev => (new Command(async () =>
+        {
+            await _navigationService.GoBackAsync();
         }));
-
-
     }
 }
