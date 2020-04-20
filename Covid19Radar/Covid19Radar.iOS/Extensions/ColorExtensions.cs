4543b50796675a3f04ba0427ec71c27cf2dbf333
Albilaga <albilaga.lp@gmail.com>
2020-04-20 12:25:48 +0700

    Fix Placeholder Color In Custom Entry.

---
+++ Covid19Radar/Covid19Radar.iOS/Extensions/ColorExtensions.cs
@@ -0,0 +1,16 @@
+﻿using UIKit;
+
+namespace Covid19Radar.iOS.Extensions
+{
+    public class ColorExtensions
+    {
+		internal static UIColor LabelColor
+		{
+			get
+			{
+                return UIColor.Black;
+            }
+		}
+
+	}
+}
