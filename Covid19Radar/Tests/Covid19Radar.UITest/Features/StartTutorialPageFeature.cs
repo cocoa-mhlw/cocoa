a26e9653a0823a1c82e68931d28ea651ba8ea961
Pedro Costa <pedrofcj@gmail.com>
2020-04-20 14:15:23 +0800

    Changed to Nuget package Added iOS test fixture

--- Covid19Radar/Tests/Covid19Radar.UITest/Features/StartTutorialPageFeature.cs
+++ Covid19Radar/Tests/Covid19Radar.UITest/Features/StartTutorialPageFeature.cs
@@ -6,7 +6,7 @@ namespace Covid19Radar.UITest
 {
 
     [TestFixture(Platform.Android)]
-    //[TestFixture(Platform.iOS)]
+    [TestFixture(Platform.iOS)]
     public partial class StartTutorialPageFeature : BaseLocalFeature
     {
         public StartTutorialPageFeature(Platform platform) : base(platform)
