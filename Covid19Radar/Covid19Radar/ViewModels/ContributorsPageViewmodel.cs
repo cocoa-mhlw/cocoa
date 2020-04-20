472e0a13bfc60ebdbc84b7c92eff0534f44c9665
Colin Bacon <iambacon@users.noreply.github.com>
2020-04-20 17:19:15 +0800

    Add UI for contributors page

---
+++ Covid19Radar/Covid19Radar/ViewModels/ContributorsPageViewmodel.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Covid19Radar.Model;
+using Prism.Navigation;
+
+namespace Covid19Radar.ViewModels
+{
+    public class ContributorsPageViewModel : ViewModelBase
+    {
+        public List<ContributorModel> Contributors { get; set; }
+
+        public ContributorsPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = Resx.AppResources.TitleContributorsPage;
+            SetData();
+        }
+
+        private void SetData()
+        {
+            Contributors = new List<ContributorModel>
+            {
+                new ContributorModel { Fullname = "Kazumi Hirose", Description = "Originator" },
+                new ContributorModel { Fullname = "Kristina Yasuda", Description = "Public Relations and Activity" },
+                new ContributorModel { Fullname = "Tsuyoshi Ushio", Description = "Server side / Azure Functions" },
+                new ContributorModel { Fullname = "Yasuaki Matsuda", Description = "Server side / Azure Functions" },
+                new ContributorModel { Fullname = "Noriko Matsumoto", Description = "Design UI/UX / Main Site Designer" },
+                new ContributorModel { Fullname = "Kazuki Ota", Description = "Create Build pipeline on AppCenter" },
+                new ContributorModel { Fullname = "Takeshi Sakurai ", Description = "Client iOS" },
+                new ContributorModel { Fullname = "Fumiya Kume", Description = "Client Xamarin" },
+                new ContributorModel { Fullname = "Taiki Yoshida", Description = "Power BI / Power Apps" },
+                new ContributorModel { Fullname = "Takayuki Hirose", Description = "Device Mac Lending" },
+                new ContributorModel { Fullname = "Tomoaki Ueno", Description = "Medical Information and FieAdvisory" },
+                new ContributorModel { Fullname = "Takashi Takebayashi", Description = "Translate" }
+            };
+        }
+    }
+}
---
+++ Covid19Radar/Covid19Radar/Views/ContributorsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Covid19Radar.Views
+{
+    public partial class ContributorsPage : ContentPage
+    {
+        public ContributorsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
