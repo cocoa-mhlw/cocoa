de9ad34a367d5b898b71fbf3979efd14dacca605
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-20 23:29:56 +0900

    Beacon list work

---
+++ Covid19Radar/Covid19Radar/Templates/BeaconItemTemplate .xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Covid19Radar.Templates
+{
+    public partial class BeaconItemTemplate
+    {
+        public BeaconItemTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}
