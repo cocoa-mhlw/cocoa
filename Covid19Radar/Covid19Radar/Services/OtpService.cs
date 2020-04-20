03a1e929378f023613dc6d5e4907d9f317fd8c9f
Colin Bacon <iambacon@users.noreply.github.com>
2020-04-20 12:07:57 +0800

    Request OTP when user adds phone number and presses next

---
+++ Covid19Radar/Covid19Radar/Services/OtpService.cs
@@ -0,0 +1,20 @@
+﻿using System.Threading.Tasks;
+using Covid19Radar.Model;
+
+namespace Covid19Radar.Services
+{
+    public class OTPService
+    {
+        private readonly HttpDataService _httpDataService;
+
+        public OTPService()
+        {
+            _httpDataService = Xamarin.Forms.DependencyService.Resolve<HttpDataService>();
+        }
+
+        public Task SendOTPAsync(UserDataModel user, string phoneNumber)
+        {
+            return _httpDataService.PostOTPAsync(user, phoneNumber);
+        }
+    }
+}
