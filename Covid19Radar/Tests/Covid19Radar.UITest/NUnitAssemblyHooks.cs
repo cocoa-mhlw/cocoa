07c4d1c7dc196b1e7f7c2dc22ef18a2f05e60094
Pedro Costa <pedrofcj@gmail.com>
2020-04-20 11:25:31 +0800

    Added UI test project and a sample on first page

---
+++ Covid19Radar/Tests/Covid19Radar.UITest/NUnitAssemblyHooks.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using TechTalk.SpecFlow;
+
+namespace Covid19Radar.UITest
+{
+    [SetUpFixture]
+    public class NUnitAssemblyHooks
+    {
+        [OneTimeSetUp]
+        public void AssemblyInitialize()
+        {
+            var currentAssembly = typeof(NUnitAssemblyHooks).Assembly;
+
+            TestRunnerManager.OnTestRunStart(currentAssembly);
+        }
+
+        [OneTimeTearDown]
+        public void AssemblyCleanup()
+        {
+            var currentAssembly = typeof(NUnitAssemblyHooks).Assembly;
+
+            TestRunnerManager.OnTestRunEnd(currentAssembly);
+        }
+    }
+}
