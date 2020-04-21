e402785cee97e52668d3f675677d50ca875499a1
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-22 01:14:49 +0900

    Fix a bit

--- Covid19Radar/Covid19Radar.Android/MainActivity.cs
+++ Covid19Radar/Covid19Radar.Android/MainActivity.cs
@@ -39,12 +39,6 @@ namespace Covid19Radar.Droid
             Xamarin.Essentials.Platform.Init(this, bundle);
             global::Rg.Plugins.Popup.Popup.Init(this, bundle);
 
-            /// <summary>
-            /// Please call this before Forms is initialized to make sure assemblies link properly.
-            /// Make sure to perform this step on each platform.
-            /// </summary>
-            FormsWebViewRenderer.Initialize();
-
             global::Xamarin.Forms.Forms.Init(this, bundle);
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init(enableFastRenderer: true);
             global::FFImageLoading.ImageService.Instance.Initialize(new FFImageLoading.Config.Configuration()
