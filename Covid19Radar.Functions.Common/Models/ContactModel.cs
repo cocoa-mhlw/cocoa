48dbda24d1127efb104be92e62355a7831a3cfff
Yasuaki Matsuda <dark_crash@hotmail.com>
2020-04-21 05:20:10 +0000

    Merged PR 83: add Contact Function
    ## Add Contact Function
    - Cosmos Trigger function.
    - Remove not use functions.
    
    Related work items: #155, #156

---
+++ Covid19Radar.Functions.Common/Models/ContactModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Models
+{
+    public class ContactModel
+    {
+        /// <summary>
+        /// for Cosmos DB
+        /// </summary>
+        public string id { get; set; }
+        /// <summary>
+        /// PartitionKey
+        /// </summary>
+        public string PartitionKey { get; set; }
+        /// <summary>
+        /// The splited timespan.
+        /// </summary>
+        public string KeyTime { get; set; }
+
+        public BeaconModel Beacon1 { get; set; }
+        public BeaconModel Beacon2 { get; set; }
+
+    }
+}
