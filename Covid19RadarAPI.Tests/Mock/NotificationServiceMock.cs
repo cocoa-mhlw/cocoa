4c4bacea7a6f356cc2ef865ac14a6eef712a54e7
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-25 00:15:12 +0900

    Fix Build Error for Functions tests project

---
+++ Covid19RadarAPI.Tests/Mock/NotificationServiceMock.cs
@@ -0,0 +1,12 @@
+﻿using Covid19Radar.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Tests.Mock
+{
+    public class NotificationServiceMock : INotificationService
+    {
+        public DateTime LastNotificationTime { get; set; } = DateTime.MinValue;
+    }
+}
