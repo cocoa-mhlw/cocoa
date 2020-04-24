dafcaa1e055ad84a5854b80a6c88548c2d62195e
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-24 21:51:48 +0900

    Build error quick fix

---
+++ Covid19Radar/Covid19Radar/Model/Language.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Model
+{
+    public class Language
+    {
+        public string DisplayName
+        {
+            get;
+            set;
+        }
+        public string ShortName
+        {
+            get;
+            set;
+        }
+    }
+}
