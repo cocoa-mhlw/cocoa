de276b7c98ecb525a966e444f9a9591c6cacd24c
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-26 18:06:24 +0900

    Implement Change user status view and viewmodel

--- Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/UserSettingPageViewModel.cs
@@ -1,16 +1,34 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
+using Covid19Radar.Common;
+using Covid19Radar.Model;
 using Covid19Radar.Services;
 using Prism.Ioc;
 using Prism.Navigation;
 using Xamarin.Forms;
+using System.Reactive.Linq;
+using Reactive.Bindings;
+using System.Reactive.Disposables;
+using System;
+using Reactive.Bindings.Extensions;
 
 namespace Covid19Radar.ViewModels
 {
-    public class UserSettingPageViewModel : ViewModelBase
+    public class PickerItem
+    {
+        public int PickerCode { get; set; }
+        public string PickerName { get; set; }
+    }
+
+    public class UserSettingPageViewModel : ViewModelBase, IDisposable
     {
         private readonly OTPService _otpService;
         private readonly UserDataService _userDataService;
         private string _phoneNumber;
+
+        public List<string> UserStatuses { get; } = Enum.GetNames(typeof(UserStatus)).ToList();
+        public ReactiveProperty<UserStatus> SelectedUserStatus { get; }
+        private CompositeDisposable Disposable { get; } = new CompositeDisposable();
 
         public string PhoneNumber
         {
@@ -29,6 +47,14 @@ namespace Covid19Radar.ViewModels
             Title = Resources.AppResources.TitleStatusSettings;
             _otpService = App.Current.Container.Resolve<OTPService>();
             _userDataService = App.Current.Container.Resolve<UserDataService>();
+
+            SelectedUserStatus = new ReactiveProperty<UserStatus>().AddTo(this.Disposable);
+            this.SelectedUserStatus.ObserveProperty(x => x.Value)
+                .Subscribe(x =>
+                {
+                    Console.WriteLine(x);
+                })
+                .AddTo(this.Disposable);
         }
 
         public override void OnNavigatedTo(INavigationParameters parameters)
@@ -46,6 +72,10 @@ namespace Covid19Radar.ViewModels
         private string PhoneNumberWithoutMask()
         {
             return new string(("+81").Concat(PhoneNumber.Where(char.IsDigit).Skip(1)).ToArray());
+        }
+        public void Dispose()
+        {
+            this.Disposable.Dispose();
         }
     }
 }
