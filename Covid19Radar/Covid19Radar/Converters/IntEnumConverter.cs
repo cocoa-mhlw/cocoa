cf85777fb464d7fca5eef3e9ef25ef5346f8a153
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-26 18:06:24 +0900

    Implement Change user status view and viewmodel

---
+++ Covid19Radar/Covid19Radar/Converters/IntEnumConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Covid19Radar.Converters
+{
+    public class IntEnumConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is Enum)
+            {
+                return (int)value;
+            }
+            return 0;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                return Enum.ToObject(targetType, value);
+            }
+            return 0;
+        }
+    }
+}
