c8a346736250f3275b7202ed26175d9ed2993f66
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-28 00:26:02 +0900

    Implement CryptionService and mock Validation

---
+++ Covid19Radar.Functions.Common/Services/IValidationUserService.cs
@@ -0,0 +1,12 @@
+﻿using Covid19Radar.Models;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Covid19Radar.Services
+{
+    public interface IValidationUserService
+    {
+
+        Task<bool> ValidateAsync(HttpRequest req, IUser user);
+    }
+}
