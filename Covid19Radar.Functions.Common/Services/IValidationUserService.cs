521f5164f7114f4920f3170339f4b55da3394d9d
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-28 07:13:51 +0900

    Add Validation for UserApi

--- Covid19Radar.Functions.Common/Services/IValidationUserService.cs
+++ Covid19Radar.Functions.Common/Services/IValidationUserService.cs
@@ -1,5 +1,6 @@
 ﻿using Covid19Radar.Models;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
 namespace Covid19Radar.Services
@@ -7,6 +8,19 @@ namespace Covid19Radar.Services
     public interface IValidationUserService
     {
 
-        Task<bool> ValidateAsync(HttpRequest req, IUser user);
+        Task<ValidateResult> ValidateAsync(HttpRequest req, IUser user);
+
+        public struct ValidateResult
+        {
+            public static readonly ValidateResult Error = new ValidateResult()
+            {
+                IsValid = false,
+                ErrorActionResult = new BadRequestResult()
+            };
+            public bool IsValid;
+            public UserModel User;
+            public IActionResult ErrorActionResult;
+        }
+
     }
 }
