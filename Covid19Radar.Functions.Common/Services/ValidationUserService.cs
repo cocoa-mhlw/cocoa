9695d5ff9df096d1a76a86cf101eefd887db9716
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-28 07:13:51 +0900

    Add Validation for UserApi

--- Covid19Radar.Functions.Common/Services/ValidationUserService.cs
+++ Covid19Radar.Functions.Common/Services/ValidationUserService.cs
@@ -5,26 +5,86 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using System.Linq;
+using Covid19Radar.DataStore;
+using Microsoft.Azure.Cosmos;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Covid19Radar.Services
 {
     public class ValidationUserService : IValidationUserService
     {
+        const string AuthorizationType = "COVID-19-RADAR";
+        private readonly ICosmos Cosmos;
+        private readonly ICryptionService Cryption;
         private readonly ILogger<ValidationUserService> Logger;
         public ValidationUserService(
+            ICosmos cosmos,
+            ICryptionService cryption,
             Microsoft.Extensions.Configuration.IConfiguration config,
             ILogger<ValidationUserService> logger)
         {
-            this.Logger = logger;
+            Cosmos = cosmos;
+            Cryption = cryption;
+            Logger = logger;
         }
 
-        public async Task<bool> ValidateAsync(HttpRequest req, IUser user)
+
+        public async Task<IValidationUserService.ValidateResult> ValidateAsync(HttpRequest req, IUser user)
         {
+            if (string.IsNullOrWhiteSpace(user.UserUuid)
+             || string.IsNullOrWhiteSpace(user.Major)
+             || string.IsNullOrWhiteSpace(user.Minor))
+            {
+                return IValidationUserService.ValidateResult.Error;
+            }
+
             Microsoft.Extensions.Primitives.StringValues value;
-            if (!req.Headers.TryGetValue("Authorization", out value)) return false;
-            if (value.Count != 1) return false;
-            return true;
+            if (!req.Headers.TryGetValue("Authorization", out value)) return IValidationUserService.ValidateResult.Error;
+            if (value.Count != 1) return IValidationUserService.ValidateResult.Error;
+            var authorization = value.First();
+            if (!authorization.StartsWith(AuthorizationType)) return IValidationUserService.ValidateResult.Error;
+            var authorizationCode = authorization.Remove(0, AuthorizationType.Length + 1);
+            return await Query(req, user, authorizationCode);
         }
 
+        private async Task<IValidationUserService.ValidateResult> Query(HttpRequest req, IUser user, string authorizationCode)
+        {
+            try
+            {
+                var itemResult = await Cosmos.User.ReadItemAsync<UserModel>(user.GetId(), PartitionKey.None);
+                if (itemResult.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    var isValid = authorizationCode == Cryption.Unprotect(itemResult.Resource.ProtectSecret);
+                    return new IValidationUserService.ValidateResult() {
+                        IsValid = isValid,
+                        User = itemResult.Resource,
+                        ErrorActionResult = (!isValid ? new BadRequestResult() : null)
+                    };
+                }
+            }
+            catch (CosmosException ex)
+            {
+                // 429–TooManyRequests
+                if (ex.StatusCode == System.Net.HttpStatusCode.TooManyRequests)
+                {
+                    return new IValidationUserService.ValidateResult()
+                    {
+                        IsValid = false,
+                        ErrorActionResult = new StatusCodeResult(503)
+                    };
+                }
+                return new IValidationUserService.ValidateResult()
+                {
+                    IsValid = false,
+                    ErrorActionResult = new StatusCodeResult((int)ex.StatusCode)
+                };
+            }
+            return new IValidationUserService.ValidateResult()
+            {
+                IsValid = false,
+                ErrorActionResult = new BadRequestResult()
+            };
+        }
     }
 }
--- Covid19RadarAPI/Startup.cs
+++ Covid19RadarAPI/Startup.cs
@@ -22,6 +22,7 @@ namespace Covid19Radar
             builder.Services.AddSingleton<ICryptionService, CryptionService>();
             builder.Services.AddSingleton<DataStore.ICosmos, DataStore.Cosmos>();
             builder.Services.AddSingleton<INotificationService, NotificationService>();
+            builder.Services.AddSingleton<IValidationUserService, ValidationUserService>();
             builder.Services.AddSingleton<IOtpGenerator, OtpGenerator>();
             builder.Services.AddSingleton<IOtpService, OtpService>();
             builder.Services.AddSingleton<ISmsSender, SmsSender>();
