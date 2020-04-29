bd58cca40fce6286601ba08b51831ed6372551c9
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-04-29 10:21:24 +0900

    Create a dedicated project for an external interface and move resources

---
+++ src/Covid19Radar.Api.External/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Covid19Radar.Services;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using Microsoft.Extensions.Logging.Debug;
+using Microsoft.AspNetCore.Http;
+
+[assembly: FunctionsStartup(typeof(Covid19Radar.Startup))]
+
+namespace Covid19Radar
+{
+    public class Startup : FunctionsStartup
+    {
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            builder.Services.AddLogging();
+            builder.Services.AddHttpClient();
+            builder.Services.AddSingleton<ICryptionService, CryptionService>();
+            builder.Services.AddSingleton<DataStore.ICosmos, DataStore.Cosmos>();
+            builder.Services.AddSingleton<INotificationService, NotificationService>();
+            builder.Services.AddSingleton<IValidationUserService, ValidationUserService>();
+        }
+    }
+}
+
