50b22e0985194bf75c95a040d9990139a689983b
Kazumi Hirose <kazumihirose@hotmail.com>
2020-04-30 15:08:06 +0900

    Beacon Uuid Model and Get BeaconUuid

---
+++ Covid19Radar/Covid19Radar/Model/BeaconUuidModel.cs
@@ -0,0 +1,31 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Covid19Radar.Models
+{
+    public sealed class BeaconUuidModel
+    {
+        public BeaconUuidModel()
+        {
+        }
+
+        /// <summary>
+        /// for Cosmos DB id
+        /// </summary>
+        public string id;
+        /// <summary>
+        /// Beacon Uuid
+        /// </summary>
+        public string BeaconUuid;
+        /// <summary>
+        /// created timestamp UTC
+        /// </summary>
+        public DateTime CreateTime;
+        /// <summary>
+        /// Reloading Time UTC
+        /// </summary>
+        public DateTime EndTime;
+    }
+}
