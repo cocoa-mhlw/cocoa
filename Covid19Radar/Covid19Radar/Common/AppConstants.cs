b07541f83705aa146a0f835a0dd9dcf43cf94eca
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-02 21:35:28 +0900

    modify http method for api. track exception. some little fix.

--- Covid19Radar/Covid19Radar/Common/AppConstants.cs
+++ Covid19Radar/Covid19Radar/Common/AppConstants.cs
@@ -9,7 +9,7 @@ namespace Covid19Radar.Common
         public static readonly string AppCenterTokensAndroid = "APPCENTER_ANDROID";
         public static readonly string AppCenterTokensIOS = "APPCENTER_IOS";
 
-        public static readonly string ApiUserSecretKeyPrefix = "COVID-19-RADAR";
+        public static readonly string ApiUserSecretKeyPrefix = "Bearer";
 
         public static readonly string ApiBaseUrl = "https://covid19radar.azurewebsites.net/api";
         public static readonly string ApiSecret = "API_SECRET";
