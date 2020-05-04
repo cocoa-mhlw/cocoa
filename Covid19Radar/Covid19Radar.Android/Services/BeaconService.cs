3d90009d8b99dd351fe88721b152b72bbddcf14b
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-04 10:50:21 +0900

    change cache point

--- Covid19Radar/Covid19Radar.Android/Services/BeaconService.cs
+++ Covid19Radar/Covid19Radar.Android/Services/BeaconService.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using AltBeaconOrg.BoundBeacon;
 using AltBeaconOrg.BoundBeacon.Startup;
 using Android.App;
@@ -16,6 +17,7 @@ using Covid19Radar.Droid.Services;
 using Covid19Radar.Model;
 using Covid19Radar.Services;
 using Java.Util;
+using Plugin.CurrentActivity;
 using SQLite;
 using Xamarin.Forms;
 
@@ -24,35 +26,61 @@ namespace Covid19Radar.Droid.Services
 {
     public class BeaconService : IBeaconService, IDisposable
     {
-        private static object dataLock = new object();
-        private AltBeaconOrg.BoundBeacon.Region _fieldRegion;
+        private HttpDataService _httpDataService;
+        private MinutesTimer _uploadTimer;
         private UserDataModel _userData;
-        private RangeNotifier _rangeNotifier;
-        private MonitorNotifier _monitorNotifier;
-        private BeaconManager _beaconManager;
-        private readonly MainActivity _mainActivity;
-        private BeaconTransmitter _beaconTransmitter;
-        private readonly SQLiteConnection _connection;
-        private readonly UserDataService _userDataService;
-        private readonly HttpDataService _httpDataService;
-        private readonly MinutesTimer _uploadTimer;
+        private SQLiteConnection _connection;
 
         public BeaconService()
         {
-            _mainActivity = MainActivity.Instance;
-            _connection = DependencyService.Resolve<ISQLiteConnectionProvider>().GetConnection();
-            _connection.CreateTable<BeaconDataModel>();
-            _userDataService = DependencyService.Resolve<UserDataService>();
-            _userData = _userDataService.Get();
             _httpDataService = DependencyService.Resolve<HttpDataService>();
-            _uploadTimer = new MinutesTimer(_userData.GetJumpHashTimeDifference());
-            _uploadTimer.Start();
-            _uploadTimer.TimeOutEvent += TimerUpload;
+            _connection = DependencyService.Get<ISQLiteConnectionProvider>().GetConnection();
+            _connection.CreateTable<BeaconDataModel>();
         }
 
-        private async void TimerUpload(EventArgs e)
+        protected MainActivity MainActivity
+        {
+            get { return (MainActivity)CrossCurrentActivity.Current.Activity; }
+        }
+
+        public List<BeaconDataModel> GetBeaconData()
+        {
+            return MainActivity.GetBeaconData();
+        }
+
+        public async void StartRagingBeacons(UserDataModel userData)
+        {
+            _userData = userData;
+            BeaconUuidModel _beaconUuid = await _httpDataService.GetBeaconUuidAsync();
+
+            MainActivity.StartRagingBeacons(_beaconUuid);
+            /*
+            _uploadTimer = new MinutesTimer(_userData.GetJumpHashTimeDifference());
+            _uploadTimer.Start();
+            _uploadTimer.TimeOutEvent += TimerUploadAsync;
+            */
+        }
+
+        public void StopRagingBeacons()
+        {
+            MainActivity.StopRagingBeacons();
+        }
+
+        public async void StartAdvertisingBeacons(UserDataModel userData)
+        {
+            BeaconUuidModel _beaconUuid = await _httpDataService.GetBeaconUuidAsync();
+            MainActivity.StartAdvertisingBeacons(userData, _beaconUuid);
+        }
+
+        public void StopAdvertisingBeacons()
+        {
+            MainActivity.StopAdvertisingBeacons();
+        }
+
+        private async void TimerUploadAsync(EventArgs e)
         {
             System.Diagnostics.Debug.WriteLine(DateTime.Now.ToString());
+
             List<BeaconDataModel> beacons = _connection.Table<BeaconDataModel>().ToList();
             foreach (var beacon in beacons)
             {
@@ -60,227 +88,13 @@ namespace Covid19Radar.Droid.Services
                 if (!await _httpDataService.PostBeaconDataAsync(_userData, beacon)) continue;
 
                 var key = beacon.Id;
-                lock (dataLock)
+                lock (MainActivity.dataLock)
                 {
                     var b = _connection.Table<BeaconDataModel>().SingleOrDefault(x => x.Id == key);
                     b.IsSentToServer = true;
                     _connection.Update(b);
                 }
             }
-        }
-
-        public void RequestPermission()
-        {
-            if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
-            {
-                string[] permissions = new string[] {
-                    Android.Manifest.Permission.Bluetooth,
-                    Android.Manifest.Permission.BluetoothAdmin,
-                    Android.Manifest.Permission.AccessCoarseLocation,
-                    Android.Manifest.Permission.AccessFineLocation
-                };
-
-                _mainActivity.RequestPermissions(permissions, 0);
-            }
-        }
-
-        public BeaconManager BeaconManagerImpl
-        {
-            get
-            {
-                if (_beaconManager == null)
-                {
-                    _beaconManager = InitializeBeaconManager();
-                }
-                return _beaconManager;
-            }
-        }
-
-        public void InitializeService()
-        {
-            _beaconManager = InitializeBeaconManager();
-        }
-
-        private BeaconManager InitializeBeaconManager()
-        {
-            RequestPermission();
-            // Enable the BeaconManager 
-            _beaconManager = BeaconManager.GetInstanceForApplication(_mainActivity);
-
-            /*
-            #region Set up Beacon Simulator for TEST USE
-            // Beacon Simulator
-            var beaconSimulator = new BeaconSimulator();
-            beaconSimulator.CreateBasicSimulatedBeacons();
-            BeaconManager.BeaconSimulator = beaconSimulator;
-            // Beacon Simulator
-            #endregion Set up Beacon Simulator for TEST USE
-            */
-
-            _monitorNotifier = new MonitorNotifier();
-            _rangeNotifier = new RangeNotifier();
-
-            //iBeacon
-            BeaconParser beaconParser = new BeaconParser().SetBeaconLayout(AppConstants.iBeaconFormat);
-            _beaconManager.BeaconParsers.Add(beaconParser);
-
-            // BeaconManager Setting
-            // Check Touch おそらくmain activity beacon consumer側で設定
-            /*
-            _beaconManager.SetForegroundScanPeriod(AppConstants.BEACONS_UPDATES_IN_MILLISECONDS);
-            _beaconManager.SetForegroundBetweenScanPeriod(AppConstants.BEACONS_UPDATES_IN_MILLISECONDS);
-            _beaconManager.SetBackgroundScanPeriod(AppConstants.BEACONS_UPDATES_IN_MILLISECONDS);
-            _beaconManager.SetBackgroundBetweenScanPeriod(AppConstants.BEACONS_UPDATES_IN_MILLISECONDS);
-            _beaconManager.UpdateScanPeriods();
-            */
-
-            // MonitorNotifier
-            _monitorNotifier.DetermineStateForRegionComplete += DetermineStateForRegionComplete;
-            _monitorNotifier.EnterRegionComplete += EnterRegionComplete;
-            _monitorNotifier.ExitRegionComplete += ExitRegionComplete;
-            _beaconManager.AddMonitorNotifier(_monitorNotifier);
-
-            // RangeNotifier
-            _rangeNotifier.DidRangeBeaconsInRegionComplete += DidRangeBeaconsInRegionComplete;
-            _beaconManager.AddRangeNotifier(_rangeNotifier);
-
-
-            _fieldRegion = new AltBeaconOrg.BoundBeacon.Region("AppAppApp", Identifier.Parse(AppConstants.iBeaconAppUuid), null, null);
-
-            _beaconManager.Bind(_mainActivity);
-            return _beaconManager;
-        }
-
-        public List<BeaconDataModel> GetBeaconData()
-        {
-            return _connection.Table<BeaconDataModel>().ToList();
-        }
-
-
-        public void StartBeacon()
-        {
-            BeaconManagerImpl.SetForegroundBetweenScanPeriod(AppConstants.BeaconsUpdateInMillisec);
-            BeaconManagerImpl.AddMonitorNotifier(_monitorNotifier);
-            BeaconManagerImpl.AddRangeNotifier(_rangeNotifier);
-
-            _beaconManager.StartMonitoringBeaconsInRegion(_fieldRegion);
-            _beaconManager.StartRangingBeaconsInRegion(_fieldRegion);
-        }
-
-        public void StopBeacon()
-        {
-            _beaconManager.StopMonitoringBeaconsInRegion(_fieldRegion);
-            _beaconManager.StopRangingBeaconsInRegion(_fieldRegion);
-            _beaconManager.Unbind(_mainActivity);
-        }
-
-        public void StopAdvertising()
-        {
-            _beaconTransmitter.StopAdvertising();
-        }
-
-        public void StartAdvertising(UserDataModel userData)
-        {
-
-            Beacon beacon = new Beacon.Builder()
-                                .SetId1(AppConstants.iBeaconAppUuid)
-                                .SetId2(userData.Major)
-                                .SetId3(userData.Minor)
-                                .SetTxPower(-59)
-                                .SetManufacturer(AppConstants.CompanyCoreApple)
-                                .Build();
-
-            BeaconParser beaconParser = new BeaconParser().SetBeaconLayout(AppConstants.iBeaconFormat);
-
-            _beaconTransmitter = new BeaconTransmitter(_mainActivity, beaconParser);
-            _beaconTransmitter.StartAdvertising(beacon);
-
-        }
-
-        private async void DidRangeBeaconsInRegionComplete(object sender, RangeEventArgs rangeEventArgs)
-        {
-            System.Diagnostics.Debug.WriteLine("DidRangeBeaconsInRegionComplete");
-            var now = DateTime.UtcNow;
-            var keyTime = now.ToString("yyyyMMddHH");
-
-            ICollection<Beacon> beacons = rangeEventArgs.Beacons;
-            if (beacons != null && beacons.Count > 0)
-            {
-                var foundBeacons = beacons.ToList();
-                foreach (Beacon beacon in foundBeacons)
-                {
-                    var key = $"{beacon.Id1}{beacon.Id2}{beacon.Id3}.{keyTime}";
-                    lock (dataLock)
-                    {
-                        var result = _connection.Table<BeaconDataModel>().SingleOrDefault(x => x.Id == key);
-                        if (result == null)
-                        {
-                            // New
-                            BeaconDataModel data = new BeaconDataModel();
-                            data.Id = key;
-                            data.Count = 0;
-                            data.UserBeaconUuid = AppConstants.iBeaconAppUuid;
-                            data.BeaconUuid = beacon.Id1.ToString();
-                            data.Major = beacon.Id2.ToString();
-                            data.Minor = beacon.Id3.ToString();
-                            data.Distance = beacon.Distance;
-                            data.MinDistance = beacon.Distance;
-                            data.MaxDistance = beacon.Distance;
-                            data.Rssi = beacon.Rssi;
-                            //                       data.TXPower = beacon.TxPower;
-                            data.ElaspedTime = new TimeSpan();
-                            data.LastDetectTime = now;
-                            data.FirstDetectTime = now;
-                            data.KeyTime = keyTime;
-                            data.IsSentToServer = false;
-                            _connection.Insert(data);
-                        }
-                        else
-                        {
-                            // Update
-                            BeaconDataModel data = result;
-                            data.Id = key;
-                            data.Count++;
-                            data.UserBeaconUuid = AppConstants.iBeaconAppUuid;
-                            data.BeaconUuid = beacon.Id1.ToString();
-                            data.Major = beacon.Id2.ToString();
-                            data.Minor = beacon.Id3.ToString();
-                            data.Distance += (beacon.Distance - data.Distance) / data.Count;
-                            data.MinDistance = (beacon.Distance < data.MinDistance ? beacon.Distance : data.MinDistance);
-                            data.MaxDistance = (beacon.Distance > data.MaxDistance ? beacon.Distance : data.MaxDistance);
-                            data.Rssi = beacon.Rssi;
-                            //                        data.TXPower = beacon.TxPower;
-                            data.ElaspedTime += now - data.LastDetectTime;
-                            data.LastDetectTime = now;
-                            data.KeyTime = keyTime;
-                            data.IsSentToServer = false;
-                            _connection.Update(data);
-                            System.Diagnostics.Debug.WriteLine(Utils.SerializeToJson(data));
-                        }
-                    }
-                }
-            }
-        }
-
-        private void DetermineStateForRegionComplete(object sender, MonitorEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("DetermineStateForRegionComplete");
-            System.Diagnostics.Debug.WriteLine(e.ToString());
-            _beaconManager.StartRangingBeaconsInRegion(_fieldRegion);
-        }
-
-        private void EnterRegionComplete(object sender, MonitorEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("EnterRegionComplete ---- StartRanging");
-            System.Diagnostics.Debug.WriteLine(e.ToString());
-            _beaconManager.StartRangingBeaconsInRegion(_fieldRegion);
-        }
-
-        private void ExitRegionComplete(object sender, MonitorEventArgs e)
-        {
-            System.Diagnostics.Debug.WriteLine("ExitRegionComplete ---- StopRanging");
-            System.Diagnostics.Debug.WriteLine(e.ToString());
-            _beaconManager.StopRangingBeaconsInRegion(_fieldRegion);
         }
 
         public void Dispose()
