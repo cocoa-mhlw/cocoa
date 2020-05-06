a51aa49e37e5837e7385f44981c1fdefb8afaf95
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-06 11:48:04 +0900

    Apply Xamarin Visual Material Theme

--- Covid19Radar/Covid19Radar.iOS/AppDelegate.cs
+++ Covid19Radar/Covid19Radar.iOS/AppDelegate.cs
@@ -41,6 +41,8 @@ namespace Covid19Radar.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            global::Xamarin.Forms.FormsMaterial.Init();
+
             global::Rg.Plugins.Popup.Popup.Init();
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             global::FFImageLoading.ImageService.Instance.Initialize(new FFImageLoading.Config.Configuration()
