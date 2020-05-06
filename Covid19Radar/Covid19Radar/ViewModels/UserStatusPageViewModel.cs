930b998f06437b2eca3967f4509c4966e25460bd
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-06 11:48:04 +0900

    Apply Xamarin Visual Material Theme

--- Covid19Radar/Covid19Radar/ViewModels/UserStatusPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/UserStatusPageViewModel.cs
@@ -6,11 +6,7 @@ using Covid19Radar.Services;
 using Prism.Ioc;
 using Prism.Navigation;
 using Xamarin.Forms;
-using System.Reactive.Linq;
-using Reactive.Bindings;
-using System.Reactive.Disposables;
 using System;
-using Reactive.Bindings.Extensions;
 
 namespace Covid19Radar.ViewModels
 {
@@ -20,28 +16,16 @@ namespace Covid19Radar.ViewModels
         public string UserStatusName { get; set; }
     }
 
-    public class UserStatusPageViewModel : ViewModelBase, IDisposable
+    public class UserStatusPageViewModel : ViewModelBase
     {
         private readonly UserDataService _userDataService;
-
-        public List<string> UserStatuses { get; } = Enum.GetNames(typeof(UserStatus)).ToList();
-        public ReactiveProperty<UserStatus> SelectedUserStatus { get; set; }
-        private CompositeDisposable _disposable { get; } = new CompositeDisposable();
         private UserDataModel _userData;
 
         public UserStatusPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = Resources.AppResources.TitileUserStatusSettings;
             _userDataService = App.Current.Container.Resolve<UserDataService>();
-
             _userData = _userDataService.Get();
-            SelectedUserStatus = new ReactiveProperty<UserStatus>(_userData.UserStatus).AddTo(_disposable);
-            SelectedUserStatus.ObserveProperty(x => x.Value).Subscribe(OnChangeUserStatus).AddTo(_disposable);
-        }
-
-        private void OnChangeUserStatus(UserStatus userStatus)
-        {
-            _userData.UserStatus = userStatus;
         }
 
         public Command OnClickSave => (new Command(async () =>
@@ -49,10 +33,5 @@ namespace Covid19Radar.ViewModels
             await _userDataService.SetAsync(_userData);
             await NavigationService.NavigateAsync("HomePage");
         }));
-
-        public void Dispose()
-        {
-            this._disposable.Dispose();
-        }
     }
 }
