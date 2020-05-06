6d35cc53ba658fd3937e7331a89971fee628c30a
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-07 01:59:42 +0900

    Add Main view

---
+++ Covid19Radar/Covid19Radar/ViewModels/ExposuresPageViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Covid19Radar.ViewModels
+{
+    public class ExposuresPageViewModel : ViewModelBase
+    {
+        public ExposuresPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = Resources.AppResources.TitleHeadsup;
+        }
+
+        public Command OnClickPrev => (new Command(() =>
+        {
+            NavigationService.NavigateAsync("HomePage");
+        }));
+    }
+}
---
+++ Covid19Radar/Covid19Radar/ViewModels/Tutorial/InitSettingPageViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Covid19Radar.Common;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Covid19Radar.ViewModels
+{
+    public class InitSettingPageViewModel : ViewModelBase
+    {
+        public InitSettingPageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = Resources.AppResources.TitleDeviceAccess;
+        }
+
+        public Command OnClickNotNow => new Command(() => NavigationService.NavigateAsync("SetupCompletedPage"));
+        public Command OnClickEnable => new Command(async () =>
+        {
+            // TODO Allow popup?
+
+            var enabled = await Xamarin.ExposureNotifications.ExposureNotification.IsEnabledAsync();
+            if (!enabled)
+            {
+                await Xamarin.ExposureNotifications.ExposureNotification.StartAsync();
+                await NavigationService.NavigateAsync("SetupCompletedPage");
+            }
+        });
+    }
+}
