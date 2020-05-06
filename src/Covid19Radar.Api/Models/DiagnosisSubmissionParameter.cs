461b9a9b379b338e1df1ec2b1a836e0bfa0b3c79
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-06 21:57:32 +0900

    add draft DiagnosisApi. and obsolete some apis.

---
+++ src/Covid19Radar.Api/Models/DiagnosisSubmissionParameter.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Models
+{
+	public class DiagnosisSubmissionParameter : IUser
+	{
+		[JsonProperty("submissionNumber")]
+		public string SubmissionNumber { get; set; }
+		[JsonProperty("userUuid")]
+		public string UserUuid { get; set; }
+        /// <summary>
+        /// Major - in this app case mapping user id
+        /// </summary>
+        /// <value>BLE major number</value>
+        [JsonProperty("userMajor")]
+        public string UserMajor { get; set; }
+        /// <summary>
+        /// MInor - in this app case mapping user id
+        /// </summary>
+        /// <value>BLE minor number</value>
+        [JsonProperty("userMinor")]
+        public string UserMinor { get; set; }
+
+        [JsonProperty("keys")]
+		public TemporaryExposureKey[] Keys { get; set; }
+        string IUser.Major => UserMajor;
+        string IUser.Minor => UserMinor;
+
+    }
+}
