cff715fd6cd29a37db2f5ea2fc5cbf5f1bb5018f
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-08 14:12:32 +0900

    Prepare Shell and tutorial navigation

--- Covid19Radar/Covid19Radar/Views/MainPage.xaml.cs
+++ Covid19Radar/Covid19Radar/Views/MainPage.xaml.cs
@@ -3,15 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Covid19Radar.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class UserStatusPage : ContentPage
+    public partial class MainPage : Shell
     {
-        public UserStatusPage()
+        public MainPage()
         {
             InitializeComponent();
         }
