cf9baa550d6ee0308a038aab774ce72618388c04
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-12 21:33:26 +0900

    modify, validate with hash and user uuid on ValidationUserService

--- src/Covid19Radar.Functions.Common/Services/ICryptionService.cs
+++ src/Covid19Radar.Functions.Common/Services/ICryptionService.cs
@@ -7,6 +7,7 @@ namespace Covid19Radar.Services
     public interface ICryptionService
     {
         string CreateSecret(string userUuid);
+        bool ValidateSecret(string userUuid, string secret);
         string Protect(string secret);
         string Unprotect(string protectSecret);
     }
