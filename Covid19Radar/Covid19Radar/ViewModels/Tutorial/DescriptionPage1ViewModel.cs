1d70e600384bdf4eeaed991667d00be1d15f752c
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-14 23:32:52 +0900

    Navigation fix

---
+++ Covid19Radar/Covid19Radar/ViewModels/Tutorial/DescriptionPage1ViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using Covid19Radar.Model;
+using Prism.Navigation;
+using Xamarin.Forms;
+
+namespace Covid19Radar.ViewModels
+{
+    public class DescriptionPage1ViewModel : ViewModelBase
+    {
+        public DescriptionPage1ViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = Resources.AppResources.TitleHowItWorks;
+        }
+
+        public Command OnClickNext => new Command(async () =>
+        {
+            await NavigationService.NavigateAsync("DescriptionPage2");
+        });
+    }
+}
