7def117dfb5a3802774752eab40b220033be3b66
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-16 00:30:22 +0900

    add draft batch service

--- src/Covid19Radar.Functions.Common/Models/TemporaryExposureKeyModel.cs
+++ src/Covid19Radar.Functions.Common/Models/TemporaryExposureKeyModel.cs
@@ -8,25 +8,30 @@ using System.Text;
 
 namespace Covid19Radar.Models
 {
-	public class TemporaryExposureKeyModel
-	{
-		public string id { get; set; } = Guid.NewGuid().ToString();
+    public class TemporaryExposureKeyModel
+    {
+        public string id { get; set; } = Guid.NewGuid().ToString();
 
-		public byte[] KeyData { get; set; }
-		public int RollingPeriod { get; }
-		public int RollingStartIntervalNumber { get; }
-		public int TransmissionRiskLevel { get; }
+        public byte[] KeyData { get; set; }
+        public int RollingPeriod { get; set; }
+        public int RollingStartIntervalNumber { get; set; }
+        public int TransmissionRiskLevel { get; set; }
 
-		public TemporaryExposureKey ToKey()
-		{
-			return new TemporaryExposureKey()
-			{
-				KeyData = ByteString.CopyFrom(KeyData),
-				RollingStartIntervalNumber = RollingStartIntervalNumber,
-				RollingPeriod = RollingPeriod,
-				TransmissionRiskLevel = TransmissionRiskLevel
-			};
-		}
-	}
+        public long RollingStartUnixTimeSeconds { get => DateTimeOffset.FromUnixTimeSeconds(RollingStartIntervalNumber * 10 * 60).ToUnixTimeSeconds(); }
+        public long RollingPeriodSeconds { get => RollingPeriod * 10 * 60; }
+
+        public ulong Timestamp { get; set; }
+
+        public TemporaryExposureKey ToKey()
+        {
+            return new TemporaryExposureKey()
+            {
+                KeyData = ByteString.CopyFrom(KeyData),
+                RollingStartIntervalNumber = RollingStartIntervalNumber,
+                RollingPeriod = RollingPeriod,
+                TransmissionRiskLevel = TransmissionRiskLevel
+            };
+        }
+    }
 
 }
