c1b8a5aec5831c87b3253317ffe0bf5dcaf4dc4e
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-16 12:19:17 +0900

    Rebase prepare

---
+++ Covid19Radar/Covid19Radar/Views/MasterDetail/AppHomePageMasterMenuItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Covid19Radar.Views
+{
+
+    public class AppHomePageMasterMenuItem
+    {
+        public AppHomePageMasterMenuItem()
+        {
+            TargetType = typeof(AppHomePageMasterMenuItem);
+        }
+        public int Id { get; set; }
+        public string Title { get; set; }
+
+        public Type TargetType { get; set; }
+    }
+}
