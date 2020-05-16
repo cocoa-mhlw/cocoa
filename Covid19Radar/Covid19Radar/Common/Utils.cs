d5ed7db01206471c73ce4ee55cd1d48454429364
Kazuki Ota <kaota@microsoft.com>
2020-05-16 11:44:04 +0900

    不要なtry catchを削除

--- Covid19Radar/Covid19Radar/Common/Utils.cs
+++ Covid19Radar/Covid19Radar/Common/Utils.cs
@@ -7,30 +7,8 @@ namespace Covid19Radar.Common
 {
     public static class Utils
     {
-        public static string SerializeToJson(object obj)
-        {
-            try
-            {
-                var json = JsonConvert.SerializeObject(obj);
-                return json;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
+        public static string SerializeToJson(object obj) => JsonConvert.SerializeObject(obj);
 
-        public static T DeserializeFromJson<T>(string jsonObj)
-        {
-            try
-            {
-                var result = JsonConvert.DeserializeObject<T>(jsonObj);
-                return result;
-            }
-            catch (Exception ex)
-            {
-                throw;
-            }
-        }
+        public static T DeserializeFromJson<T>(string jsonObj) => JsonConvert.DeserializeObject<T>(jsonObj);
     }
 }
