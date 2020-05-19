5919ac8e7d9ffbab7ab9e5eabe6a7d712e025c95
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-19 22:59:06 +0900

    modify service injection for External startup.

--- src/Covid19Radar.Api.External/Startup.cs
+++ src/Covid19Radar.Api.External/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Microsoft.Extensions.Logging.Debug;
 using Microsoft.AspNetCore.Http;
+using Covid19Radar.DataAccess;
 
 [assembly: FunctionsStartup(typeof(Covid19Radar.Startup))]
 
@@ -23,6 +24,8 @@ namespace Covid19Radar
             builder.Services.AddSingleton<DataStore.ICosmos, DataStore.Cosmos>();
             builder.Services.AddSingleton<INotificationService, NotificationService>();
             builder.Services.AddSingleton<IValidationUserService, ValidationUserService>();
+            builder.Services.AddSingleton<IDiagnosisRepository, CosmosDiagnosisRepository>();
+            builder.Services.AddSingleton<ITemporaryExposureKeyRepository, CosmosTemporaryExposureKeyRepository>();
         }
     }
 }
