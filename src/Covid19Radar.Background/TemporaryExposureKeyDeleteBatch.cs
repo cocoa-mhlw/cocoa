2c7d307d507755ccd4f2b396a311ba250e6d3343
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-19 19:23:18 +0900

    add draft delete batch for TEK

--- src/Covid19Radar.Background/TemporaryExposureKeyDeleteBatch.cs
+++ src/Covid19Radar.Background/TemporaryExposureKeyDeleteBatch.cs
@@ -7,20 +7,19 @@ using Microsoft.Extensions.Logging;
 
 namespace Covid19Radar
 {
-    public class TemporaryExposureKeyExportBatch
+    public class TemporaryExposureKeyDeleteBatch
     {
-        public readonly ITemporaryExposureKeyExportBatchService BatchService;
+        public readonly ITemporaryExposureKeyDeleteBatchService BatchService;
 
-        public TemporaryExposureKeyExportBatch(ITemporaryExposureKeyExportBatchService batchService)
+        public TemporaryExposureKeyDeleteBatch(ITemporaryExposureKeyDeleteBatchService batchService)
         {
             BatchService = batchService;
         }
 
-        [FunctionName("TemporaryExposureKeyExportBatch")]
-        public async Task Run([TimerTrigger("0 0 * * * *")]TimerInfo myTimer, ILogger log)
+        [FunctionName("TemporaryExposureKeyDeleteBatch")]
+        public async Task Run([TimerTrigger("0 * * * * *")]TimerInfo myTimer, ILogger log)
         {
             log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
-
             await BatchService.RunAsync();
         }
     }
