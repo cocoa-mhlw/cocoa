65f8213c0f74b8606ee7701c17a9609d96df23f4
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-20 00:57:01 +0900

    modify UserApi

--- src/Covid19Radar.Api/Models/UserResultModel.cs
+++ src/Covid19Radar.Api/Models/UserResultModel.cs
@@ -8,31 +8,6 @@ namespace Covid19Radar.Models
     public class UserResultModel
     {
         /// <summary>
-        ///  Status Contactd,OnSet,Suspected,Inspection,Infection
-        /// </summary>
-        /// <value></value>
-        private UserStatus _UserStatus;
-        [JsonProperty("userStatus")]
-        public string UserStatus
-        {
-            get
-            {
-                return Enum.GetName(typeof(Covid19Radar.Common.UserStatus), _UserStatus);
-            }
-            set
-            {
-                _UserStatus = Enum.Parse<Covid19Radar.Common.UserStatus>(value);
-            }
-        }
-        /// <summary>
-        /// set SserStatus
-        /// </summary>
-        /// <param name="s">UserStatus</param>
-        public void SetStatus(UserStatus s)
-        {
-            _UserStatus = s;
-        }
-        /// <summary>
         /// Last notification date and time
         /// </summary>
         [JsonProperty("lastNotificationTime")]
