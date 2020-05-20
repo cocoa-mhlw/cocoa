4defce21efb49065e1f9a71eb447185a4b4deb9b
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-21 00:42:00 +0900

    Add Debug Exposure Notification Page (Test Mock and LIve)

--- Covid19Radar/Covid19Radar/ViewModels/HomePage/SettingsPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/SettingsPageViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections.Generic;
 using System.ComponentModel;
+using System.Threading.Tasks;
 using System.Windows.Input;
 using Acr.UserDialogs;
 using Covid19Radar.Common;
@@ -7,6 +8,7 @@ using Covid19Radar.Model;
 using Covid19Radar.Renderers;
 using Covid19Radar.Resources;
 using Covid19Radar.Services;
+using Covid19Radar.Views;
 using Prism.Navigation;
 using Xamarin.ExposureNotifications;
 using Xamarin.Forms;
@@ -23,36 +25,93 @@ namespace Covid19Radar.ViewModels
             set { SetProperty(ref _AppVersion, value); }
         }
 
+        private bool _EnableExposureNotification;
+
+        public bool EnableExposureNotification
+        {
+            get { return _EnableExposureNotification; }
+            set
+            {
+                SetProperty(ref _EnableExposureNotification, value);
+                RaisePropertyChanged(nameof(EnableExposureNotification));
+            }
+        }
+
+        private bool _EnableLocalNotification;
+
+        public bool EnableLocalNotification
+        {
+            get { return _EnableLocalNotification; }
+            set
+            {
+                SetProperty(ref _EnableLocalNotification, value);
+                RaisePropertyChanged(nameof(EnableLocalNotification));
+            }
+        }
+
+        private bool _ResetData;
+
+        public bool ResetData
+        {
+            get { return _ResetData; }
+            set
+            {
+                SetProperty(ref _ResetData, value);
+                RaisePropertyChanged(nameof(ResetData));
+            }
+        }
+
         public SettingsPageViewModel(INavigationService navigationService) : base(navigationService)
         {
             Title = AppResources.HomePageTitle;
             AppVer = AppConstants.AppVersion;
         }
 
-        public bool EnableNotifications
+
+        Task Disabled()
         {
-            get => LocalStateManager.Instance.EnableNotifications;
-            set
-            {
-                LocalStateManager.Instance.EnableNotifications = value;
-                LocalStateManager.Save();
-            }
+            LocalStateManager.Instance.LastIsEnabled = false;
+            LocalStateManager.Instance.IsWelcomed = false;
+            LocalStateManager.Save();
+            return NavigationService.NavigateAsync(nameof(HomePage));
         }
 
-        public Command OnResetClick => new Command(async () =>
+
+        // Switch Behevior
+        public ICommand OnChangeEnableExposureNotification => new Command(async () =>
         {
-            var check = await UserDialogs.Instance.ConfirmAsync("Could you reset all data?", "Reset All Data", "OK", "Cancel");
-            if (check)
-            {
-                UserDialogs.Instance.ShowLoading("Deleting data");
+            
+        });
 
-                // TODO Exposure notification reset all data
-
-                UserDialogs.Instance.HideLoading();
-
-                await UserDialogs.Instance.ConfirmAsync("The data has been reset.", "Reset", "OK");
-            }
+        public ICommand OnChangeEnableNotification => new Command(async () =>
+        {
 
         });
+
+        public ICommand OnChangeResetData => new Command(async () =>
+        {
+
+        });
+
+
+        public Command OnSaveClick => new Command(async () =>
+        {
+            if (ResetData)
+            {
+                var check = await UserDialogs.Instance.ConfirmAsync("Could you reset all data?", "Reset All Data", "OK", "Cancel");
+                if (check)
+                {
+                    UserDialogs.Instance.ShowLoading("Deleting data");
+
+                    // TODO Exposure notification reset all data
+
+                    UserDialogs.Instance.HideLoading();
+
+                    await UserDialogs.Instance.ConfirmAsync("The data has been reset.", "Reset", "OK");
+                }
+            }
+        });
+
+
     }
 }
