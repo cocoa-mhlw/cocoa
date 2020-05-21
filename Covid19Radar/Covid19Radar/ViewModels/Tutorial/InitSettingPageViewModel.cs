257cb4b1c4175e2a2b46e3df42e844b449e26b97
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-21 16:40:59 +0900

    Fix Tutorial

--- Covid19Radar/Covid19Radar/ViewModels/Tutorial/InitSettingPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/Tutorial/InitSettingPageViewModel.cs
@@ -1,4 +1,5 @@
-﻿using Covid19Radar.Common;
+﻿using Acr.UserDialogs;
+using Covid19Radar.Common;
 using Covid19Radar.Renderers;
 using Covid19Radar.Services;
 using Covid19Radar.Views;
@@ -22,8 +23,26 @@ namespace Covid19Radar.ViewModels
         });
         public Command OnClickEnable => new Command(async () =>
         {
+            var check = await UserDialogs.Instance.ConfirmAsync("周辺の他のスマートフォンとの間でランダムIDを安全に収集したり共有したりするには、Bluetoothを使用する必要があります。\nCOVID-19(新型コロナウイルス感染症)の検査結果が陽性だった人があなたの周囲にいた場合、 [TODO Replace Public Health Authority] から通知を受け取ることができます。\n濃厚接触の可能性がある日付、期間、電波強度がアプリと共有されます。", "COVID-19(新型コロナウイルス感染症)の濃厚接触の可能性の通知をオンにしますか?", "OK", "Cancel");
+            if (!check)
+            {
+                return;
+            }
+
+            UserDialogs.Instance.ShowLoading("有効にしています。");
+
             LocalStateManager.Instance.LastIsEnabled = true;
             LocalStateManager.Save();
+
+            if (LocalStateManager.Instance.LastIsEnabled && LocalStateManager.Instance.IsWelcomed)
+            {
+                if (!await Xamarin.ExposureNotifications.ExposureNotification.IsEnabledAsync())
+                {
+                    await Xamarin.ExposureNotifications.ExposureNotification.StartAsync();
+                }
+            }
+            UserDialogs.Instance.HideLoading();
+
             await NavigationService.NavigateAsync(nameof(SetupCompletedPage));
 
         });
