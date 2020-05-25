61a6ca9df78f8b8beb5624565b9bf7efed9c7d23
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-25 15:58:14 +0900

    deinit exposure notification submodule

--- Covid19Radar/Covid19Radar.Android/Services/DeviceCheckService.cs
+++ Covid19Radar/Covid19Radar.Android/Services/DeviceCheckService.cs
@@ -12,19 +12,33 @@ using Android.Widget;
 using Covid19Radar.Services;
 using Covid19Radar.Droid.Services;
 using Xamarin.Forms;
+using Covid19Radar.Model;
+using Covid19Radar.Common;
+using Android.Gms.SafetyNet;
 
 [assembly: Dependency(typeof(DeviceCheckService))]
 namespace Covid19Radar.Droid.Services
 {
-    public class DeviceCheckService : IDeviceCheckService
+    public class DeviceCheckService : IDeviceVerifier
     {
+
+        readonly string safetyNetApiKey = "YOUR-KEY-HERE";
+
+        public Task<string> VerifyAsync(SelfDiagnosisSubmission submission)
+        {
+            var nonce = submission.GetAndroidNonce();
+            return GetSafetyNetAttestationAsync(nonce);
+        }
+
         /// <summary>
         /// Verification device information required for positive submissions
         /// </summary>
         /// <returns>Device Verification Payload</returns>
-        public Task<string> GetDeviceVerificationPayload()
+        async Task<string> GetSafetyNetAttestationAsync(byte[] nonce)
         {
-            return Task.FromResult("");
+            using var client = SafetyNetClass.GetClient(Android.App.Application.Context);
+            using var response = await client.AttestAsync(nonce, safetyNetApiKey);
+            return response.JwsResult;
         }
     }
 }
