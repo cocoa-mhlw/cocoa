1861405e3ad9842201241609d96176c42e8c9056
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-27 22:52:06 +0900

    fix DeviceValidationService for apple

--- src/Covid19Radar.Api/Services/DeviceValidationService.cs
+++ src/Covid19Radar.Api/Services/DeviceValidationService.cs
@@ -21,6 +21,7 @@ using System.Collections;
 using Covid19Radar.Api.DataAccess;
 using System.Security.Cryptography;
 using Covid19Radar.Api.Extensions;
+using System.Text.RegularExpressions;
 
 namespace Covid19Radar.Api.Services
 {
@@ -56,7 +57,7 @@ namespace Covid19Radar.Api.Services
                 case "android":
                     return await ValidationAndroid(param, param.GetAndroidNonce(), requestTime, app);
                 case "ios":
-                    return await ValidationApple(param);
+                    return await ValidationApple(param, requestTime, app);
             }
             return false;
         }
@@ -82,7 +83,7 @@ namespace Covid19Radar.Api.Services
         /// <remarks>
         /// https://developer.apple.com/documentation/devicecheck/accessing_and_modifying_per-device_data
         /// </remarks>
-        public async Task<bool> ValidationApple(DiagnosisSubmissionParameter param)
+        public async Task<bool> ValidationApple(DiagnosisSubmissionParameter param, DateTimeOffset requestTime, AuthorizedApp app)
         {
             var payload = new ApplePayload()
             {
@@ -101,26 +102,105 @@ namespace Covid19Radar.Api.Services
                 payload.TransactionId = Convert.ToBase64String(sha.ComputeHash(value));
             }
 
+            var jwt = GenerateClientSecretJWT(requestTime, app.DeviceCheckKeyId, app.DeviceCheckTeamId, app.DeviceCheckPrivateKey);
+
             var content = new StringContent(JsonConvert.SerializeObject(payload));
             var request = new HttpRequestMessage(HttpMethod.Post, UrlApple);
-            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", AppleBearerToken);
+            request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", jwt);
             var response = await ClientApple.SendAsync(request);
 
-            switch (response.StatusCode)
-            {
-                // 200 OK:                  The transaction was successful
-                // 200 Bit State Not Found: The bit state wasn't found
-                case System.Net.HttpStatusCode.OK:
-                    if (response.ReasonPhrase == "OK") return true;
+            //switch (response.StatusCode)
+            //{
+            //    // 200 OK:                  The transaction was successful
+            //    // 200 Bit State Not Found: The bit state wasn't found
+            //    case System.Net.HttpStatusCode.OK:
+            //        if (response.ReasonPhrase == "OK") return true;
 
+            //        break;
+            //    // 
+            //    default:
+            //        break;
+            //}
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                return false;
+
+            return true;
+        }
+
+        static string GenerateClientSecretJWT(DateTimeOffset requestTime, string keyId, string teamId, string p8FileContents)
+        {
+            var headers = new Dictionary<string, object>
+            {
+                { "alg", "ES256" },
+                { "kid", keyId }
+            };
+
+            var payload = new Dictionary<string, object>
+            {
+                { "iss", teamId },
+                { "iat", requestTime.ToUnixTimeMilliseconds() }
+            };
+
+            var secretKey = CleanP8Key(p8FileContents);
+
+            // Get our headers/payloads into a json string
+            var headerStr = "{" + string.Join(",", headers.Select(kvp => $"\"{kvp.Key}\":\"{kvp.Value.ToString()}\"")) + "}";
+            var payloadStr = "{";
+            foreach (var kvp in payload)
+            {
+                if (kvp.Value is int || kvp.Value is long || kvp.Value is double)
+                    payloadStr += $"\"{kvp.Key}\":{kvp.Value.ToString()},";
+                else
+                    payloadStr += $"\"{kvp.Key}\":\"{kvp.Value.ToString()}\",";
+            }
+            payloadStr = payloadStr.TrimEnd(',') + "}";
+
+
+            // Load the key text
+            var key = CngKey.Import(Convert.FromBase64String(secretKey), CngKeyBlobFormat.Pkcs8PrivateBlob);
+
+            using (var dsa = new ECDsaCng(key))
+            {
+                var unsignedJwt = Base64UrlEncode(Encoding.UTF8.GetBytes(headerStr))
+                                        + "." + Base64UrlEncode(Encoding.UTF8.GetBytes(payloadStr));
+
+                var signature = dsa.SignData(Encoding.UTF8.GetBytes(unsignedJwt), HashAlgorithmName.SHA256);
+
+                return unsignedJwt + "." + Base64UrlEncode(signature);
+            }
+        }
+
+        static string CleanP8Key(string p8Contents)
+        {
+            // Remove whitespace
+            var tmp = Regex.Replace(p8Contents, "\\s+", string.Empty, RegexOptions.Singleline);
+
+            // Remove `---- BEGIN PRIVATE KEY ----` bits
+            tmp = Regex.Replace(tmp, "-{1,}.*?-{1,}", string.Empty, RegexOptions.Singleline);
+
+            return tmp;
+        }
+
+        static string Base64UrlEncode(byte[] data)
+        {
+            var base64 = Convert.ToBase64String(data, 0, data.Length);
+            var base64Url = new StringBuilder();
+
+            foreach (var c in base64)
+            {
+                if (c == '+')
+                    base64Url.Append('-');
+                else if (c == '/')
+                    base64Url.Append('_');
+                else if (c == '=')
                     break;
-                // 
-                default:
-                    break;
+                else
+                    base64Url.Append(c);
             }
 
-            return false;
+            return base64Url.ToString();
         }
+
 
 
         /// <summary>
