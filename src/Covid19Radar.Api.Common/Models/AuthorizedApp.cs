cb6ab05ace6345f5e28cbd2145132e26387a2341
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-27 22:08:42 +0900

    add authorizedApp Model  for cosmos data store

---
+++ src/Covid19Radar.Api.Common/Models/AuthorizedApp.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Api.Models
+{
+    public class AuthorizedApp
+    {
+        public string id { get => Platform; }
+
+        public string PackageName { get; set; }
+
+        public string Platform { get; set; }
+
+        public string[] AllowedRegions { get; set; }
+
+        // SafetyNet configuration
+
+        public string[] SafetyNetApkDigestSha256 { get; set; }
+
+        public bool SafetyNetBasicIntegrity { get; set; }
+
+        public bool SafetyNetCtsProfileMatch { get; set; }
+
+        public int SafetyNetPastTimeSeconds { get; set; }
+
+        public int SafetyNetFutureTimeSeconds { get; set; }
+
+        // DeviceCheck configuration
+
+        public string DeviceCheckKeyId { get; set; }
+
+        public string DeviceCheckTeamId { get; set; }
+
+        public string DeviceCheckPrivateKey { get; set; }
+
+        // Utils
+        public static DevicePlatform ParsePlatform(string platform) =>
+            platform.Equals("android", StringComparison.OrdinalIgnoreCase)
+                ? DevicePlatform.Android
+                : DevicePlatform.iOS;
+
+        public enum DevicePlatform
+        {
+            iOS,
+            Android
+        }
+    }
+}
