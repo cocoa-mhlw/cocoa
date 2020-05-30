22fb669fd0bdd95f959535b3f8848dfea5feeac7
Noriko Matsumoto <n.matsumoto0822@gmail.com>
2020-05-31 02:50:09 +0900

    Add Tutorial Page

---
+++ Covid19Radar/Covid19Radar/Views/Tutorial/NorijiWorkspace/TutorialPage6.xaml6.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Covid19Radar.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class TutorialPage6 : ContentPage
+    {
+        public TutorialPage6()
+        {
+            InitializeComponent();
+        }
+    }
+}
