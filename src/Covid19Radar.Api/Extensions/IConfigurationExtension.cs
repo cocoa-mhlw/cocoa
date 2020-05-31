5e24fa25f347ca6d01bba7ba6d35f0934d09c828
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-05-31 14:34:45 +0900

    fix configuration

--- src/Covid19Radar.Api/Extensions/IConfigurationExtension.cs
+++ src/Covid19Radar.Api/Extensions/IConfigurationExtension.cs
@@ -7,8 +7,6 @@ namespace Covid19Radar.Api
         public static string ExportKeyUrl(this IConfiguration config) => config["ExportKeyUrl"];
         public static string TekExportBlobStorageContainerPrefix(this IConfiguration config) => config["TekExportBlobStorageContainerPrefix"];
         public static string[] SupportRegions(this IConfiguration config) => config["SupportRegions"].Split(',');
-        public static string VerificationKeyId(this IConfiguration config) => config["VerificationKeyId"];
-        public static string VerificationKeyVersion(this IConfiguration config) => config["VerificationKeyVersion"];
         public static string AndroidPackageName(this IConfiguration config) => config["AndroidPackageName"];
         public static string iOSBundleId(this IConfiguration config) => config["iOSBundleId"];
         public static string iOSDeviceCheckKeyId(this IConfiguration config) => config["iOSDeviceCheckKeyId"];
