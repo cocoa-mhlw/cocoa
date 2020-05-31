3ddfbc26548645abefe42252ae5d33b095ee8212
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-31 13:02:57 +0900

    Add Direct Project Reference

---
+++ xamarin.exposurenotification/ExposureNotification.App/ExposureNotification.App.iOS/Services/DeviceChecker.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using ExposureNotification.App.iOS.Services;
+using ExposureNotification.App.Services;
+using ExposureNotification.Backend.Network;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(DeviceChecker))]
+
+namespace ExposureNotification.App.iOS.Services
+{
+	public class DeviceChecker : IDeviceVerifier
+	{
+		public async Task<string> VerifyAsync(SelfDiagnosisSubmission submission)
+		{
+			var token = await DeviceCheck.DCDevice.CurrentDevice.GenerateTokenAsync();
+			return Convert.ToBase64String(token.ToArray());
+		}
+	}
+}
---
+++ xamarin.exposurenotification/ExposureNotification.Backend.Functions/Settings.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace ExposureNotification.Backend.Functions
+{
+	public class Settings
+	{
+		public string DbConnectionString { get; set; }
+
+		public string BlobStorageConnectionString { get; set; }
+
+		public string BlobStorageContainerNamePrefix { get; set; }
+
+		public string[] SupportedRegions { get; set; }
+
+		public bool DeleteKeysFromDbAfterBatching { get; set; }
+
+		public bool DisableDeviceVerification { get; set; }
+
+		public string SigningKeyBase64String { get; set; }
+		public string VerificationKeyId { get; set; }
+		public string VerificationKeyVersion { get; set; }
+
+		public string AndroidPackageName { get; set; }
+		public string iOSBundleId { get; set; }
+		public string iOSDeviceCheckKeyId { get; set; }
+		public string iOSDeviceCheckTeamId { get; set; }
+		public string iOSDeviceCheckPrivateKey { get; set; }
+
+
+		public override string ToString()
+		{
+			string WriteSecret(string s)
+				=> string.IsNullOrEmpty(s) ? "NULL" : "<hidden>";
+
+			var sb = new StringBuilder();
+
+			sb.AppendLine($"DbConnectionString : {WriteSecret(DbConnectionString)}");
+			sb.AppendLine($"BlobStorageConnectionString : {WriteSecret(BlobStorageConnectionString)}");
+			sb.AppendLine($"BlobStorageContainerNamePrefix : {BlobStorageContainerNamePrefix}");
+			sb.AppendLine($"SupportedRegions: {string.Join(';', SupportedRegions)}");
+			sb.AppendLine($"SigningKeyBase64String : {WriteSecret(SigningKeyBase64String)}");
+			sb.AppendLine($"VerificationKeyId: {VerificationKeyId}");
+			sb.AppendLine($"VerificationKeyVersion : {VerificationKeyVersion}");
+			sb.AppendLine($"DeleteKeysFromDbAfterBatching : {DeleteKeysFromDbAfterBatching}");
+			sb.AppendLine($"DisableDeviceVerification : {DisableDeviceVerification}");
+			sb.AppendLine($"AndroidPackageName : {AndroidPackageName}");
+			sb.AppendLine($"iOSBundleId : {iOSBundleId}");
+			sb.AppendLine($"iOSDeviceCheckKeyId : {iOSDeviceCheckKeyId}");
+			sb.AppendLine($"iOSDeviceCheckPrivateKey : {WriteSecret(iOSDeviceCheckPrivateKey)}");
+			sb.AppendLine($"iOSDeviceCheckTeamId : {iOSDeviceCheckTeamId}");
+
+			return sb.ToString();
+		}
+	}
+}
