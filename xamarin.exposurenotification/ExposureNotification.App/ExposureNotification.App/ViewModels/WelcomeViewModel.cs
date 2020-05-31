d5bea8e54b5cc3fbf2c4a61deb6fd3601a829bc0
Kazumi Hirose <kazumihirose@hotmail.com>
2020-05-31 13:02:57 +0900

    Add Direct Project Reference

---
+++ xamarin.exposurenotification/ExposureNotification.App/ExposureNotification.App/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using ExposureNotification.App.Services;
+using ExposureNotification.App.Views;
+using MvvmHelpers.Commands;
+
+using Command = MvvmHelpers.Commands.Command;
+
+namespace ExposureNotification.App.ViewModels
+{
+	public class WelcomeViewModel : ViewModelBase
+	{
+		public WelcomeViewModel()
+		{
+			_ = Initialize();
+		}
+
+		async Task Initialize()
+		{
+			IsEnabled = await Xamarin.ExposureNotifications.ExposureNotification.IsEnabledAsync();
+			if (IsEnabled)
+				await GoToAsync($"//{nameof(InfoPage)}");
+		}
+
+		public new bool IsEnabled
+		{
+			get => LocalStateManager.Instance.LastIsEnabled;
+			set
+			{
+				LocalStateManager.Instance.LastIsEnabled = value;
+				LocalStateManager.Save();
+				OnPropertyChanged();
+			}
+		}
+
+		public bool IsWelcomed
+		{
+			get => LocalStateManager.Instance.IsWelcomed;
+			set
+			{
+				LocalStateManager.Instance.IsWelcomed = value;
+				LocalStateManager.Save();
+				OnPropertyChanged();
+			}
+		}
+
+		public AsyncCommand EnableCommand
+			=> new AsyncCommand(async () =>
+			{
+				using var _ = UserDialogs.Instance.Loading();
+
+				try
+				{
+					var enabled = await Xamarin.ExposureNotifications.ExposureNotification.IsEnabledAsync();
+					if (!enabled)
+						await Xamarin.ExposureNotifications.ExposureNotification.StartAsync();
+
+					await GoToAsync($"//{nameof(InfoPage)}");
+				}
+				catch (Exception ex)
+				{
+					Console.WriteLine($"Unable to start notifications: {ex}");
+				}
+			});
+
+		public ICommand GetStartedCommand
+			=> new Command(() => IsWelcomed = true);
+
+		public ICommand NotNowCommand
+			=> new Command(() => IsWelcomed = false);
+	}
+}
---
+++ xamarin.exposurenotification/ExposureNotification.App/ExposureNotification.App/Views/ExposureDetailsPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using ExposureNotification.App.ViewModels;
+using Xamarin.ExposureNotifications;
+using Xamarin.Forms;
+
+namespace ExposureNotification.App.Views
+{
+	public partial class ExposureDetailsPage : ContentPage
+	{
+		public ExposureDetailsPage()
+		{
+			InitializeComponent();
+
+			BindingContext = new ExposureDetailsViewModel();
+		}
+	}
+}
