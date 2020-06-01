57f1c5dca6abe0ece6bd3ea39b763da66ac76e4e
YASUAKI MATSUDA <dark_crash@hotmail.com>
2020-06-02 00:19:34 +0900

    modify performance

---
+++ src/Covid19Radar.Api.Common/Common/PartitionKeyRotation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+
+namespace Covid19Radar.Api.Common
+{
+    public class PartitionKeyRotation
+    {
+        private readonly string[] Keys;
+        private readonly int Max;
+        private int Current = -1;
+        public PartitionKeyRotation(string[] keys)
+        {
+            Keys = keys;
+            Max = keys.Length;
+            Current = RandomNumberGenerator.GetInt32(0, Max - 1);
+        }
+
+        public string Next()
+        {
+            var nextValue = Interlocked.Increment(ref Current);
+            var nextKey = Keys[nextValue % Max];
+            if (nextValue >= Max)
+            {
+                Interlocked.CompareExchange(ref Current, nextValue % Max, nextValue);
+            }
+            return nextKey;
+        }
+
+    }
+}
