9159d1533831ca188fa14ed848c320d00a0b8c90
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-03 22:34:07 +0900

    Add Noriji's work

--- Covid19Radar/Covid19Radar/Views/HelpPage/HelpMenuPage.xaml.cs
+++ Covid19Radar/Covid19Radar/Views/HelpPage/HelpMenuPage.xaml.cs
@@ -5,7 +5,7 @@ using Xamarin.Forms.Xaml;
 namespace Covid19Radar.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class HelpMenuPage : PopupPage
+    public partial class HelpMenuPage : ContentPage
     {
         public HelpMenuPage()
         {
