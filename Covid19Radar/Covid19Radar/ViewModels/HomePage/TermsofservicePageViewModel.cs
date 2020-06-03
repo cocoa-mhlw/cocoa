0ec42c4c2fe1562058d9b0452822c8023eecf6d8
Noriko Matsumoto <n.matsumoto0822@gmail.com>
2020-06-03 21:55:54 +0900

    Add Terms of service Page

---
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/TermsofservicePageViewModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Acr.UserDialogs;
+using Covid19Radar.Common;
+using Covid19Radar.Model;
+using Covid19Radar.Renderers;
+using Covid19Radar.Resources;
+using Covid19Radar.Services;
+using Covid19Radar.Views;
+using Prism.Navigation;
+using Xamarin.ExposureNotifications;
+using Xamarin.Forms;
+
+namespace Covid19Radar.ViewModels
+{
+    public class TermsofservicePageViewModel : ViewModelBase
+    {
+        private string _AppVersion;
+
+        public string AppVer
+        {
+            get { return _AppVersion; }
+            set { SetProperty(ref _AppVersion, value); }
+        }
+        private UserDataModel _UserData;
+        public UserDataModel UserData
+        {
+            get { return _UserData; }
+            set { SetProperty(ref _UserData, value); }
+        }
+
+        private readonly ExposureNotificationService exposureNotificationService;
+
+        private readonly UserDataService userDataService;
+        public TermsofservicePageViewModel(INavigationService navigationService, UserDataService userDataService, ExposureNotificationService exposureNotificationService) : base(navigationService, userDataService, exposureNotificationService)
+        {
+            Title = AppResources.SettingsPageTitle;
+            AppVer = AppConstants.AppVersion;
+            this.userDataService = userDataService;
+            _UserData = this.userDataService.Get();
+            this.exposureNotificationService = exposureNotificationService;
+        }
+
+
+        private void _userDataChanged(object sender, UserDataModel e)
+        {
+            _UserData = this.userDataService.Get();
+            RaisePropertyChanged();
+        }
+
+
+        public ICommand OnChangeExposureNotificationState => new Command(async () =>
+        {
+            await userDataService.SetAsync(_UserData);
+        });
+
+
+        public ICommand OnChangeNotificationState => new Command(async () =>
+        {
+            await userDataService.SetAsync(_UserData);
+        });
+
+        public ICommand OnChangeResetData => new Command(async () =>
+        {
+            var check = await UserDialogs.Instance.ConfirmAsync(
+                Resources.AppResources.SettingsPageDialogResetText,
+                Resources.AppResources.SettingsPageDialogResetTitle,
+                Resources.AppResources.ButtonOk,
+                Resources.AppResources.ButtonCancel
+            );
+            if (check)
+            {
+                UserDialogs.Instance.ShowLoading(Resources.AppResources.LoadingTextDeleting);
+
+                if (await ExposureNotification.IsEnabledAsync())
+                {
+                    await ExposureNotification.StopAsync();
+                }
+
+                // Reset All Data and Optout
+                UserDataModel userData = new UserDataModel();
+                await userDataService.SetAsync(userData);
+
+                UserDialogs.Instance.HideLoading();
+                await UserDialogs.Instance.AlertAsync(Resources.AppResources.SettingsPageDialogResetCompletedText);
+                Application.Current.Quit();
+
+                // Application close
+                Xamarin.Forms.DependencyService.Get<ICloseApplication>().closeApplication();
+                return;
+            }
+        });
+    }
+}
