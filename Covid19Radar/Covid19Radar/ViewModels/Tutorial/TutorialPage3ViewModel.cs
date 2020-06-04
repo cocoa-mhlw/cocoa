06f33360403f7399781bc725bbb8a53192c4baae
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-04 21:52:45 +0900

    Add WebView

--- Covid19Radar/Covid19Radar/ViewModels/Tutorial/TutorialPage3ViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/Tutorial/TutorialPage3ViewModel.cs
@@ -1,6 +1,9 @@
 ﻿using System.Collections.Generic;
+using Acr.UserDialogs;
 using Covid19Radar.Model;
 using Covid19Radar.Renderers;
+using Covid19Radar.Resources;
+using Covid19Radar.Services;
 using Covid19Radar.Views;
 using Prism.Navigation;
 using Xamarin.Forms;
@@ -9,9 +12,33 @@ namespace Covid19Radar.ViewModels
 {
     public class TutorialPage3ViewModel : ViewModelBase
     {
-        public TutorialPage3ViewModel() : base()
+        private readonly UserDataService userDataService;
+        private UserDataModel userData;
+
+        public TutorialPage3ViewModel(INavigationService navigationService, UserDataService userDataService) : base(navigationService, userDataService)
         {
-            Title = Resources.AppResources.TitleHowItWorks;
+            Title = AppResources.TitleConsentByUserPage;
+            this.userDataService = userDataService;
+            userData = this.userDataService.Get();
         }
+        public Command OnClickAgree => new Command(async () =>
+        {
+
+            UserDialogs.Instance.ShowLoading(Resources.AppResources.LoadingTextRegistering);
+            if (!userDataService.IsExistUserData)
+            {
+                userData = await userDataService.RegisterUserAsync();
+                if (userData == null)
+                {
+                    UserDialogs.Instance.HideLoading();
+                    await UserDialogs.Instance.AlertAsync(Resources.AppResources.DialogNetworkConnectionError, "Connection error", Resources.AppResources.ButtonOk);
+                    return;
+                }
+            }
+            userData.IsOptined = true;
+            await userDataService.SetAsync(userData);
+            UserDialogs.Instance.HideLoading();
+            await NavigationService.NavigateAsync(nameof(DescriptionPage1));
+        });
     }
 }
