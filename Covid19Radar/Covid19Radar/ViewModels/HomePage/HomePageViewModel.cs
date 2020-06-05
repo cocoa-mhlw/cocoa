2b409760ff4011d947b96160235d79e940a4bf97
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-05 19:52:00 +0900

    Update SomeView
    - Exposure Counter and Date
    - App Start date
    - Add Notify
    - Add Inquery

--- Covid19Radar/Covid19Radar/ViewModels/HomePage/HomePageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/HomePageViewModel.cs
@@ -13,32 +13,45 @@ using Prism.Navigation;
 using Xamarin.Essentials;
 using Xamarin.ExposureNotifications;
 using Xamarin.Forms;
+using System.Globalization;
 
 namespace Covid19Radar.ViewModels
 {
     public class HomePageViewModel : ViewModelBase
     {
-        private string _exposureCount;
         private readonly UserDataService userDataService;
         private readonly ExposureNotificationService exposureNotificationService;
         private UserDataModel userData;
+        private string _startDate;
+        private string _pastDate;
 
-
-        public string ExposureCount
+        public string StartDate
         {
-            get { return _exposureCount; }
-            set { SetProperty(ref _exposureCount, value); }
+            get { return _startDate; }
+            set { SetProperty(ref _startDate, value); }
+        }
+        public string PastDate
+        {
+            get { return _pastDate; }
+            set { SetProperty(ref _pastDate, value); }
         }
 
         public HomePageViewModel(INavigationService navigationService, UserDataService userDataService, ExposureNotificationService exposureNotificationService) : base(navigationService, userDataService, exposureNotificationService)
         {
             Title = AppResources.HomePageTitle;
-            ExposureCount = String.Format("{0}{1}", 1, "件の接触がありました");
             this.userDataService = userDataService;
             this.exposureNotificationService = exposureNotificationService;
 
             _ = exposureNotificationService.StartExposureNotification();
             userData = this.userDataService.Get();
+            StartDate = userData.StartDateTime.ToLocalTime().ToString("D", CultureInfo.CurrentCulture);
+
+            TimeSpan timeSpan = DateTime.Now - userData.StartDateTime;
+            PastDate = timeSpan.Days.ToString("D", CultureInfo.CurrentCulture);
+            if (PastDate == "0")
+            {
+                PastDate = "";
+            }
         }
 
         public Command OnClickHelp => new Command(async () =>
@@ -53,7 +66,14 @@ namespace Covid19Radar.ViewModels
 
         public Command OnClickExposures => new Command(async () =>
         {
-            await NavigationService.NavigateAsync(nameof(ExposuresPage));
+            var count = exposureNotificationService.GetExposureCount();
+            if (count > 0)
+            {
+                await NavigationService.NavigateAsync(nameof(ContactedNotifyPage));
+                return;
+            }
+            await NavigationService.NavigateAsync(nameof(NotContactPage));
+            return;
         });
 
         public Command OnClickShareApp => new Command(async () =>
