5b69e24cbfad51fdc564d181e70ecc8a3d5a1943
changeworld <changesworlds@gmail.com>
2020-06-07 10:56:14 +0900

    Fix term

--- Covid19Radar/Covid19Radar/ViewModels/HelpPage/HelpMenuPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HelpPage/HelpMenuPageViewModel.cs
@@ -25,7 +25,7 @@ namespace Covid19Radar.ViewModels
 
         public HelpMenuPageViewModel(INavigationService navigationService) : base(navigationService)
         {
-            Title = "使い方";// Resources.AppResources.HelpMenuPageTitle;
+            Title = Resources.AppResources.HelpMenuPageTitle;
             MenuItems = new ObservableCollection<MainMenuModel>();
             MenuItems.Add(new MainMenuModel()
             {
