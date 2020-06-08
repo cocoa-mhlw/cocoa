34068ac6c041b2985b5d34f2701d52a9053fbd06
Yasuaki Matsuda <dark_crash@hotmail.com>
2020-06-08 11:02:46 +0900

    add define json of export files for cdn

---
+++ src/Covid19Radar.Background/Models/TemporaryExposureKeyExportFileModel.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Covid19Radar.Background.Models
+{
+    /// <summary>
+    /// export file information model
+    /// </summary>
+    public class TemporaryExposureKeyExportFileModel
+    {
+        /// <summary>
+        /// region
+        /// </summary>
+        [JsonProperty("region")]
+        public string Region { get; set; }
+        /// <summary>
+        /// download url
+        /// </summary>
+        [JsonProperty("url")]
+        public string Url { get; set; }
+        /// <summary>
+        /// created unix time secs
+        /// </summary>
+        [JsonProperty("created")]
+        public ulong Created { get; set; }
+    }
+}
