43aca06f9aeca0d49ec655b830f9e993130891fc
Yasuaki Matsuda <dark_crash@hotmail.com>
2020-06-10 10:13:25 +0900

    update tek protoc

---
+++ Tools/compileProtoc.bat
@@ -0,0 +1,4 @@
+
+protoc --proto_path=..\src\Covid19Radar.Background\Protobuf\ --csharp_out=..\src\Covid19Radar.Background\Protobuf\ --csharp_opt=file_extension=.g.cs TemporaryExposureKeyExportFileFormat.proto
+
+PAUSE
