f8fee1e144b808e16639f2e54960e11c5a22293c
Kazuki Ota <kaota@microsoft.com>
2020-06-10 20:46:14 +0900

    implement android

---
+++ Covid19Radar/Covid19Radar/Effects/SetImageToPageTitleEffect.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Covid19Radar.Effects
+{
+    public class SetImageToPageTitleEffect : RoutingEffect
+    {
+        public SetImageToPageTitleEffect() : base($"Covid19Radar.{nameof(SetImageToPageTitleEffect)}")
+        {
+        }
+    }
+}
