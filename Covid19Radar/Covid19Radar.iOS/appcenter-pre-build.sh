30909e1067196bc5643448c770010285d5312ee3
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-12 19:51:19 +0900

    App Center Build Runtime

--- Covid19Radar/Covid19Radar.iOS/appcenter-pre-build.sh
+++ Covid19Radar/Covid19Radar.iOS/appcenter-pre-build.sh
@@ -15,5 +15,12 @@ sed -i '' "s/APPCENTER_IOS/$APPCENTER_IOS/g" $APP_CONSTANT_FILE
 
 # Print out file for reference
 cat $APP_CONSTANT_FILE
-
 echo "Updated id!"
+
+# Donwload Runtime
+curl -L -O https://bosstoragemirror.blob.core.windows.net/wrench/jenkins/d16-6/29c4ea73109b377a71866c53a6d43033d5c5e90b/49/package/notarized/xamarin.ios-13.18.2.1.pkg
+curl -L -O https://bosstoragemirror.blob.core.windows.net/wrench/jenkins/d16-6/29c4ea73109b377a71866c53a6d43033d5c5e90b/49/package/notarized/xamarin.mac-6.18.2.1.pkg
+
+# Install Runtime
+sudo installer -store -pkg "xamarin.ios-13.18.2.1.pkg" -target /
+sudo installer -store -pkg "xamarin.mac-6.18.2.1.pkg" -target /
