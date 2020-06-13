980f9edbe0c64019a191ec1618f26ca1aba00eb0
Kazumi Hirose <kazumihirose@hotmail.com>
2020-06-14 03:22:00 +0900

    Fix Permit Notification

--- Covid19Radar/Covid19Radar/ViewModels/Tutorial/TutorialPage5ViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/Tutorial/TutorialPage5ViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
 using Covid19Radar.Model;
 using Covid19Radar.Renderers;
+using Covid19Radar.Resources;
 using Covid19Radar.Services;
 using Covid19Radar.Views;
+using Plugin.LocalNotification;
 using Prism.Navigation;
 using Xamarin.Forms;
 
@@ -23,6 +25,13 @@ namespace Covid19Radar.ViewModels
         public Command OnClickEnable => new Command(async () =>
         {
             userData.IsNotificationEnabled = true;
+            var notification = new NotificationRequest
+            {
+                NotificationId = 100,
+                Title = AppResources.LocalNotificationPermittedTitle,
+                Description = AppResources.LocalNotificationPermittedDescription
+            };
+            NotificationCenter.Current.Show(notification);
             await userDataService.SetAsync(userData);
             await NavigationService.NavigateAsync(nameof(TutorialPage6));
         });
