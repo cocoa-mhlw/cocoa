20baefa2cc8d5b607b3f61fbc59d1244cda78036
Yasuaki Matsuda <dark_crash@hotmail.com>
2020-06-20 23:21:11 +0900

    add CustomVerification for hxxxxs with hide out back-endpoint

---
+++ src/Covid19Radar.Api.Common/Models/CustomVerificationStatusModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Covid19Radar.Api.Models
+{
+    public class CustomVerificationStatusModel
+    {
+        public string id { get; set; }
+        public string Result { get; set; }
+        public int HttpStatusCode { get; set; }
+    }
+}
