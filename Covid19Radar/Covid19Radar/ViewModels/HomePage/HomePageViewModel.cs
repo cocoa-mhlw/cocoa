601646553755aa1a0ec6c4f9e54c9fe0f3bf0cf9
Reo Ando <ando.reo420@gmail.com>
2020-06-21 18:35:28 +0900

    #541 Covid19Radar/Covid19Radar配下のcsファイルに不要なインポート文が含まれているので削除

--- Covid19Radar/Covid19Radar/ViewModels/HomePage/HomePageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/HomePageViewModel.cs
@@ -1,19 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Windows.Input;
-using Acr.UserDialogs;
 using Covid19Radar.Common;
 using Covid19Radar.Model;
-using Covid19Radar.Renderers;
 using Covid19Radar.Resources;
 using Covid19Radar.Services;
 using Covid19Radar.Views;
 using Prism.Navigation;
-using Xamarin.Essentials;
-using Xamarin.ExposureNotifications;
 using Xamarin.Forms;
-using System.Globalization;
 
 namespace Covid19Radar.ViewModels
 {
