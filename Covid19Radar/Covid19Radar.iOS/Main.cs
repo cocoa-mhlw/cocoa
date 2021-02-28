8d5622f6edc39b3bf8550c8f9f71b00de1bf031b
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-03-01 02:43:44 +0900

    Apply CommonServiceLocator to the iOS.

--- Covid19Radar/Covid19Radar.iOS/Main.cs
+++ Covid19Radar/Covid19Radar.iOS/Main.cs
@@ -1,4 +1,9 @@
-﻿using UIKit;
+﻿using Covid19Radar.iOS.Services;
+using Covid19Radar.iOS.Services.Logs;
+using Covid19Radar.Services;
+using Covid19Radar.Services.Logs;
+using DryIoc;
+using UIKit;
 
 namespace Covid19Radar.iOS
 {
@@ -7,9 +12,20 @@ namespace Covid19Radar.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
+            App.InitializeServiceLocator(RegisterPlatformTypes);
+
+            App.InitializeExposureNotification();
+
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
             UIApplication.Main(args, null, "AppDelegate");
+        }
+
+        private static void RegisterPlatformTypes(Container container)
+        {
+            container.Register<ILogPathDependencyService, LogPathServiceIos>(Reuse.Singleton);
+            container.Register<ISecureStorageDependencyService, SecureStorageServiceIos>(Reuse.Singleton);
+            container.Register<IPreferencesService, PreferencesService>(Reuse.Singleton);
         }
     }
 }
