c518099905472ae03b6c60595e4a1a7065e71672
Takym <15681312+Takym@users.noreply.github.com>
2021-03-13 05:22:37 +0900

    文書コメントを追加。

--- Covid19Radar/Covid19Radar/ViewModels/News/NewsPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/News/NewsPageViewModel.cs
@@ -1,6 +1,8 @@
 ﻿#nullable enable
 
 using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
 using Acr.UserDialogs;
 using Covid19Radar.Resources;
 using Covid19Radar.Services.Logs;
@@ -9,31 +11,61 @@ using Xamarin.Forms;
 
 namespace Covid19Radar.ViewModels
 {
+	/// <summary>
+	///  <see cref="Covid19Radar.Views.NewsPage"/>のイベントを実行します。
+	/// </summary>
 	public class NewsPageViewModel : ViewModelBase
 	{
 		private readonly ILoggerService _logger;
 		private          string?        _g_search;
 
+		/// <summary>
+		///  Google 検索する内容を取得または設定します。
+		/// </summary>
 		public string? GSearch
 		{
 			get => _g_search;
 			set => this.SetProperty(ref _g_search, value);
 		}
 
-		public Command OnSearch => new Command(() => this.ShowPage(
-			$"{AppResources.GoogleSearchUrl}+{Uri.EscapeDataString(_g_search ?? string.Empty)}"
+		/// <summary>
+		///  「最新情報をGoogleで検索」ボタンが押下された時に実行される処理を取得します。
+		/// </summary>
+		public virtual ICommand OnSearch => new Command(async () =>
+			await this.ShowPage( $"{AppResources.GoogleSearchUrl}+{Uri.EscapeDataString(_g_search ?? string.Empty)}"
 		));
-		public Command OnClick_ShowCoronaGoJP    => new Command(() => this.ShowPage(AppResources.CoronaGoJPUrl));
-		public Command OnClick_ShowStopCOVID19JP => new Command(() => this.ShowPage(AppResources.StopCOVID19JPUrl));
-		public Command OnClick_ShowWikipedia     => new Command(() => this.ShowPage(AppResources.WikipediaUrl));
 
+		/// <summary>
+		///  「内閣官房のデータ」ボタンが押下された時に実行される処理を取得します。
+		/// </summary>
+		public virtual ICommand OnClick_ShowCoronaGoJP => new Command(async () => await this.ShowPage(AppResources.CoronaGoJPUrl));
+
+		/// <summary>
+		///  「対策ダッシュボード」ボタンが押下された時に実行される処理を取得します。
+		/// </summary>
+		public virtual ICommand OnClick_ShowStopCOVID19JP => new Command(async () => await this.ShowPage(AppResources.StopCOVID19JPUrl));
+
+		/// <summary>
+		///  「ウィキペディアの情報」ボタンが押下された時に実行される処理を取得します。
+		/// </summary>
+		public virtual ICommand OnClick_ShowWikipedia => new Command(async () => await this.ShowPage(AppResources.WikipediaUrl));
+
+		/// <summary>
+		///  型'<see cref="Covid19Radar.ViewModels.NewsPageViewModel"/>'の新しいインスタンスを生成します。
+		/// </summary>
+		/// <param name="logger">ログの出力先を指定します。</param>
 		public NewsPageViewModel(ILoggerService logger)
 		{
 			_logger    = logger ?? throw new ArgumentNullException(nameof(logger));
 			this.Title = AppResources.NewsPageTitle;
 		}
 
-		private async void ShowPage(string url)
+		/// <summary>
+		///  指定されたWebサイトを表示します。
+		/// </summary>
+		/// <param name="url">Webサイトへの完全なアドレスです。<see langword="null"/>または空文字を指定する事はできません。</param>
+		/// <returns>この処理の非同期操作です。</returns>
+		protected async ValueTask ShowPage(string url)
 		{
 			_logger.StartMethod();
 			if (string.IsNullOrEmpty(url)) {
--- Covid19Radar/Covid19Radar/Views/News/NewsPage.xaml.cs
+++ Covid19Radar/Covid19Radar/Views/News/NewsPage.xaml.cs
@@ -1,8 +1,16 @@
-﻿using Xamarin.Forms;
+﻿#nullable enable
+
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Covid19Radar.Views
 {
+	/// <summary>
+	///  最新情報ページを表します。
+	/// </summary>
+	/// <remarks>
+	///  内部処理は型'<see cref="Covid19Radar.ViewModels.NewsPageViewModel"/>'にあります。
+	/// </remarks>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class NewsPage : ContentPage
 	{
