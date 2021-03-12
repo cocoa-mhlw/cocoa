d29b23438b1c334964d7ded9b5a777b2cfe559c0
Takym <15681312+Takym@users.noreply.github.com>
2021-03-13 06:30:19 +0900

    NewsPageViewModel、最適化。

--- Covid19Radar/Covid19Radar/ViewModels/News/NewsPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/News/NewsPageViewModel.cs
@@ -8,6 +8,7 @@ using Covid19Radar.Resources;
 using Covid19Radar.Services.Logs;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using SysColor = System.Drawing.Color;
 
 namespace Covid19Radar.ViewModels
 {
@@ -16,8 +17,12 @@ namespace Covid19Radar.ViewModels
 	/// </summary>
 	public class NewsPageViewModel : ViewModelBase
 	{
-		private readonly ILoggerService _logger;
-		private          string?        _g_search;
+		private static readonly Uri                  CoronaGoJPUrl    = new Uri(AppResources.CoronaGoJPUrl);
+		private static readonly Uri                  StopCOVID19JPUrl = new Uri(AppResources.StopCOVID19JPUrl);
+		private static readonly Uri                  WikipediaUrl     = new Uri(AppResources.WikipediaUrl);
+		private        readonly ILoggerService       _logger;
+		private                 string?              _g_search;
+		private                 BrowserLaunchOptions _blo;
 
 		/// <summary>
 		///  Google 検索する内容を取得または設定します。
@@ -29,26 +34,33 @@ namespace Covid19Radar.ViewModels
 		}
 
 		/// <summary>
+		///  ブラウザの起動設定を取得または設定します。
+		/// </summary>
+		public BrowserLaunchOptions BrowserLaunchOptions
+		{
+			get => _blo;
+			set => this.SetProperty(ref _blo, value);
+		}
+
+		/// <summary>
 		///  「最新情報をGoogleで検索」ボタンが押下された時に実行される処理を取得します。
 		/// </summary>
-		public virtual ICommand OnSearch => new Command(async () =>
-			await this.ShowPage( $"{AppResources.GoogleSearchUrl}+{Uri.EscapeDataString(_g_search ?? string.Empty)}"
-		));
+		public virtual ICommand OnSearch { get; }
 
 		/// <summary>
 		///  「内閣官房のデータ」ボタンが押下された時に実行される処理を取得します。
 		/// </summary>
-		public virtual ICommand OnClick_ShowCoronaGoJP => new Command(async () => await this.ShowPage(AppResources.CoronaGoJPUrl));
+		public virtual ICommand OnClick_ShowCoronaGoJP { get; }
 
 		/// <summary>
 		///  「対策ダッシュボード」ボタンが押下された時に実行される処理を取得します。
 		/// </summary>
-		public virtual ICommand OnClick_ShowStopCOVID19JP => new Command(async () => await this.ShowPage(AppResources.StopCOVID19JPUrl));
+		public virtual ICommand OnClick_ShowStopCOVID19JP { get; }
 
 		/// <summary>
 		///  「ウィキペディアの情報」ボタンが押下された時に実行される処理を取得します。
 		/// </summary>
-		public virtual ICommand OnClick_ShowWikipedia => new Command(async () => await this.ShowPage(AppResources.WikipediaUrl));
+		public virtual ICommand OnClick_ShowWikipedia { get; }
 
 		/// <summary>
 		///  型'<see cref="Covid19Radar.ViewModels.NewsPageViewModel"/>'の新しいインスタンスを生成します。
@@ -58,6 +70,30 @@ namespace Covid19Radar.ViewModels
 		{
 			_logger    = logger ?? throw new ArgumentNullException(nameof(logger));
 			this.Title = AppResources.NewsPageTitle;
+
+			_blo = new BrowserLaunchOptions() {
+				LaunchMode            = BrowserLaunchMode .SystemPreferred,
+				Flags                 = BrowserLaunchFlags.None,
+				TitleMode             = BrowserTitleMode  .Show,
+				PreferredControlColor = SysColor.FromArgb(0xCC, 0xCC, 0xCC, 0xCC),
+				PreferredToolbarColor = SysColor.FromArgb(0x44, 0x44, 0x44, 0x44)
+			};
+
+			this.OnSearch = new Command(async () => {
+				// await this.ShowPage($"{AppResources.GoogleSearchUrl}+{Uri.EscapeDataString(_g_search ?? string.Empty)}");
+				string url  = AppResources.GoogleSearchUrl;
+				string text = Uri.EscapeDataString(_g_search ?? string.Empty);
+				await this.ShowPage(new Uri(string.Create((url.Length + text.Length + 1), (url, text), (span, arg) => {
+					arg.url.AsSpan().CopyTo(span);
+					span = span.Slice(arg.url.Length);
+					span[0] = '+';
+					arg.text.AsSpan().CopyTo(span.Slice(1));
+				})));
+			});
+
+			this.OnClick_ShowCoronaGoJP    = new Command(async () => await this.ShowPage(CoronaGoJPUrl));
+			this.OnClick_ShowStopCOVID19JP = new Command(async () => await this.ShowPage(StopCOVID19JPUrl));
+			this.OnClick_ShowWikipedia     = new Command(async () => await this.ShowPage(WikipediaUrl));
 		}
 
 		/// <summary>
@@ -65,15 +101,20 @@ namespace Covid19Radar.ViewModels
 		/// </summary>
 		/// <param name="url">Webサイトへの完全なアドレスです。<see langword="null"/>または空文字を指定する事はできません。</param>
 		/// <returns>この処理の非同期操作です。</returns>
-		protected async ValueTask ShowPage(string url)
+		protected async ValueTask ShowPage(Uri url)
 		{
 			_logger.StartMethod();
-			if (string.IsNullOrEmpty(url)) {
+			if (url is null) {
 				_logger.Warning("The URL was null or empty.");
 				await UserDialogs.Instance.AlertAsync(AppResources.NewsPage_UrlWasEmpty);
 			} else {
 				_logger.Info($"The URL: {url}");
-				await Browser.OpenAsync(url);
+				_logger.Debug($"The launch mode: {_blo.LaunchMode}");
+				_logger.Debug($"The flags: {_blo.Flags}");
+				_logger.Debug($"The title mode: {_blo.TitleMode}");
+				_logger.Debug($"The preferred control color: {_blo.PreferredControlColor}");
+				_logger.Debug($"The preferred toolbar color: {_blo.PreferredToolbarColor}");
+				await Browser.OpenAsync(url, _blo);
 			}
 			_logger.EndMethod();
 		}
