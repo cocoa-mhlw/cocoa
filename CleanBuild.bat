c1e59bdec556f747860088802c216264e5541246
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2021-03-31 08:48:01 +0900

    fix comment out

--- CleanBuild.bat
+++ CleanBuild.bat
@@ -1,6 +1,6 @@
-# This Source Code Form is subject to the terms of the Mozilla Public
-# License, v. 2.0. If a copy of the MPL was not distributed with this
-# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+rem This Source Code Form is subject to the terms of the Mozilla Public
+rem License, v. 2.0. If a copy of the MPL was not distributed with this
+rem file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 git clean -xdf --exclude=packages
 
