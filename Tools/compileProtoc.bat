f9ec648f9f7251efe667e191969ade28fb368bbe
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-03-30 17:25:19 +0900

    Add License notice header to batch(.bat) files

--- Tools/compileProtoc.bat
+++ Tools/compileProtoc.bat
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 
 protoc --proto_path=..\src\Covid19Radar.Background\Protobuf\ --csharp_out=..\src\Covid19Radar.Background\Protobuf\ --csharp_opt=file_extension=.g.cs TemporaryExposureKeyExportFileFormat.proto
 
