af565aa549107b301c51e49575954eb91774a181
zipperpull <zipperpull.23@gmail.com>
2021-03-31 23:43:07 +0900

    fix UTF-8 BOM to file top

--- Covid19Radar/Covid19Radar/AssemblyInfo.cs
+++ Covid19Radar/Covid19Radar/AssemblyInfo.cs
@@ -1,8 +1,8 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
-﻿using Xamarin.Forms;
+using Xamarin.Forms;
 [assembly: ExportFont("fa-regular-400.otf", Alias = "FA-R")]
 [assembly: ExportFont("fa-solid-900.otf", Alias = "FA-S")]
 [assembly: ExportFont("fa-brands-400.otf", Alias = "FA-B")]
