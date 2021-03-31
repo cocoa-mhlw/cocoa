fba62a42a7a62ffd4d238c9292f9322e230982fa
Yu Muramatsu <37988559+Murayu0225@users.noreply.github.com>
2021-03-31 13:04:09 +0900

    Update compileProtoc.bat

--- Tools/compileProtoc.bat
+++ Tools/compileProtoc.bat
@@ -1,6 +1,6 @@
-rem This Source Code Form is subject to the terms of the Mozilla Public
-rem License, v. 2.0. If a copy of the MPL was not distributed with this
-rem file, You can obtain one at https://mozilla.org/MPL/2.0/.
+@REM This Source Code Form is subject to the terms of the Mozilla Public
+@REM License, v. 2.0. If a copy of the MPL was not distributed with this
+@REM file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 
 protoc --proto_path=..\src\Covid19Radar.Background\Protobuf\ --csharp_out=..\src\Covid19Radar.Background\Protobuf\ --csharp_opt=file_extension=.g.cs TemporaryExposureKeyExportFileFormat.proto
