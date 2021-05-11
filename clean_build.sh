000c0ff7ac35efca7838edf2a8f4e913dbcedb96
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-05-11 12:54:44 +0900

    Create clean_build.sh

---
+++ clean_build.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+git clean -xdf --exclude=packages
+
+find . -name 'obj' | xargs rm -rf
+find . -name 'bin' | xargs rm -rf
+
