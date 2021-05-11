a00563ad3a0eb20a73f3da4d1e7c3f0d6ede930f
Takym <15681312+Takym@users.noreply.github.com>
2021-05-11 18:35:55 +0900

    `CreateLogRow` を微修正

--- Covid19Radar/Covid19Radar/Services/Logs/LogWriter.cs
+++ Covid19Radar/Covid19Radar/Services/Logs/LogWriter.cs
@@ -1,4 +1,4 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
@@ -151,7 +151,7 @@ namespace Covid19Radar.Services.Logs
             var sb = new StringBuilder();
             for (int i = 0; i < cols.Length; ++i) {
                 if (i != 0) {
-                    sb.Append(", ");
+                    sb.Append(',');
                 }
                 string s = cols[i];
                 sb.Append('\"');
