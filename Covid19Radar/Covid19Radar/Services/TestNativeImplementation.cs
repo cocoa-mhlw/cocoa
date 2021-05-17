f26eb3d8c73e636fb103eacd126661ac890cc55f
I.Maruyama <i-maruyama@fit.ac.jp>
2021-05-17 20:48:24 +0900

    コードのクリンナップ

--- Covid19Radar/Covid19Radar/Services/TestNativeImplementation.cs
+++ Covid19Radar/Covid19Radar/Services/TestNativeImplementation.cs
@@ -4,9 +4,9 @@
 
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text.RegularExpressions;
 using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.ExposureNotifications;
 
