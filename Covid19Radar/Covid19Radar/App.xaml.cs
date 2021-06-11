c496a3b2073bc0604dbd0232f21b183cc1dd4f75
Takym <15681312+Takym@users.noreply.github.com>
2021-06-11 12:29:46 +0900

    `IDateTimeUtility` をサービス化

--- Covid19Radar/Covid19Radar/App.xaml.cs
+++ Covid19Radar/Covid19Radar/App.xaml.cs
@@ -1,4 +1,4 @@
-/* This Source Code Form is subject to the terms of the Mozilla Public
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
@@ -173,6 +173,7 @@ namespace Covid19Radar
         {
             // Services
             container.Register<IUserDataRepository, UserDataRepository>(Reuse.Singleton);
+            container.Register<IDateTimeUtility, DateTimeUtility>(Reuse.Singleton);
             container.Register<ILoggerService, LoggerService>(Reuse.Singleton);
             container.Register<ILogFileService, LogFileService>(Reuse.Singleton);
             container.Register<ILogPathService, LogPathService>(Reuse.Singleton);
