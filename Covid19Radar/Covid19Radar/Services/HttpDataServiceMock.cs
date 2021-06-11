d40ac672e975ed9c3ce3aae541933c7bea3c96b6
I.Maruyama <i-maruyama@fit.ac.jp>
2021-06-12 07:03:50 +0900

    changed for review (before making common class)

--- Covid19Radar/Covid19Radar/Services/HttpDataServiceMock.cs
+++ Covid19Radar/Covid19Radar/Services/HttpDataServiceMock.cs
@@ -22,10 +22,7 @@ namespace Covid19Radar.Services
         private readonly HttpClient downloadClient;
         public HttpDataServiceMock(IHttpClientService httpClientService)
         {
-            if (DownloadRequired())
-            {
                 downloadClient = httpClientService.Create();
-            }
         }
 
         // copy from ./HttpDataService.cs
@@ -64,7 +61,7 @@ namespace Covid19Radar.Services
             });
         }
 
-        private TemporaryExposureKeyExportFileModel TestDat(long created)
+        private TemporaryExposureKeyExportFileModel CreateTestData(long created)
         {
             return new TemporaryExposureKeyExportFileModel()
             {
@@ -74,16 +71,17 @@ namespace Covid19Radar.Services
             };
         }
 
-        private long TimeBefore(int day)
-            => new DateTimeOffset(DateTime.UtcNow.ToLocalTime().AddDays(day)).ToUnixTimeMilliseconds();
+        private long CalcTimeAddDays(int day)
+          => new DateTimeOffset(DateTime.UtcNow.AddDays(day)).ToUnixTimeMilliseconds();
 
-        private long NightBefore(int day)
+        private long CalcMidnightTimeAddDays(int day)
         {
-            DateTime d = DateTime.UtcNow.ToLocalTime().AddDays(day);
+            DateTime d = DateTime.UtcNow.AddDays(day);
             return (new DateTimeOffset(new DateTime(d.Year, d.Month, d.Day, 0, 1, 2, 3)).ToUnixTimeMilliseconds());
+            // set 0 hour,1 min,2 sec,3 millisecond for debug
         }
 
-        private List<TemporaryExposureKeyExportFileModel> DataPreset(int dataVer)
+        private List<TemporaryExposureKeyExportFileModel> DataPreset(int dataVersion)
         {
             /* DataPreset for TEK FileModel
                0(default): nothing (default for v1.2.3)
@@ -91,18 +89,18 @@ namespace Covid19Radar.Services
                2: last night
                please add
              */
-            switch (dataVer)
+            switch (dataVersion)
             {
                 case 2:
-                    return new List<TemporaryExposureKeyExportFileModel> { TestDat(NightBefore(-1)), TestDat(NightBefore(0)) };
+                    return new List<TemporaryExposureKeyExportFileModel> { CreateTestData(CalcMidnightTimeAddDays(-1)), CreateTestData(CalcMidnightTimeAddDays(0)) };
                 case 1:
-                    return new List<TemporaryExposureKeyExportFileModel> { TestDat(TimeBefore(-1)), TestDat(TimeBefore(0)) };
+                    return new List<TemporaryExposureKeyExportFileModel> { CreateTestData(CalcTimeAddDays(-1)), CreateTestData(CalcTimeAddDays(0)) };
                 case 0:
                 default:
                     return new List<TemporaryExposureKeyExportFileModel>();
             }
         }
-        private static bool DownloadRequired()
+        private static bool IsDownloadRequired()
         {
             string url = AppSettings.Instance.CdnUrlBase;
             return (Regex.IsMatch(url, @"^https://.*\..*\..*/$"));
@@ -111,13 +109,13 @@ namespace Covid19Radar.Services
         async Task<List<TemporaryExposureKeyExportFileModel>> IHttpDataService.GetTemporaryExposureKeyList(string region, CancellationToken cancellationToken)
         {
             /* CdnUrlBase trick for Debug_Mock
-               "https://www.example.com/"(url with 2+ periods) -> download "url"+"c19r/440/list.json"
-               "1598022036649,1598022036751,1598022036826" -> direct input timestamps 
-               "https://CDN_URL_BASE/2" -> dataVer = 2
-               "https://CDN_URL_BASE/" -> dataVer = 0 (default)
+               "https://www.example.com/"(url with 2+ periods) -> download "url"+"c19r/440/list.json".  IsDownloadRequired
+               "1598022036649,1598022036751,1598022036826" -> direct input timestamps.  IsDirectInput
+               "https://CDN_URL_BASE/2" -> dataVersion = 2
+               "https://CDN_URL_BASE/" -> dataVersion = 0 (default)
             */
             string url = AppSettings.Instance.CdnUrlBase;
-            if (DownloadRequired())
+            if (IsDownloadRequired())
             {
                 // copy from GetTemporaryExposureKeyList @ ./HttpDataService.cs and delete logger part
                 var container = AppSettings.Instance.BlobStorageContainerName;
@@ -134,16 +132,21 @@ namespace Covid19Radar.Services
                     return new List<TemporaryExposureKeyExportFileModel>();
                 }
             }
-            else if (Regex.IsMatch(url, @"^(\d+,)+\d+,*$"))
+            else if (IsDirectInput(url))
             {
                 Debug.WriteLine("HttpDataServiceMock::GetTemporaryExposureKeyList direct data called");
-                return (url.Split(",").ToList().Select(x => TestDat(Convert.ToInt64(x))).ToList());
+                return (url.Split(",").ToList().Select(x => CreateTestData(Convert.ToInt64(x))).ToList());
             }
             else
             {
                 Debug.WriteLine("HttpDataServiceMock::GetTemporaryExposureKeyList preset data called");
                 return (DataPreset(NumberEndofSentence(url)));
             }
+        }
+
+        bool IsDirectInput(string url)
+        {
+            return Regex.IsMatch(url, @"^(\d+,)+\d+,*$");
         }
 
         // copy from ./TestNativeImplementation.cs
@@ -160,16 +163,26 @@ namespace Covid19Radar.Services
             return (new string[] { urlApi, urlRegister, urlDiagnosis });
         }
 
+        private string UrlApiRegister()
+        {
+            string url = AppSettings.Instance.ApiUrlBase;
+            Regex r = new Regex("/r(egister)?[0-9]+");
+            Regex d = new Regex("/d(iagnosis)?[0-9]+");
+            string urlRegister = r.Match(url).Value;
+            url = r.Replace(url, "");
+            return ( urlRegister);
+        }
+
         // copy from ./TestNativeImplementation.cs
         private ushort NumberEndofSentence(string url)
         {
             Match match = Regex.Match(url, @"(?<d>\d+)$");
-            ushort dataVer = 0;
+            ushort dataVersion = 0;
             if (match.Success)
             {
-                dataVer = Convert.ToUInt16(match.Groups["d"].Value);
+                dataVersion = Convert.ToUInt16(match.Groups["d"].Value);
             }
-            return (dataVer);
+            return (dataVersion);
         }
 
         async Task<bool> IHttpDataService.PostRegisterUserAsync()
@@ -186,14 +199,14 @@ namespace Covid19Radar.Services
         Task<HttpStatusCode> IHttpDataService.PutSelfExposureKeysAsync(DiagnosisSubmissionParameter request)
         {
             var code = HttpStatusCode.OK; // default. for PutSelfExposureKeys NG
-            var dataVer = NumberEndofSentence(UrlApi()[2]);
-            if (dataVer >= 100)
+            var dataVersion = NumberEndofSentence(UrlApi()[2]);
+            if (dataVersion >= 100) // HttpStatusCode >=100 by RFC2616#section-10
             {
-                code = (HttpStatusCode)dataVer;
+                code = (HttpStatusCode)dataVersion;
             }
             else
             {
-                switch (dataVer)
+                switch (dataVersion)
                 {
                     case 1:
                         code = HttpStatusCode.NoContent; //  for Successful PutSelfExposureKeys 
--- Covid19Radar/Covid19Radar/Services/TestNativeImplementation.cs
+++ Covid19Radar/Covid19Radar/Services/TestNativeImplementation.cs
@@ -12,149 +12,165 @@ using Xamarin.ExposureNotifications;
 
 namespace Covid19Radar.Services
 {
-	public class TestNativeImplementation : INativeImplementation
-	{
-		static readonly Random random = new Random();
+    public class TestNativeImplementation : INativeImplementation
+    {
+        static readonly Random random = new Random();
 
-		Task WaitRandom()
-			=> Task.Delay(random.Next(100, 2500));
+        Task WaitRandom()
+            => Task.Delay(random.Next(100, 2500));
 
-		public async Task StartAsync()
-		{
-			await WaitRandom();
-			Preferences.Set("fake_enabled", true);
-		}
+        public async Task StartAsync()
+        {
+            await WaitRandom();
+            Preferences.Set("fake_enabled", true);
+        }
 
-		public async Task StopAsync()
-		{
-			await WaitRandom();
-			Preferences.Set("fake_enabled", false);
-		}
+        public async Task StopAsync()
+        {
+            await WaitRandom();
+            Preferences.Set("fake_enabled", false);
+        }
 
-		public async Task<bool> IsEnabledAsync()
-		{
-			await WaitRandom();
-			return Preferences.Get("fake_enabled", true);
-		}
+        public async Task<bool> IsEnabledAsync()
+        {
+            await WaitRandom();
+            return Preferences.Get("fake_enabled", true);
+        }
 
-		public async Task<IEnumerable<TemporaryExposureKey>> GetSelfTemporaryExposureKeysAsync()
-		{
-			var keys = new List<TemporaryExposureKey>();
+        public async Task<IEnumerable<TemporaryExposureKey>> GetSelfTemporaryExposureKeysAsync()
+        {
+            var keys = new List<TemporaryExposureKey>();
 
-			for (var i = 1; i < 14; i++)
-				keys.Add(GenerateRandomKey(i));
+            for (var i = 1; i < 14; i++)
+                keys.Add(GenerateRandomKey(i));
 
-			await WaitRandom();
+            await WaitRandom();
 
-			return keys;
-		}
+            return keys;
+        }
 
-		public Task<Status> GetStatusAsync()
-			=> Task.FromResult(Preferences.Get("fake_enabled", true) ? Status.Active : Status.Disabled);
+        public Task<Status> GetStatusAsync()
+            => Task.FromResult(Preferences.Get("fake_enabled", true) ? Status.Active : Status.Disabled);
 
-		private ushort[] DataPreset(int dataVer)
-		{
-			/* DataPreset for ExposureDetection
-			   0(default): two low-risk matches (default for v1.2.3)
-			   1: one high-risk match and 2 low-risk matches 
-			   2: no match
-			   3+: please add
-			 */
-			switch (dataVer)
-			{
-				case 1:
-					return (
-						new ushort[] {10, 3, 27, 0, 0, // ExposureDetectionSummary
-									   13, 15, 65, 27, (ushort)RiskLevel.High, // ExposureInfo 1st
-									   10, 15, 65, 5, (ushort)RiskLevel.Medium, // ExposureInfo 2st
-									   11,  5, 40, 3, (ushort)RiskLevel.Low, // ExposureInfo  3nd
-						});
-				case 2:
-					return (
-						new ushort[] {0, 0, 0, 0, 0, // ExposureDetectionSummary
-						});
-				case 0:
-				default:
-					return (
-						new ushort[] {10, 2, 5, 0, 0, // ExposureDetectionSummary
-									   10, 15, 65, 5, (ushort)RiskLevel.Medium, // ExposureInfo 1st (RiskLevel.Medium=4)
-									   11,  5, 40, 3, (ushort)RiskLevel.Low, // ExposureInfo  2nd(RiskLevel.Low=2)
-						});
-			}
-		}
+        enum PresetDataType
+        {
+            Default = 0, // two low-risk matches (default for v1.2.3)
+            OneHighRiskMatchAnd2LowRiskMatches = 1, // one high-risk match and 2 low-risk matches 
+            NoMatch = 2, 
+            // please add "YourDataType = <int>"
+        }
 
-		public string[] UrlApi()
-		{
-			// "url/api" -> { "url/api", "", "" }
-			// "url/api1/register1/diagnosis2" -> { "url/api1", "/register1", "/diagnosis2" } 
-			var url = AppSettings.Instance.ApiUrlBase;
-			var r = new Regex("/r(egister)?[0-9]+");
-			var d = new Regex("/d(iagnosis)?[0-9]+");
-			var urlRegister = r.Match(url).Value;
-			url = r.Replace(url, "");
-			var urlDiagnosis = d.Match(url).Value;
-			url = d.Replace(url, "");
-			var urlApi = url;
-			return (new string[] { urlApi, urlRegister, urlDiagnosis });
-		}
+        private ushort[] DataPreset(int intDataType)
+        {
+            /* DataPreset returns ushort[].
+               index[0] ~ index[4] : data for ExposureDetectionSummary 
+               index[5] ~ index[10] : 1st data for ExposureInfo
+               index[11] ~ index[15] : 2nd data for ExposureInfo
+               index[16] ~ index[20] : 3rd data for ExposureInfo
+             */
+            PresetDataType dataType = (PresetDataType) intDataType;
+            switch (dataType)
+            {
+                case PresetDataType.OneHighRiskMatchAnd2LowRiskMatches:
+                    return (
+                        new ushort[] {10, 3, 27, 0, 0, // ExposureDetectionSummary 
+                                       13, 15, 65, 27, (ushort)RiskLevel.High, // ExposureInfo 1st
+                                       10, 15, 65, 5, (ushort)RiskLevel.Medium, // ExposureInfo 2st
+                                       11,  5, 40, 3, (ushort)RiskLevel.Low, // ExposureInfo  3nd
+                        });
+                case PresetDataType.NoMatch:
+                    return (
+                        new ushort[] {0, 0, 0, 0, 0, // ExposureDetectionSummary
+                        });
+                case PresetDataType.Default:
+                default:
+                    return (
+                        new ushort[] {10, 2, 5, 0, 0, // ExposureDetectionSummary
+                                       10, 15, 65, 5, (ushort)RiskLevel.Medium, // ExposureInfo 1st (RiskLevel.Medium=4)
+                                       11,  5, 40, 3, (ushort)RiskLevel.Low, // ExposureInfo  2nd(RiskLevel.Low=2)
+                        });
+            }
+        }
 
-		public ushort NumberEndofSentence(string url)
-		{
-			var match = Regex.Match(url, @"(?<d>\d+)$");
-			ushort dataVer = 0;
-			if (match.Success)
-			{
-				dataVer = Convert.ToUInt16(match.Groups["d"].Value);
-			}
-			return (dataVer);
-		}
+        public string[] GetUrlPathSegment()
+        {
+            // "url/api" -> { "url/api", "", "" }
+            // "url/base/api/register1/diagnosis2" -> { "url/base/api", "/register1", "/diagnosis2" } 
+            // "url/api1/r1/d2" -> { "url/api1", "/r1", "/d2" } 
+            // "url/api1/d2/r1" -> { "url/api1", "/r1", "/d2" } 
+            var url = AppSettings.Instance.ApiUrlBase;
+            var r = new Regex("/r(egister)?[0-9]+");
+            var d = new Regex("/d(iagnosis)?[0-9]+");
+            var urlRegister = r.Match(url).Value;
+            url = r.Replace(url, "");
+            var urlDiagnosis = d.Match(url).Value;
+            url = d.Replace(url, "");
+            var urlApi = url;
+            return (new string[] { urlApi, urlRegister, urlDiagnosis });
+        }
 
-		private ushort[] Data()
-		{
-			string url = UrlApi()[0];
-			if (Regex.IsMatch(url, @"^(\d+,)+\d+,?$"))
-			{
-				return url.Split(",").ToList().Select(x => Convert.ToUInt16(x)).ToArray();
-			}
-			return DataPreset(NumberEndofSentence(url));
+        public ushort GetNumberEndofSentence(string url)
+        {
+            var match = Regex.Match(url, @"(?<d>\d+)$");
+            ushort dataVer = 0;
+            if (match.Success)
+            {
+                dataVer = Convert.ToUInt16(match.Groups["d"].Value);
+            }
+            return (dataVer);
+        }
 
-		}
+        private ushort[] CreatePresetData()
+        {
+            string url = GetUrlPathSegment()[0];
+            if (Regex.IsMatch(url, @"^(\d+,)+\d+,?$"))
+            {
+                return url.Split(",").ToList().Select(x => Convert.ToUInt16(x)).ToArray();
+            }
+            return DataPreset(GetNumberEndofSentence(url));
 
-		public Task<(ExposureDetectionSummary summary, Func<Task<IEnumerable<ExposureInfo>>> getInfo)> DetectExposuresAsync(IEnumerable<string> files)
-		{
-			/* ApiUrlBase trick for Debug_Mock
-			"10,2,5,0,0,10,15,65,5,4,11,5,40,3,2" -> direct input (the same with default)
-			"https://API_URL_BASE/api2" -> dataVer = 2
-			"https://API_URL_BASE/api" -> dataVer = 0 (default)
-			others -> dataVer is the number at the end of the sentence
-			*/
-			var dataPreset = Data();
-			int i = 0;
-			var summary = new ExposureDetectionSummary(d[i++], d[i++], d[i++], new TimeSpan[d[i++]], d[i++]);
+        }
 
-			Task<IEnumerable<ExposureInfo>> GetInfo()
-			{
-				var info = new List<ExposureInfo>();
-				while (i < d.Length)
-				{
-					info.Add(new ExposureInfo(DateTime.UtcNow.AddDays(-d[i++]), TimeSpan.FromMinutes(d[i++]), d[i++], d[i++], (Xamarin.ExposureNotifications.RiskLevel)d[i++]));
-				};
-				return Task.FromResult<IEnumerable<ExposureInfo>>(info);
-			}
+        public Task<(ExposureDetectionSummary summary, Func<Task<IEnumerable<ExposureInfo>>> getInfo)> DetectExposuresAsync(IEnumerable<string> files)
+        {
+            /* ApiUrlBase trick for Debug_Mock
+            "10,2,5,0,0,10,15,65,5,4,11,5,40,3,2" -> direct input (the same with default)
+            "https://API_URL_BASE/api2" -> dataVer = 2
+            "https://API_URL_BASE/api" -> dataVer = 0 (default)
+            others -> dataVer is the number at the end of the sentence
+            */
+            var dataPreset = CreatePresetData();
+            int index = 0;
+            var summary = new ExposureDetectionSummary(dataPreset[index++], dataPreset[index++],
+                dataPreset[index++], new TimeSpan[dataPreset[index++]], dataPreset[index++]);
+            // c.f.: ExposureDetectionSummary(daysSinceLastExposure=dataPreset[0],matchedKeyCount=dataPreset[1],maximumRiskScore=dataPreset[2],attenuationDurations=new TimeSpan[dataPreset[3]],summationRiskScore=dataPreset[4])
 
-			return Task.FromResult<(ExposureDetectionSummary, Func<Task<IEnumerable<ExposureInfo>>>)>((summary, GetInfo));
-		}
+            Task<IEnumerable<ExposureInfo>> GetInfo()
+            {
+                var info = new List<ExposureInfo>();
+                while (index < dataPreset.Length)
+                {
+                    info.Add(new ExposureInfo(DateTime.UtcNow.AddDays(-dataPreset[index++]),
+                        TimeSpan.FromMinutes(dataPreset[index++]), dataPreset[index++],
+                        dataPreset[index++], (Xamarin.ExposureNotifications.RiskLevel)dataPreset[index++]));
+                    // c.f.: ExposureInfo(DateTime timestamp, TimeSpan duration, int attenuationValue, int totalRiskScore, RiskLevel riskLevel)
+                };
+                return Task.FromResult<IEnumerable<ExposureInfo>>(info);
+            }
 
-		static TemporaryExposureKey GenerateRandomKey(int daysAgo)
-		{
-			var buffer = new byte[16];
-			random.NextBytes(buffer);
+            return Task.FromResult<(ExposureDetectionSummary, Func<Task<IEnumerable<ExposureInfo>>>)>((summary, GetInfo));
+        }
 
-			return new TemporaryExposureKey(
-				buffer,
-				DateTimeOffset.UtcNow.AddDays(-1 * daysAgo),
-				TimeSpan.FromMinutes(random.Next(5, 120)),
-				(RiskLevel)random.Next(1, 8));
-		}
-	}
+        static TemporaryExposureKey GenerateRandomKey(int daysAgo)
+        {
+            var buffer = new byte[16];
+            random.NextBytes(buffer);
+
+            return new TemporaryExposureKey(
+                buffer,
+                DateTimeOffset.UtcNow.AddDays(-1 * daysAgo),
+                TimeSpan.FromMinutes(random.Next(5, 120)),
+                (RiskLevel)random.Next(1, 8));
+        }
+    }
 }
