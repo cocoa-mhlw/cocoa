b1197894b9735c2ec9babfcc10f3e91f15711d7e
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-07-26 18:16:11 +0900

    Create LocalNotificationService

--- Covid19Radar/Covid19Radar.Android/MainApplication.cs
+++ Covid19Radar/Covid19Radar.Android/MainApplication.cs
@@ -39,6 +39,7 @@ namespace Covid19Radar.Droid
             container.Register<ISecureStorageDependencyService, SecureStorageServiceAndroid>(Reuse.Singleton);
             container.Register<IPreferencesService, PreferencesService>(Reuse.Singleton);
             container.Register<IApplicationPropertyService, ApplicationPropertyService>(Reuse.Singleton);
+            container.Register<ILocalNotificationService, LocalNotificationService>(Reuse.Singleton);
 
 #if USE_MOCK
             container.Register<IDeviceVerifier, DeviceVerifierMock>(Reuse.Singleton);
