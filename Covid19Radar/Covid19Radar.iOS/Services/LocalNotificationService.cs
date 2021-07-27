b13134f7298c638aee57d455a00bdc7f8cb2bc42
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2021-07-27 23:32:20 +0900

    Add LocalNotificationService for iOS

--- Covid19Radar/Covid19Radar.iOS/Services/LocalNotificationService.cs
+++ Covid19Radar/Covid19Radar.iOS/Services/LocalNotificationService.cs
@@ -2,7 +2,11 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using System;
+using System.Threading.Tasks;
 using Covid19Radar.Services;
+using Foundation;
+using UserNotifications;
 
 namespace Covid19Radar.iOS.Services
 {
@@ -10,7 +14,35 @@ namespace Covid19Radar.iOS.Services
     {
         public void ShowExposureNotification()
         {
-            throw new System.NotImplementedException();
+            // TODO: iOS側は非同期なのでとりあえずasync/awaitでTaskで囲んでおく
+            _ = Task.Run(() => ScheduleLocalNotification());
+        }
+
+        private async Task ScheduleLocalNotification()
+        {
+            try
+            {
+                var settings = await UNUserNotificationCenter.Current.GetNotificationSettingsAsync();
+                if (settings.AuthorizationStatus != UNAuthorizationStatus.Authorized)
+                {
+                    // TODO: 通知許諾がされていない場合のエラーハンドリングは握りつぶす？
+                    throw new NotImplementedException();
+                }
+
+                var content = new UNMutableNotificationContent();
+
+                // TODO: 文言
+                content.Title = "重要なお知らせ";
+                content.Body = "新型コロナウイルス感染症の陽性登録者と接触した可能性があります。接触確認アプリにアクセスして陽性者との接触を確認してください。";
+
+                // TODO: 発動タイミングは即時で大丈夫か
+                var request = UNNotificationRequest.FromIdentifier(new NSUuid().AsString(), content, null);
+                var notificationCenter = UNUserNotificationCenter.Current;
+                await notificationCenter.AddNotificationRequestAsync(request);
+            }
+            catch
+            {
+            }
         }
     }
 }
