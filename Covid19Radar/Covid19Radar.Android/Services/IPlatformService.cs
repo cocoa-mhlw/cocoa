a5f04bf5a28d84614d6da30a055ca411eafbb000
cocoa-dev <69558847+cocoa-dev@users.noreply.github.com>
2021-09-28 11:48:36 +0900

    Synchronize with the internal repository
    - ホーム画面で動作状況を確認できる

---
+++ Covid19Radar/Covid19Radar.Android/Services/IPlatformService.cs
@@ -0,0 +1,14 @@
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+
+using Android.App;
+
+namespace Covid19Radar.Droid.Services
+{
+    public interface IPlatformService
+    {
+        Activity CurrentActivity { get; }
+        string GooglePlayServiceIdentifier { get; }
+    }
+}
