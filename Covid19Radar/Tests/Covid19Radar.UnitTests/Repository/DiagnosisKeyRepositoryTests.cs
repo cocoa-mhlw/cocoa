6e6153661db539427166633f01742fb4fdbc9ed7
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2021-11-21 14:03:13 +0900

    add DownloadDiagnosisKeysAsyncTests

--- Covid19Radar/Tests/Covid19Radar.UnitTests/Repository/DiagnosisKeyRepositoryTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/Repository/DiagnosisKeyRepositoryTests.cs
@@ -12,6 +12,8 @@ using Moq.Protected;
 using System.Threading.Tasks;
 using System.Net;
 using System.Text;
+using System.IO;
+using System;
 
 namespace Covid19Radar.UnitTests.Repository
 {
@@ -33,11 +35,6 @@ namespace Covid19Radar.UnitTests.Repository
                 mockClientService.Object,
                 mockLoggerService.Object
             );
-
-        public interface IHttpMessageHandler
-        {
-            Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken);
-        }
 
         [Fact]
         public async void GetDiagnosisKeysListAsyncTests_Success()
@@ -75,7 +72,6 @@ namespace Covid19Radar.UnitTests.Repository
             Assert.Equal(2, result[1].Region);
             Assert.Equal("https://example.com/2", result[1].Url);
             Assert.Equal(87654321, result[1].Created);
-
         }
 
         [Theory]
@@ -101,8 +97,95 @@ namespace Covid19Radar.UnitTests.Repository
             var result = await unitUnderTest.GetDiagnosisKeysListAsync("https://example.com", default);
 
             Assert.Equal(0, result.Count);
-            
         }
 
+        [Fact]
+        public async void DownloadDiagnosisKeysAsyncTests_Success()
+        {
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+
+            var text = "";
+            var content = new ByteArrayContent(Encoding.UTF8.GetBytes(text));
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/zip");
+
+            httpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(new HttpResponseMessage
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = content,
+                }));
+
+            var client = new HttpClient(httpMessageHandler.Object, false);
+            mockClientService.Setup(x => x.Create()).Returns(client);
+
+            var entry = new DiagnosisKeyEntry()
+            {
+                Region = 1,
+                Url = "https://example.com/1.zip",
+                Created = 12345678
+            };
+
+            var unitUnderTest = CreateRepository();
+
+            var tmpPath = Path.GetTempPath();
+            var result = await unitUnderTest.DownloadDiagnosisKeysAsync(entry, tmpPath, default);
+            
+            var expectedOutputPath = Path.Combine(tmpPath, "1.zip");
+            Assert.Equal(expectedOutputPath, result);
+
+            if (File.Exists(result))
+            {
+                File.Delete(result);
+            }
+        }
+
+
+        [Theory]
+        [InlineData(System.Net.HttpStatusCode.NotFound)]
+        [InlineData(System.Net.HttpStatusCode.InternalServerError)]
+        public async void DownloadDiagnosisKeysAsyncTests_HttpError(System.Net.HttpStatusCode statusCode)
+        {
+            var httpMessageHandler = new Mock<HttpMessageHandler>();
+
+            var text = "";
+            var content = new ByteArrayContent(Encoding.UTF8.GetBytes(text));
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue(@"application/zip");
+
+            httpMessageHandler.Protected()
+                .Setup<Task<HttpResponseMessage>>("SendAsync", ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .Returns(Task.FromResult(new HttpResponseMessage
+                {
+                    StatusCode = statusCode,
+                    Content = content,
+                }));
+
+            var client = new HttpClient(httpMessageHandler.Object, false);
+            mockClientService.Setup(x => x.Create()).Returns(client);
+
+            var entry = new DiagnosisKeyEntry()
+            {
+                Region = 1,
+                Url = "https://example.com/1.zip",
+                Created = 12345678
+            };
+
+            var unitUnderTest = CreateRepository();
+
+            var tmpPath = Path.GetTempPath();
+
+            await Assert.ThrowsAsync<HttpRequestException>(async () =>
+            {
+                string result = await unitUnderTest.DownloadDiagnosisKeysAsync(entry, tmpPath, default);
+            });
+
+            var expectedOutputPath = Path.Combine(tmpPath, "1.zip");
+            if (File.Exists(expectedOutputPath))
+            {
+                File.Delete(expectedOutputPath);
+            }
+        }
     }
 }
