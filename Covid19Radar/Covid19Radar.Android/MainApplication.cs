89a2cf5820500ea7c2bfa29c55087cae0baf3300
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-11-21 13:01:07 +0900

    Rename class LogPathPlatformService to LocalPathService.

--- Covid19Radar/Covid19Radar.Android/MainApplication.cs
+++ Covid19Radar/Covid19Radar.Android/MainApplication.cs
@@ -82,7 +82,7 @@ namespace Covid19Radar.Droid
         {
             // Services
             container.Register<IBackupAttributeService, BackupAttributeService>(Reuse.Singleton);
-            container.Register<ILogPathPlatformService, LogPathPlatformService>(Reuse.Singleton);
+            container.Register<ILocalPathService, LocalPathService>(Reuse.Singleton);
             container.Register<ILogPeriodicDeleteService, LogPeriodicDeleteService>(Reuse.Singleton);
             container.Register<ISecureStorageDependencyService, Services.SecureStorageService>(Reuse.Singleton);
             container.Register<IPreferencesService, PreferencesService>(Reuse.Singleton);
