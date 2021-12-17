969d6862414f4e8bb293fd0e1694ab431bbadcdb
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-12-17 16:51:24 +0900

    Remove unnecessary Configuration keys.

--- src/Covid19Radar.Api.Common/Extensions/IConfigurationExtension.cs
+++ src/Covid19Radar.Api.Common/Extensions/IConfigurationExtension.cs
@@ -3,7 +3,6 @@
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
 using Microsoft.Extensions.Configuration;
-using System;
 
 namespace Covid19Radar.Api
 {
@@ -22,11 +21,6 @@ namespace Covid19Radar.Api
         public static string CosmosDatabaseId(this IConfiguration config) => config["COSMOS_DATABASE_ID"];
         public static bool CosmosAutoGenerate(this IConfiguration config) => bool.Parse(config["COSMOS_AUTO_GENERATE"]);
 
-        public static byte[] CryptionKey(this IConfiguration config) => Convert.FromBase64String(config["CRYPTION_KEY"]);
-        public static byte[] CryptionIV(this IConfiguration config) => Convert.FromBase64String(config["CRYPTION_IV"]);
-        public static string CryptionHash(this IConfiguration config) => config["CRYPTION_HASH"];
-        public static byte[] CryptionKey2(this IConfiguration config) => Convert.FromBase64String(config["CRYPTION_KEY2"]);
-        public static byte[] CryptionIV2(this IConfiguration config) => Convert.FromBase64String(config["CRYPTION_IV2"]);
         public static string InquiryLogApiKey(this IConfiguration config) => config["InquiryLogApiKey"];
 
     }
