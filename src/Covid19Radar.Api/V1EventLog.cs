5c5db305a85af1299f498b59ca053791a18d1373
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-12-23 13:35:57 +0900

    Add V1EventLog API(blank).

---
+++ src/Covid19Radar.Api/V1EventLog.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+
+namespace Covid19Radar.Api
+{
+    public class V1EventLog
+    {
+        public V1EventLog()
+        {
+        }
+
+        [FunctionName(nameof(V1EventLog))]
+        public async Task<IActionResult> RunAsync(
+        [HttpTrigger(AuthorizationLevel.Function, "put", Route = "v1/event_log")] HttpRequest req
+        )
+        {
+            return new OkObjectResult("");
+        }
+
+    }
+}
