cfe7a33b9e61a288a3d1c913bbbd401c9b73fab7
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2021-12-24 00:13:16 +0900

    calc total exposure minuts

--- Covid19Radar/Covid19Radar/ViewModels/HomePage/LowRiskContactPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/LowRiskContactPageViewModel.cs
@@ -2,40 +2,61 @@
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
-using Covid19Radar.Common;
 using Covid19Radar.Services;
 using Covid19Radar.Services.Logs;
 using Covid19Radar.Resources;
+using Covid19Radar.Repository;
 using Prism.Navigation;
 using Xamarin.Forms;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace Covid19Radar.ViewModels
 {
     public class LowRiskContactPageViewModel: ViewModelBase
     {
         private readonly ILoggerService loggerService;
+        private readonly IUserDataRepository userDataRepository;
 
         public LowRiskContactPageViewModel(
             INavigationService navigationService,
-            ILoggerService loggerService) : base(navigationService)
+            ILoggerService loggerService,
+            IUserDataRepository userDataRepository) : base(navigationService)
         {
             Title = AppResources.TitileUserStatusSettings;
             this.loggerService = loggerService;
+            this.userDataRepository = userDataRepository;
         }
 
-        public override void Initialize(INavigationParameters parameters)
+        public override async void Initialize(INavigationParameters parameters)
         {
             base.Initialize(parameters);
 
             loggerService.StartMethod();
             loggerService.EndMethod();
+
+            var totalNumberOfExposureMinutes = await calcTotalNumberOfExposureMinutes();
+            TotalContactMinutes = $"直近14日間に合計{totalNumberOfExposureMinutes}分間";
         }
 
-        private string _totalContactMinutes = "直近14日間に合計30分間";
+        private string _totalContactMinutes;
         public string TotalContactMinutes
         {
             get => _totalContactMinutes;
             set => SetProperty(ref _totalContactMinutes, value);
         }
+
+        private async Task<int> calcTotalNumberOfExposureMinutes()
+        {
+            var windows = await userDataRepository.GetExposureWindowsAsync();
+            var numberOfExposureMinutesList = windows
+                .ToArray()
+                .Select(aggregateSecondsSinceLastScans);
+
+            return numberOfExposureMinutesList.Aggregate(0, (sum, x) => sum + x);
+        }
+
+        private int aggregateSecondsSinceLastScans(Chino.ExposureWindow window) =>
+            window.ScanInstances.Select(x => x.SecondsSinceLastScan).Aggregate(0, (sum, x) => sum + x);
     }
 }
