594edc552c20d606efdae9d7ab5a847ea7a4740e
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2021-12-25 16:07:33 +0900

    COCOA2 send all teks took from EN API.

--- Covid19Radar/Covid19Radar/Common/AppConstants.cs
+++ Covid19Radar/Covid19Radar/Common/AppConstants.cs
@@ -7,11 +7,6 @@ namespace Covid19Radar.Common
     public static class AppConstants
     {
         /// <summary>
-        /// Number of days covered from the date of diagnosis or onset
-        /// </summary>
-        public const int DaysToSendTek = -3;
-
-        /// <summary>
         /// Max Error Count
         /// </summary>
         public const int MaxErrorCount = 3;
--- Covid19Radar/Covid19Radar/ViewModels/HomePage/NotifyOtherPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/NotifyOtherPageViewModel.cs
@@ -317,22 +317,12 @@ namespace Covid19Radar.ViewModels
 
                 loggerService.Info($"TemporaryExposureKeys-count: {temporaryExposureKeyList.Count()}");
 
-                IList<TemporaryExposureKey> filteredTemporaryExposureKeyList
-                    = TemporaryExposureKeyUtils.FiilterTemporaryExposureKeys(
-                        temporaryExposureKeyList,
-                        _diagnosisDate,
-                        AppConstants.DaysToSendTek,
-                        loggerService
-                        );
-
-                loggerService.Info($"FilteredTemporaryExposureKeys-count: {filteredTemporaryExposureKeyList.Count()}");
-
                 // TODO: Save and use revoke operation.
                 string idempotencyKey = Guid.NewGuid().ToString();
 
                 IList<HttpStatusCode> httpStatusCodes = await diagnosisKeyRegisterServer.SubmitDiagnosisKeysAsync(
                     _diagnosisDate,
-                    filteredTemporaryExposureKeyList,
+                    temporaryExposureKeyList,
                     ProcessingNumber,
                     idempotencyKey
                     );
