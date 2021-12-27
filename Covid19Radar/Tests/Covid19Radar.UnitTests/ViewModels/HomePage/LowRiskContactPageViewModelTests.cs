8b946964eb8b283ffa3b5d7cf24646deb9d88e8e
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2021-12-27 15:06:21 +0900

    remove using

--- Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/LowRiskContactPageViewModelTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/LowRiskContactPageViewModelTests.cs
@@ -1,7 +1,6 @@
 ﻿// This Source Code Form is subject to the terms of the Mozilla Public
 // License, v. 2.0. If a copy of the MPL was not distributed with this
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
-using System;
 using Covid19Radar.ViewModels;
 using Covid19Radar.Repository;
 using Covid19Radar.Services.Logs;
