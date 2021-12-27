b8042d446f266471f76fcac96feee6d68d745e23
Kazuhiro Hayashi <kazuhiro.hayashi.49@gmail.com>
2021-12-27 14:37:31 +0900

    refactor test

--- Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/LowRiskContactPageViewModelTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/LowRiskContactPageViewModelTests.cs
@@ -43,16 +43,28 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
         }
 
         [Theory]
-        [InlineData(new int[]{ 60 }, "1分")]
-        [InlineData(new int[] { 60, 60 }, "2分")]
-        [InlineData(new int[] { 60 * 60 }, "1時間")]
-        [InlineData(new int[] { 60 * 59, 60 * 5 }, "1時間4分")]
-        [InlineData(new int[] { 60 * 60 * 10 }, "10時間")]
-        [InlineData(new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2時間50分")]
-        public void Initialize_Time_ja(int[] seconds, string expected)
+        [InlineData("ja-JP", new int[] { 60 }, "1分")]
+        [InlineData("ja-JP", new int[] { 60, 60 }, "2分")]
+        [InlineData("ja-JP", new int[] { 60 * 60 }, "1時間")]
+        [InlineData("ja-JP", new int[] { 60 * 59, 60 * 5 }, "1時間4分")]
+        [InlineData("ja-JP", new int[] { 60 * 60 * 10 }, "10時間")]
+        [InlineData("ja-JP", new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2時間50分")]
+        [InlineData("en-US", new int[] { 60 }, "1min")]
+        [InlineData("en-US", new int[] { 60, 60 }, "2min")]
+        [InlineData("en-US", new int[] { 60 * 60 }, "1h")]
+        [InlineData("en-US", new int[] { 60 * 59, 60 * 5 }, "1h4min")]
+        [InlineData("en-US", new int[] { 60 * 60 * 10 }, "10h")]
+        [InlineData("en-US", new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2h50min")]
+        [InlineData("zh-Hans",new int[] { 60 }, "1分钟")]
+        [InlineData("zh-Hans", new int[] { 60, 60 }, "2分钟")]
+        [InlineData("zh-Hans", new int[] { 60 * 60 }, "1小时")]
+        [InlineData("zh-Hans", new int[] { 60 * 59, 60 * 5 }, "1小时4分钟")]
+        [InlineData("zh-Hans", new int[] { 60 * 60 * 10 }, "10小时")]
+        [InlineData("zh-Hans", new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2小时50分钟")]
+        public void Initialize_Time(string language, int[] seconds, string expected)
         {
             var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("ja-JP");
+            AppResources.Culture = new CultureInfo(language);
 
             var dummyExposureWindowList = new List<ExposureWindow>();
 
@@ -88,16 +100,31 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
         }
 
         [Theory]
-        [InlineData(new int[] { 60 }, "1min")]
-        [InlineData(new int[] { 60, 60 }, "2min")]
-        [InlineData(new int[] { 60 * 60 }, "1h")]
-        [InlineData(new int[] { 60 * 59, 60 * 5 }, "1h4min")]
-        [InlineData(new int[] { 60 * 60 * 10 }, "10h")]
-        [InlineData(new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2h50min")]
-        public void Initialize_Time_en(int[] seconds, string expected)
+        [InlineData("ja-JP", new int[] { 5 }, "0分")]
+        [InlineData("ja-JP", new int[] { -5 }, "0分")]
+        [InlineData("ja-JP", new int[] { 59 }, "0分")]
+        [InlineData("ja-JP", new int[] { 61 }, "1分")]
+        [InlineData("ja-JP", new int[] { 3599 }, "59分")]
+        [InlineData("ja-JP", new int[] { 3601 }, "1時間")]
+        [InlineData("ja-JP", new int[] { 5432 }, "1時間30分")]
+        [InlineData("en-US", new int[] { 5 }, "0min")]
+        [InlineData("en-US", new int[] { -5 }, "0min")]
+        [InlineData("en-US", new int[] { 59 }, "0min")]
+        [InlineData("en-US", new int[] { 61 }, "1min")]
+        [InlineData("en-US", new int[] { 3599 }, "59min")]
+        [InlineData("en-US", new int[] { 3601 }, "1h")]
+        [InlineData("en-US", new int[] { 5432 }, "1h30min")]
+        [InlineData("zh-Hans", new int[] { 5 }, "0分钟")]
+        [InlineData("zh-Hans", new int[] { -5 }, "0分钟")]
+        [InlineData("zh-Hans", new int[] { 59 }, "0分钟")]
+        [InlineData("zh-Hans", new int[] { 61 }, "1分钟")]
+        [InlineData("zh-Hans", new int[] { 3599 }, "59分钟")]
+        [InlineData("zh-Hans", new int[] { 3601 }, "1小时")]
+        [InlineData("zh-Hans", new int[] { 5432 }, "1小时30分钟")]
+        public void Initialize_Time_Irregular_Time(string language, int[] seconds, string expected)
         {
             var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("en-US");
+            AppResources.Culture = new CultureInfo(language);
 
             var dummyExposureWindowList = new List<ExposureWindow>();
 
@@ -133,38 +160,17 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
         }
 
         [Theory]
-        [InlineData(new int[] { 60 }, "1分钟")]
-        [InlineData(new int[] { 60, 60 }, "2分钟")]
-        [InlineData(new int[] { 60 * 60 }, "1小时")]
-        [InlineData(new int[] { 60 * 59, 60 * 5 }, "1小时4分钟")]
-        [InlineData(new int[] { 60 * 60 * 10 }, "10小时")]
-        [InlineData(new int[] { 60 * 50, 60 * 20, 60 * 100 }, "2小时50分钟")]
-        public void Initialize_Time_zh(int[] seconds, string expected)
+        [InlineData("ja-JP", "0分")]
+        [InlineData("en-US", "0min")]
+        [InlineData("zh-Hans", "0分钟")]
+        public void Initialize_Time_Irregular_ParseError_ja(string cluture, string expected)
         {
             var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("zh-Hans");
-
-            var dummyExposureWindowList = new List<ExposureWindow>();
-
-            foreach (var second in seconds)
-            {
-                dummyExposureWindowList.Add(
-                    new ExposureWindow()
-                    {
-                        ScanInstances = new List<ScanInstance>()
-                        {
-                            new ScanInstance()
-                            {
-                                SecondsSinceLastScan = second
-                            }
-                        }
-                    }
-                );
-            }
+            AppResources.Culture = new CultureInfo(cluture);
 
             mockUserDataRepository
                 .Setup(x => x.GetExposureWindowsAsync())
-                .Returns(Task.FromResult(dummyExposureWindowList));
+                .Throws(new JsonSerializationException("parse error mock"));
 
             var lowRiskContactPageViewModel = CreateViewModel();
             var parameters = new NavigationParameters();
@@ -173,116 +179,6 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
             mockUserDataRepository
                 .Verify(x => x.GetExposureWindowsAsync(), Times.Once);
             Assert.Equal(expected, lowRiskContactPageViewModel.TotalContactTime);
-
-            AppResources.Culture = originalCalture;
-        }
-
-        [Theory]
-        [InlineData(new int[] { 5 }, "0分")]
-        [InlineData(new int[] { -5 }, "0分")]
-        [InlineData(new int[] { 59 }, "0分")]
-        [InlineData(new int[] { 61 }, "1分")]
-        [InlineData(new int[] { 3599 }, "59分")]
-        [InlineData(new int[] { 3601 }, "1時間")]
-        [InlineData(new int[] { 5432 }, "1時間30分")]
-        public void Initialize_Time_Irregular_Time(int[] seconds, string expected)
-        {
-            var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("ja-JP");
-
-            var dummyExposureWindowList = new List<ExposureWindow>();
-
-            foreach (var second in seconds)
-            {
-                dummyExposureWindowList.Add(
-                    new ExposureWindow()
-                    {
-                        ScanInstances = new List<ScanInstance>()
-                        {
-                            new ScanInstance()
-                            {
-                                SecondsSinceLastScan = second
-                            }
-                        }
-                    }
-                );
-            }
-
-            mockUserDataRepository
-                .Setup(x => x.GetExposureWindowsAsync())
-                .Returns(Task.FromResult(dummyExposureWindowList));
-
-            var lowRiskContactPageViewModel = CreateViewModel();
-            var parameters = new NavigationParameters();
-            lowRiskContactPageViewModel.Initialize(parameters);
-
-            mockUserDataRepository
-                .Verify(x => x.GetExposureWindowsAsync(), Times.Once);
-            Assert.Equal(expected, lowRiskContactPageViewModel.TotalContactTime);
-
-            AppResources.Culture = originalCalture;
-        }
-
-        [Fact]
-        public void Initialize_Time_Irregular_ParseError_ja()
-        {
-            var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("ja-JP");
-
-            mockUserDataRepository
-                .Setup(x => x.GetExposureWindowsAsync())
-                .Throws(new JsonSerializationException("parse error mock"));
-
-            var lowRiskContactPageViewModel = CreateViewModel();
-            var parameters = new NavigationParameters();
-            lowRiskContactPageViewModel.Initialize(parameters);
-
-            mockUserDataRepository
-                .Verify(x => x.GetExposureWindowsAsync(), Times.Once);
-            Assert.Equal("0分", lowRiskContactPageViewModel.TotalContactTime);
-
-            AppResources.Culture = originalCalture;
-        }
-
-        [Fact]
-        public void Initialize_Time_Irregular_ParseError_en()
-        {
-            var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("en-US");
-
-            mockUserDataRepository
-                .Setup(x => x.GetExposureWindowsAsync())
-                .Throws(new JsonSerializationException("parse error mock"));
-
-            var lowRiskContactPageViewModel = CreateViewModel();
-            var parameters = new NavigationParameters();
-            lowRiskContactPageViewModel.Initialize(parameters);
-
-            mockUserDataRepository
-                .Verify(x => x.GetExposureWindowsAsync(), Times.Once);
-            Assert.Equal("0min", lowRiskContactPageViewModel.TotalContactTime);
-
-            AppResources.Culture = originalCalture;
-        }
-
-
-        [Fact]
-        public void Initialize_Time_Irregular_ParseError_zh()
-        {
-            var originalCalture = AppResources.Culture;
-            AppResources.Culture = new CultureInfo("zh-Hans");
-
-            mockUserDataRepository
-                .Setup(x => x.GetExposureWindowsAsync())
-                .Throws(new JsonSerializationException("parse error mock"));
-
-            var lowRiskContactPageViewModel = CreateViewModel();
-            var parameters = new NavigationParameters();
-            lowRiskContactPageViewModel.Initialize(parameters);
-
-            mockUserDataRepository
-                .Verify(x => x.GetExposureWindowsAsync(), Times.Once);
-            Assert.Equal("0分钟", lowRiskContactPageViewModel.TotalContactTime);
 
             AppResources.Culture = originalCalture;
         }
