6c4b689e127d4dad32571571cb8efc681990c316
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-01-08 16:52:20 +0900

    Fix build.

--- Covid19Radar/Covid19Radar/ViewModels/HomePage/NotifyOtherPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/HomePage/NotifyOtherPageViewModel.cs
@@ -178,7 +178,7 @@ namespace Covid19Radar.ViewModels
         {
             loggerService.StartMethod();
 
-            var param = new NavigationParameters();
+            INavigationParameters param = new NavigationParameters();
             param = SubmitConsentPage.BuildNavigationParams(true, ProcessingNumber, param);
 
             var result = await NavigationService.NavigateAsync("SubmitConsentPage", param);
