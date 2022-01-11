b69ba6512bb36f06381183733ce1c28be9f0fe6f
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-01-11 11:47:18 +0900

    Send new ExposureData only.

--- Covid19Radar/Covid19Radar/Services/ExposureDetectionService.cs
+++ Covid19Radar/Covid19Radar/Services/ExposureDetectionService.cs
@@ -125,7 +125,7 @@ namespace Covid19Radar.Services
                     exposureConfiguration,
                     _deviceInfoUtility.Model,
                     enVersionStr,
-                    dailySummaries, exposureWindows
+                    newDailySummaries, newExposureWindows
                     );
             }
             catch (Exception e)
@@ -141,7 +141,7 @@ namespace Covid19Radar.Services
                     exposureConfiguration,
                     _deviceInfoUtility.Model,
                     enVersionStr,
-                    dailySummaries, exposureWindows
+                    newDailySummaries, newExposureWindows
                     );
             }
             catch (Exception e)
