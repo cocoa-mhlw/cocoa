1b8d7999831c12a218d0c96ff3f5cc94505599b7
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-01-12 11:31:04 +0900

    Use [Theory].

--- Covid19Radar/Tests/Covid19Radar.UnitTests/Services/ExposureRiskCalculationServiceTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/Services/ExposureRiskCalculationServiceTests.cs
@@ -21,8 +21,10 @@ namespace Covid19Radar.UnitTests.Services
             return new ExposureRiskCalculationService();
         }
 
-        [Fact]
-        public void LowRiskExposureTest()
+        [Theory]
+        [InlineData(1999, RiskLevel.Low)]
+        [InlineData(2000, RiskLevel.High)]
+        public void RiskExposureTest(double scoreSum, RiskLevel expected)
         {
 
             var dailySummary = new DailySummary()
@@ -30,7 +32,7 @@ namespace Covid19Radar.UnitTests.Services
                     DateMillisSinceEpoch = 0,
                     DaySummary = new ExposureSummaryData()
                     {
-                        ScoreSum = 1999.0
+                        ScoreSum = scoreSum
                     },
                     ConfirmedClinicalDiagnosisSummary = new ExposureSummaryData(),
                     ConfirmedTestSummary = new ExposureSummaryData(),
@@ -54,43 +56,7 @@ namespace Covid19Radar.UnitTests.Services
 
             RiskLevel result = service.CalcRiskLevel(dailySummary, exposureWindows);
 
-            Assert.Equal(RiskLevel.Low, result);
-        }
-
-        [Fact]
-        public void HighRiskExposureTest()
-        {
-
-            var dailySummary = new DailySummary()
-            {
-                DateMillisSinceEpoch = 0,
-                DaySummary = new ExposureSummaryData()
-                {
-                    ScoreSum = 2000.0
-                },
-                ConfirmedClinicalDiagnosisSummary = new ExposureSummaryData(),
-                ConfirmedTestSummary = new ExposureSummaryData(),
-                RecursiveSummary = new ExposureSummaryData(),
-                SelfReportedSummary = new ExposureSummaryData()
-            };
-
-            var exposureWindows = new List<ExposureWindow>()
-            {
-                new ExposureWindow()
-                {
-                    CalibrationConfidence = CalibrationConfidence.High,
-                    DateMillisSinceEpoch = 0,
-                    Infectiousness = Infectiousness.High,
-                    ReportType = ReportType.Unknown,
-                    ScanInstances = new List<ScanInstance>()
-                }
-            };
-
-            IExposureRiskCalculationService service = CreateService();
-
-            RiskLevel result = service.CalcRiskLevel(dailySummary, exposureWindows);
-
-            Assert.Equal(RiskLevel.High, result);
+            Assert.Equal(expected, result);
         }
     }
 }
