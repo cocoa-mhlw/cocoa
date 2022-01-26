206dbdb1f1e33971dbb5ecc7a7f54eafc044e5a7
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-01-27 00:06:48 +0900

    Revert "Connect to V2DiagnosisApi."
    This reverts commit 1e8913c623baeb0983df176871adf4f07cfd5d11.

--- Covid19Radar/Covid19Radar/Common/AppConstants.cs
+++ Covid19Radar/Covid19Radar/Common/AppConstants.cs
@@ -14,7 +14,7 @@ namespace Covid19Radar.Common
         /// <summary>
         /// Number of days covered from the date of diagnosis or onset
         /// </summary>
-        public const int DaysToSendTek = -3;
+        public const int DaysToSendTek = -15;
 
         /// <summary>
         /// Max Error Count
@@ -47,7 +47,7 @@ namespace Covid19Radar.Common
         /// DiagnosisApi version.
         /// (e.g. v2, v3)
         /// </summary>
-        public const string DiagnosisApiVersionCode = "v2";
+        public const string DiagnosisApiVersionCode = "v3";
 
         /// <summary>
         /// Number of day(s) that ExposureConfiguration file downloaded cache.
