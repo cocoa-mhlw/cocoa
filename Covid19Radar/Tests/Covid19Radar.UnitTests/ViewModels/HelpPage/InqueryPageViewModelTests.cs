1c1e501fd2f8767302a2b85f765e3ec378c2315b
cocoa-dev005 <93515738+cocoa-dev005@users.noreply.github.com>
2022-02-07 11:55:23 +0900

    Fix unit tests

--- Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HelpPage/InqueryPageViewModelTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HelpPage/InqueryPageViewModelTests.cs
@@ -4,6 +4,7 @@
 
 using System;
 using System.Threading.Tasks;
+using Covid19Radar.Services;
 using Covid19Radar.Services.Logs;
 using Covid19Radar.ViewModels;
 using Moq;
@@ -18,20 +19,23 @@ namespace Covid19Radar.UnitTests.ViewModels
         private readonly MockRepository mockRepository;
         private readonly Mock<INavigationService> mockNavigationService;
         private readonly Mock<ILoggerService> mockLoggerService;
-
+        private readonly Mock<IEssentialsService> mockEssentialsService;
 
         public InqueryPageViewModelTests()
         {
             mockRepository = new MockRepository(MockBehavior.Default);
             mockNavigationService = mockRepository.Create<INavigationService>();
             mockLoggerService = mockRepository.Create<ILoggerService>();
+            mockEssentialsService = mockRepository.Create<IEssentialsService>();
         }
 
         private InqueryPageViewModel CreateViewModel()
         {
             var vm = new InqueryPageViewModel(
                 mockNavigationService.Object,
-                mockLoggerService.Object);
+                mockLoggerService.Object,
+                mockEssentialsService.Object
+                );
             return vm;
         }
 
