c6bce49da1a6b21c273f467875c421e28e7e6853
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-02-21 19:24:50 +0900

    Remove a paragraph about taking a screenshot.

--- Covid19Radar/Covid19Radar/Resources/AppResources.Designer.cs
+++ Covid19Radar/Covid19Radar/Resources/AppResources.Designer.cs
@@ -1043,12 +1043,6 @@ namespace Covid19Radar.Resources {
             }
         }
         
-        public static string ContactedNotifyPageDescription0 {
-            get {
-                return ResourceManager.GetString("ContactedNotifyPageDescription0", resourceCulture);
-            }
-        }
-        
         public static string ContactedNotifyPageDescription1 {
             get {
                 return ResourceManager.GetString("ContactedNotifyPageDescription1", resourceCulture);
