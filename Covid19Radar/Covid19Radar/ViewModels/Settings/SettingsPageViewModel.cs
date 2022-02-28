146344da487b97d2ceb5c75f074c5f8d645addb4
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-02-28 13:18:40 +0900

    Bump up version to v2.0.0-beta04(closed)

--- Covid19Radar/Covid19Radar/ViewModels/Settings/SettingsPageViewModel.cs
+++ Covid19Radar/Covid19Radar/ViewModels/Settings/SettingsPageViewModel.cs
@@ -19,7 +19,7 @@ namespace Covid19Radar.ViewModels
 {
     public class SettingsPageViewModel : ViewModelBase
     {
-        private const string VERSION_SUFFIX = "-beta03";
+        private const string VERSION_SUFFIX = "-beta04";
 
         private readonly ILoggerService loggerService;
         private string _AppVersion;
