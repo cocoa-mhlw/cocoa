88a9f6a1a8a18c437174ca55bd262c7b7b84c2d9
cocoa-test002 <100112852+cocoa-test002@users.noreply.github.com>
2022-07-01 15:19:45 +0900

    #127 DismissSpringboardAlerts

--- Covid19Radar/Tests/Covid19Radar.UITestV2/Tests/S20_Tests.cs
+++ Covid19Radar/Tests/Covid19Radar.UITestV2/Tests/S20_Tests.cs
@@ -64,7 +64,7 @@ namespace CovidRadar.UITestV2
                 tutorialPageFlow.Tutorial();
             }
 
-            AppManager.DismissSpringboardAlerts();
+            //AppManager.DismissSpringboardAlerts();
 
             HomePage homePage = new HomePage();
             homePage.AssertHomePage();
