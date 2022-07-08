284261f3ea66869436ac526301475bbf58130432
cocoa-dev004 <66989461+cocoa-dev004@users.noreply.github.com>
2022-07-08 15:52:55 +0900

    Fixed file name of event log

--- Covid19Radar/Covid19Radar/Repository/EventLogRepository.cs
+++ Covid19Radar/Covid19Radar/Repository/EventLogRepository.cs
@@ -62,7 +62,7 @@ namespace Covid19Radar.Repository
 
             using var sha = SHA256.Create();
             var textBytes = Encoding.UTF8.GetBytes(clearText);
-            string hash = Convert.ToBase64String(sha.ComputeHash(textBytes));
+            string hash = Convert.ToBase64String(sha.ComputeHash(textBytes)).TrimEnd('=').Replace('+', '-').Replace('/', '_');
 
             return $"{hash}{LOG_EXTENSION}";
         }
@@ -100,6 +100,7 @@ namespace Covid19Radar.Repository
                 }
 
                 await File.WriteAllTextAsync(filePath, serializedJson);
+                _loggerService.Info($"Write event log. filePath:{filePath}");
 
                 return true;
             }
