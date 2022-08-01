3765dd869c7d60b978b98e175d7aff4c45dc8850
cocoa-dev004 <66989461+cocoa-dev004@users.noreply.github.com>
2022-08-01 16:54:13 +0900

    Fixed unit tests

--- Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/ContactedNotifyPageViewModelTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/ViewModels/HomePage/ContactedNotifyPageViewModelTests.cs
@@ -15,6 +15,7 @@ using Covid19Radar.Resources;
 using Covid19Radar.Services;
 using Covid19Radar.Services.Logs;
 using Covid19Radar.ViewModels;
+using Covid19Radar.Views;
 using Moq;
 using Prism.Navigation;
 using Xunit;
@@ -105,7 +106,7 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
                 .Returns(RiskLevel.High);
 
             var contactedNotifyViewModel = CreateViewModel();
-            contactedNotifyViewModel.Initialize(new NavigationParameters());
+            contactedNotifyViewModel.Initialize(ContactedNotifyPage.BuildNavigationParams(new V1ExposureRiskCalculationConfiguration()));
 
             Assert.Equal("0001年1月1日 月曜日以前\n2 件", contactedNotifyViewModel.ExposureCount);
             Assert.Equal("1971年1月1日 金曜日以降\n1日間に合計1分間の接触", contactedNotifyViewModel.ExposureDurationInMinutes);
@@ -192,6 +193,78 @@ namespace Covid19Radar.UnitTests.ViewModels.HomePage
 
             Assert.Empty(contactedNotifyViewModel.ExposureDurationInMinutes);
             Assert.Equal("2 件", contactedNotifyViewModel.ExposureCount);
+        }
+
+        [Fact]
+        public async Task OnExposureListTest()
+        {
+            mockExposureDataRepository
+                .Setup(x => x.GetExposureInformationList(AppConstants.TermOfExposureRecordValidityInDays))
+                .Returns(new List<UserExposureInfo>()
+                {
+                    new UserExposureInfo(),
+                    new UserExposureInfo()
+                });
+            mockExposureDataRepository
+                .Setup(x => x.GetDailySummariesAsync(AppConstants.TermOfExposureRecordValidityInDays))
+                .Returns(Task.FromResult(new List<DailySummary>()
+                {
+                    new DailySummary()
+                    {
+                        DateMillisSinceEpoch = 1000l * 60 * 60 * 24 * 365
+                    }
+                }));
+            mockExposureDataRepository
+                .Setup(x => x.GetExposureWindowsAsync(AppConstants.TermOfExposureRecordValidityInDays))
+                .Returns(Task.FromResult(new List<ExposureWindow>()
+                {
+                    new ExposureWindow()
+                    {
+                        DateMillisSinceEpoch = 1000l * 60 * 60 * 24 * 365,
+                        ScanInstances = new List<ScanInstance>() {
+                            new ScanInstance()
+                            {
+                                SecondsSinceLastScan = 60
+                            }
+                        }
+                    }
+                }));
+            mockExposureRiskCalculationService
+                .Setup(x => x.CalcRiskLevel(It.IsAny<DailySummary>(), It.IsAny<List<ExposureWindow>>(), It.IsAny<V1ExposureRiskCalculationConfiguration>()))
+                .Returns(RiskLevel.High);
+
+            var exposureRiskCalculationConfiguration =
+                new V1ExposureRiskCalculationConfiguration
+                {
+                    FormatVersion = 1,
+                    DailySummary_DaySummary_ScoreSum = new V1ExposureRiskCalculationConfiguration.Threshold { Op = "Op1", Value = 1 },
+                    DailySummary_WeightedDurationAverage = new V1ExposureRiskCalculationConfiguration.Threshold { Op = "Op2", Value = 2 },
+                    ExposureWindow_ScanInstance_SecondsSinceLastScanSum = new V1ExposureRiskCalculationConfiguration.Threshold { Op = "Op3", Value = 3 },
+                    ExposureWindow_ScanInstance_TypicalAttenuationDb_Max = new V1ExposureRiskCalculationConfiguration.Threshold { Op = "Op4", Value = 4 },
+                    ExposureWindow_ScanInstance_TypicalAttenuationDb_Min = new V1ExposureRiskCalculationConfiguration.Threshold { Op = "Op5", Value = 5 }
+                };
+
+            ContactedNotifyPageViewModel unitUnderTest = CreateViewModel();
+            unitUnderTest.Initialize(ContactedNotifyPage.BuildNavigationParams(exposureRiskCalculationConfiguration));
+            await unitUnderTest.OnExposureList.ExecuteAsync();
+
+            mockNavigationService.Verify(x => x.NavigateAsync(
+                nameof(ExposuresPage),
+                It.Is<INavigationParameters>(x =>
+                    x.ContainsKey(ExposuresPage.ExposureRiskCalculationConfigurationKey) &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).FormatVersion == 1 &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).DailySummary_DaySummary_ScoreSum.Op == "Op1" &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).DailySummary_DaySummary_ScoreSum.Value == 1 &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).DailySummary_WeightedDurationAverage.Op == "Op2" &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).DailySummary_WeightedDurationAverage.Value == 2 &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_SecondsSinceLastScanSum.Op == "Op3" &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_SecondsSinceLastScanSum.Value == 3 &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_TypicalAttenuationDb_Max.Op == "Op4" &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_TypicalAttenuationDb_Max.Value == 4 &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_TypicalAttenuationDb_Min.Op == "Op5" &&
+                    x.GetValue<V1ExposureRiskCalculationConfiguration>(ExposuresPage.ExposureRiskCalculationConfigurationKey).ExposureWindow_ScanInstance_TypicalAttenuationDb_Min.Value == 5
+                )
+            ));
         }
     }
 }
