51b2c0a67228740ec76c2fa9b6f1f74777841b78
ARIYAMA Keiji <keiji.ariyama@gmail.com>
2022-08-20 10:43:59 +0900

    Fix issue that crash after upgrade to v2.1.0.

---
+++ Covid19Radar/Covid19Radar.iOS/Services/Migration/BGTaskMigrator.cs
@@ -0,0 +1,43 @@
+﻿// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+using System.Threading.Tasks;
+using BackgroundTasks;
+using Covid19Radar.Services.Logs;
+using Xamarin.Essentials;
+
+namespace Covid19Radar.iOS.Services.Migration
+{
+    internal class BGTaskMigrator
+    {
+        // Array of old identifier.
+        private static readonly string[] OLD_IDENTIFIER_ARRAY = {
+            AppInfo.PackageName + ".delete-old-logs",
+        };
+
+        private readonly ILoggerService _loggerService;
+
+        public BGTaskMigrator(
+            ILoggerService loggerService
+            )
+        {
+            _loggerService = loggerService;
+        }
+
+
+        internal Task ExecuteAsync()
+        {
+            _loggerService.StartMethod();
+
+            foreach(var identifier in OLD_IDENTIFIER_ARRAY)
+            {
+                BGTaskScheduler.Shared.Cancel(identifier);
+                _loggerService.Info($"BGTask {identifier} has been canceled.");
+            }
+
+            _loggerService.EndMethod();
+
+            return Task.CompletedTask;
+        }
+    }
+}
--- Covid19Radar/Covid19Radar.iOS/Services/Migration/MigrationProcessService.cs
+++ Covid19Radar/Covid19Radar.iOS/Services/Migration/MigrationProcessService.cs
@@ -2,12 +2,32 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
 
+using System.Threading.Tasks;
+using Covid19Radar.Services.Logs;
 using Covid19Radar.Services.Migration;
 
 namespace Covid19Radar.iOS.Services.Migration
 {
     public class MigrationProcessService : IMigrationProcessService
     {
-        // Currently, It is not needed that iOS platform specific migration process.
+        private readonly ILoggerService _loggerService;
+
+        public MigrationProcessService(
+            ILoggerService loggerService
+        )
+        {
+            _loggerService = loggerService;
+        }
+
+        public async Task SetupAsync()
+        {
+            _loggerService.StartMethod();
+
+            await new BGTaskMigrator(
+                _loggerService
+                ).ExecuteAsync();
+
+            _loggerService.EndMethod();
+        }
     }
 }
