3b6edc51762b39686f5ab1fc4f0a3663640761b3
cocoa-dev003 <66989607+cocoa-dev003@users.noreply.github.com>
2022-10-17 11:16:50 +0900

    Fixed SurveyPage layout

--- Covid19Radar/Tests/Covid19Radar.UnitTests/Services/SurveyServiceTests.cs
+++ Covid19Radar/Tests/Covid19Radar.UnitTests/Services/SurveyServiceTests.cs
@@ -3,7 +3,6 @@
 // file, You can obtain one at https://mozilla.org/MPL/2.0/.
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Chino;
 using Covid19Radar.Model;
@@ -24,6 +23,7 @@ namespace Covid19Radar.UnitTests.Services
         private readonly MockRepository _mockRepository;
         private readonly Mock<IEventLogService> _mockEventLogService;
         private readonly Mock<IExposureDataRepository> _mockExposureDataRepository;
+        private readonly Mock<IUserDataRepository> _mockUserDataRepository;
 
         public SurveyServiceTests(ITestOutputHelper testOutputHelper)
         {
@@ -32,6 +32,7 @@ namespace Covid19Radar.UnitTests.Services
             _mockRepository = new MockRepository(MockBehavior.Default);
             _mockEventLogService = _mockRepository.Create<IEventLogService>();
             _mockExposureDataRepository = _mockRepository.Create<IExposureDataRepository>();
+            _mockUserDataRepository = _mockRepository.Create<IUserDataRepository>();
 
             MockTimeZoneInfo.SetJstLocalTimeZone();
         }
@@ -45,12 +46,13 @@ namespace Covid19Radar.UnitTests.Services
         {
             return new SurveyService(
                 _mockEventLogService.Object,
-                _mockExposureDataRepository.Object
-                );
+                _mockExposureDataRepository.Object,
+                _mockUserDataRepository.Object
+            );
         }
 
         [Fact]
-        public async Task BuildSurveyContentTests_HasExposureDataProvision()
+        public async Task BuildSurveyContentTests_HasStartDate_HasExposureDataProvision()
         {
             _mockExposureDataRepository.Setup(x => x.GetDailySummariesAsync()).ReturnsAsync(
                 new List<DailySummary>
@@ -64,9 +66,10 @@ namespace Covid19Radar.UnitTests.Services
                         }
                     }
                 });
+            _mockUserDataRepository.Setup(x => x.GetStartDate()).Returns(new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Local));
 
             SurveyService unitUnderTest = CreateService();
-            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, new DateTimeOffset(2022, 1, 1, 0, 0, 0, new TimeSpan(9, 0, 0)).DateTime, true);
+            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, true, true);
 
             _testOutputHelper.WriteLine($"TimeZoneInfo.Local: {TimeZoneInfo.Local}");
 
@@ -74,30 +77,29 @@ namespace Covid19Radar.UnitTests.Services
             Assert.Equal(2, result.Q2);
             if (MockTimeZoneInfo.IsJst())
             {
-                Assert.Equal(1640962800, result.Q3);
+                Assert.Equal(1640962800, result.StartDate);
             }
             Assert.Single(result.ExposureData.DailySummaryList);
 
             _mockExposureDataRepository.Verify(x => x.GetDailySummariesAsync(), Times.Once());
+            _mockUserDataRepository.Verify(x => x.GetStartDate(), Times.Once());
         }
 
         [Fact]
-        public async Task BuildSurveyContentTests_HasNotExposureDataProvision()
+        public async Task BuildSurveyContentTests_HasNotStartDate_HasNotExposureDataProvision()
         {
             SurveyService unitUnderTest = CreateService();
-            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, new DateTimeOffset(2022, 1, 1, 0, 0, 0, new TimeSpan(9, 0, 0)).DateTime, false);
+            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, false, false);
 
             _testOutputHelper.WriteLine($"TimeZoneInfo.Local: {TimeZoneInfo.Local}");
 
             Assert.Equal(1, result.Q1);
             Assert.Equal(2, result.Q2);
-            if (MockTimeZoneInfo.IsJst())
-            {
-                Assert.Equal(1640962800, result.Q3);
-            }
+            Assert.Null(result.StartDate);
             Assert.Null(result.ExposureData);
 
             _mockExposureDataRepository.Verify(x => x.GetDailySummariesAsync(), Times.Never());
+            _mockUserDataRepository.Verify(x => x.GetStartDate(), Times.Never());
         }
 
         [Fact]
@@ -123,9 +125,10 @@ namespace Covid19Radar.UnitTests.Services
                         }
                     }
                 });
+            _mockUserDataRepository.Setup(x => x.GetStartDate()).Returns(new DateTime(2022, 1, 1, 0, 0, 0, DateTimeKind.Local));
 
             SurveyService unitUnderTest = CreateService();
-            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, new DateTimeOffset(2022, 1, 1, 0, 0, 0, new TimeSpan(9, 0, 0)).DateTime, true);
+            SurveyContent result = await unitUnderTest.BuildSurveyContent(1, 2, true, true);
 
             List<SurveyExposureData.DailySummary> dailySummaryList = result.ExposureData.DailySummaryList;
             Assert.Equal(2, dailySummaryList.Count);
@@ -140,10 +143,10 @@ namespace Covid19Radar.UnitTests.Services
         [Fact]
         public async Task SubmitSurveyTests()
         {
-            SurveyContent testSurveyContent = new SurveyContent { Q1 = 1, Q2 = 2, Q3 = 1640962800, ExposureData = new SurveyExposureData { } };
+            SurveyContent testSurveyContent = new SurveyContent { Q1 = 1, Q2 = 2, StartDate = 1640962800, ExposureData = new SurveyExposureData { } };
 
             _mockEventLogService.Setup(x => x.SendAsync(It.IsAny<EventLog>())).ReturnsAsync(true);
-            
+
             SurveyService unitUnderTest = CreateService();
             bool result = await unitUnderTest.SubmitSurvey(testSurveyContent);
 
@@ -155,7 +158,7 @@ namespace Covid19Radar.UnitTests.Services
                     x.Subtype == "survey" &&
                     x.Content.Value<int>("q1") == 1 &&
                     x.Content.Value<int>("q2") == 2 &&
-                    x.Content.Value<long>("q3") == 1640962800 &&
+                    x.Content.Value<long?>("start_date") == 1640962800 &&
                     x.Content.Value<JObject>("exposure_data") != null
                 )),
                 Times.Once());
